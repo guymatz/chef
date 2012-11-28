5b5e4a8a69e7ec4594c6a1e3cfd364753341fd04
Kyle Jake Plimack <kyle.plimack@gmail.com>
2012-11-28 16:57:19 +0000

    deploy bind-chroot and ipplan helpers

---
+++ cookbooks/bind-chroot/attributes/default.rb
@@ -0,0 +1,33 @@
+
+case node['platform']
+when "ubuntu","debian"
+  # Set CHROOT directory
+  default['bind_chroot']['chroot_dir'] = "/var/bind9/chroot"
+  # Set user name for BIND
+  default['bind_chroot']['bind_user_name'] = "bind"
+  # Set group name for BIND
+  default['bind_chroot']['bind_group_name'] = "bind"
+  # Store zones in OpenLDAP
+  default['bind_chroot']['packages'] = %w{ bind9 bind9-doc bind9-host bind9utils dnsutils }
+  default[:bind_chroot][:service] = "bind9"
+  default[:bind_chroot][:name] = "bind"
+when "redhat","centos","fedora","scientific","amazon"
+  # Set CHROOT directory
+  default[:bind_chroot][:chroot_dir] = "/var/named/chroot"
+  # Set user name for BIND
+  default[:bind_chroot][:bind_user_name] = "named"
+  # Set group name for BIND
+  default[:bind_chroot][:bind_group_name] = "named"
+  default[:bind_chroot][:packages] = %w{ bind-chroot bind-utils perl-DBI libdbi-dbd-mysql }
+  default[:bind_chroot][:service] = "named"
+  default[:bind_chroot][:name] = "named"
+end
+
+# Store zones in OpenLDAP
+default['bind_chroot']['store_zones_in_openldap'] = "no"
+# Include zones.rfc1918
+default['bind_chroot']['zones_rfc1928'] = "yes"
+# Include openldap attributes file
+include_attribute 'bind-chroot::openldap'
+
+default[:bind_chroot][:staging] = "/var/named/chroot-stage"
--- cookbooks/bind-chroot/recipes/default.rb
+++ cookbooks/bind-chroot/recipes/default.rb
@@ -1,10 +1,8 @@
 #
-# Cookbook Name:: bind-chroot
+# Cookbook Name:: bind_chroot
 # Recipe:: default
 #
-
-
-node[:bind-chroot][:packages].each do |pkg|
+node['bind_chroot']['packages'].each do |pkg|
   package pkg do
     action :install
   end
@@ -21,66 +19,74 @@ template "/etc/default/named" do
             })
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}" do
+directory "#{node['bind_chroot']['chroot_dir']}" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
   recursive true
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}/zones" do
+directory "#{node['bind_chroot']['chroot_dir']}/zones/internal" do
+  owner node['bind_chroot']['bind_user_name']
+  group node['bind_chroot']['bind_group_name']
+  mode "0755"
+  recursive true
+end
+
+directory "#{node['bind_chroot']['chroot_dir']}/etc" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}/etc" do
+directory "#{node['bind_chroot']['chroot_dir']}/dev" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}/dev" do
-  owner node['bind_chroot']['bind_user_name']
-  group node['bind_chroot']['bind_group_name']
-  mode "0755"
-end
-
-directory "#{node['bind-chroot']['chroot_dir']}/var" do
+directory "#{node['bind_chroot']['chroot_dir']}/var" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
   action :create
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}/cache" do
+directory "#{node['bind_chroot']['chroot_dir']}/cache" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}/var/cache/bind" do
-  owner node['bind_chroot']['bind_user_name']
-  group node['bind_chroot']['bind_group_name']
-  mode "0755"
-  recursive true
-end
-
-directory "#{node['bind-chroot']['chroot_dir']}/var/run/named" do
+directory "#{node['bind_chroot']['chroot_dir']}/var/cache/bind" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
   recursive true
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}/var/named/internal" do
+directory "#{node['bind_chroot']['chroot_dir']}/var/run/named" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
   recursive true
 end
 
-directory "#{node['bind-chroot']['chroot_dir']}/var/run/bind/run" do
+directory "#{node['bind_chroot']['chroot_dir']}/var/named/internal" do
+  owner node['bind_chroot']['bind_user_name']
+  group node['bind_chroot']['bind_group_name']
+  mode "0755"
+  recursive true
+end
+
+directory "#{node['bind_chroot']['chroot_dir']}/var/run/bind/run" do
+  owner node['bind_chroot']['bind_user_name']
+  group node['bind_chroot']['bind_group_name']
+  mode "0755"
+  recursive true
+end
+
+directory "#{node[:bind_chroot][:staging]}/var/named/internal" do
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0755"
@@ -92,50 +98,97 @@ trusted_nets = Array.new
 results.each do |r|
   trusted_nets << r
 end
+Chef::Log.info("Adding trusted networks to DNS ACLs: " + trusted_nets.join(" "))
 
-template "#{node['bind-chroot']['chroot_dir']}/etc/named.conf" do
+results = search(:dns, "mode:forward")
+fwd_zones = Array.new
+results.each do |z|
+  fwd_zones << z
+end
+
+results = search(:dns, "mode:reverse")
+rev_zones = Array.new
+results.each do |z|
+  rev_zones << z
+end
+
+template "#{node['bind_chroot']['chroot_dir']}/etc/named.conf" do
   source "named.conf.local.erb"
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0644"
   variables({
-              :trusted_nets => trusted_nets
+              :trusted_nets => trusted_nets,
+              :dns_fwd_zones => fwd_zones,
+              :dns_rev_zones => rev_zones,
+              :zones_path => "internal"
             })
 end
 
-template "#{node['bind-chroot']['chroot_dir']}/etc/named.options" do
+template "#{node['bind_chroot']['chroot_dir']}/etc/named.options" do
   source "named.conf.options.erb"
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0644"
 end
 
-template "#{node['bind-chroot']['chroot_dir']}/etc/rndc.key" do
+template "#{node['bind_chroot']['chroot_dir']}/etc/rndc.key" do
   source "rndc.key.erb"
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0644"
 end
 
-cookbook_file "#{node['bind-chroot']['chroot_dir']}/etc/named.rfc1912.zones" do
+cookbook_file "#{node['bind_chroot']['chroot_dir']}/etc/named.rfc1912.zones" do
   source "named.rfc1912.zones"
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0644"
 end
 
-cookbook_file "#{node['bind-chroot']['chroot_dir']}/var/named/localhost.zone" do
+cookbook_file "#{node['bind_chroot']['chroot_dir']}/var/named/localhost.zone" do
   source "localhost.zone"
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0644"
 end
 
-cookbook_file "#{node[:bind-chroot][:zone_path]}/internal/0.0.127.in-addr.arpa.zone" do
+cookbook_file "#{node['bind_chroot']['chroot_dir']}/var/named/0.0.127.in-addr.arpa.zone" do
   source "0.0.127.in-addr.arpa.zone"
   owner node['bind_chroot']['bind_user_name']
   group node['bind_chroot']['bind_group_name']
   mode "0644"
+end
+
+cookbook_file "#{node['bind_chroot']['chroot_dir']}/var/named/named.root" do
+  source "named.root"
+  owner node['bind_chroot']['bind_user_name']
+  group node['bind_chroot']['bind_group_name']
+  mode "0644"
+end
+
+bash "export dns from ipplan" do
+  user "root"
+  cwd "/usr/local/bin/ipplan"
+  code "/usr/local/bin/ipplan/bin/ipplan-updatedns.sh"
+end
+
+git_repo = "git@github.com:kplimack/ipplan-autogen.git"
+git "/staging_bind" do
+  repository git_repo
+  revision "HEAD"
+  reference "master"
+  action :sync
+  user "root"
+  group "root"
+end
+
+bash "copy zone files from git to bind-chroot" do
+  user "root"
+  cwd "/staging_bind/"
+  code <<-EOH
+  cp -r /staging_bind/named/* /var/named/chroot/var/named/
+EOH
 end
 
 service "named" do
