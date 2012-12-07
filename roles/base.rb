f50cb993c076497c8d03bbc83bf3554ae09d8f00
Jake Plimack <jakeplimack@clearchannel.com>
2012-12-07 20:19:16 +0000

    only install sysadmin users by default

--- roles/base.rb
+++ roles/base.rb
@@ -4,7 +4,7 @@ all_env = [
     "recipe[timezone]",
     "recipe[chef-client]",
     "recipe[zsh]",
-    "recipe[users]",
+    "recipe[users::sysadmin]",
     "recipe[sudo]",
     "recipe[emacs]",
     "recipe[vim]",
