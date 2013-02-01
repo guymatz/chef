76c084a7a1a4e28582a584ef9f85d62f37903da8
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-02-01 19:05:52 +0000

    move oncall.ihrdev.com to a shared web in the vpc

---
+++ cookbooks/oncall/attributes/default.rb
@@ -0,0 +1,7 @@
+
+default[:oncall][:app_name] = "oncall"
+
+default[:oncall][:repo] = "https://github.com/iheartradio/sa-oncall"
+#leaving this on HEAD while developing, eventually this will be a git-tag
+default[:oncall][:rev] = "HEAD"
+
---
+++ cookbooks/oncall/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: oncall
+# Recipe:: default
+#
+# Copyright 2012, iHeartRadio
+#
+# All rights reserved - Do Not Redistribute
+#
+
+application "oncall" do
+  name "oncall"
+  path "/data/www/oncall"
+  owner "nobody"
+  group "nogroup"
+  repository node[:oncall][:repo]
+  revision node[:oncall][:rev]
+  migrate false
+  environment_name "production"
+  action :deploy
+
+  django do
+    requirements "requirements.txt"
+    debug true
+    settings_template "settings.py.erb"
+  end
+
+  gunicorn do
+    app_module :django
+    Chef::Log.info("Starting up Gunicorn on port 8081 for OnCall")
+    port 8081
+  end
+end
