54d39e08b1f0cc48e4f0d986b2cf67332d1b6b05
Mark Rechler <mrechler@iheart.com>
2013-02-20 14:20:52 -0500

    add server aliases

--- cookbooks/gems/attributes/mirror.rb
+++ cookbooks/gems/attributes/mirror.rb
@@ -1,5 +1,6 @@
 default[:gem_server][:rf_virtual_host_name]  = "rubygems.#{domain}"
-default[:gem_server][:rf_virtual_host_alias] = "rubygems"
+default[:gem_server][:rf_virtual_host_alias] = [ "gems.ihrdev.com", 
+                                                 "xgems.ihrdev.com" ]
 default[:gem_server][:rf_directory]          = "/data/gems.repo"
 
 default[:gem_server][:ruby_gems]             = "http://rubygems.org"
