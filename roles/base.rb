45b1d9698640f53338a7ed4ad6c36273fc6c7788
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-05 17:16:38 +0000

    add resolver to our base config

--- roles/base.rb
+++ roles/base.rb
@@ -1,30 +1,32 @@
 name "base"
 description "common role applied to all nodes"
 all_env = [
-    "recipe[timezone]",
-    "recipe[chef-client]",
-    "recipe[zsh]",
-    "recipe[users::sysadmins]",
-    "recipe[sudo]",
-    "recipe[emacs]",
-    "recipe[vim]",
-    "recipe[build-essential]",
-    "recipe[cron]",
-    "recipe[perl]",
-    "recipe[python]",
-    "recipe[screen]",
-    "recipe[tmux]",
-    "recipe[nscd]",
-    "recipe[ntp]",
-    "recipe[yum]",
-    "recipe[openssh]",
-    "recipe[motd-tail]",
-    "recipe[nagios::client]",
-    "recipe[operations]"
-]
+           "recipe[timezone]",
+           "recipe[chef-client]",
+           "recipe[resolver]",
+           "recipe[zsh]",
+           "recipe[users::sysadmins]",
+           "recipe[sudo]",
+           "recipe[emacs]",
+           "recipe[vim]",
+           "recipe[git]",
+           "recipe[build-essential]",
+           "recipe[cron]",
+           "recipe[perl]",
+           "recipe[python]",
+           "recipe[screen]",
+           "recipe[tmux]",
+           "recipe[nscd]",
+           "recipe[ntp]",
+           "recipe[yum]",
+           "recipe[openssh]",
+           "recipe[motd-tail]",
+           "recipe[nagios::client]",
+           "recipe[operations]"
+          ]
 run_list(all_env)
 
 env_run_lists(
               "_default" => all_env,
               "ec2" => all_env
-)
+              )
