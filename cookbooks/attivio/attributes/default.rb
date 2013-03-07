63751c3e9be478c9946ea0ef0fb875c9cdf25842
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-07 01:14:48 +0000

    render configs for Attivio::standalone

--- cookbooks/attivio/attributes/default.rb
+++ cookbooks/attivio/attributes/default.rb
@@ -2,10 +2,9 @@
 default[:attivio][:aie_install_path] = "/data/apps/attivio"
 default[:attivio][:install_path] = "/data/apps/attivio31"
 default[:attivio][:config_path] = "/data/apps/attivio31/iheartradio3/conf"
+default[:attivio][:bin_path] = "/data/apps/attivio31/iheartradio3/bin"
 default[:attivio][:user] = "attivio"
 default[:attivio][:group] = "attivio"
-
-default[:attivio][:env] = "local"
 
 default[:attivio][:repo] = "git@github.com:iheartradio/attivio.git"
 default[:attivio][:rev] = "deploy"
@@ -27,6 +26,19 @@ default[:attivio][:search_config] = ({
                                        "documentCacheLoadFactor" => "0.01" # cache size will be 10% of the segment size
                                      })
 
+default[:attivio][:memory] = "2g"
+
 default[:attivio][:connector_port] = "19200"
 default[:attivio][:indexer_port] = "19100"
 default[:attivio][:searcher_port] = "19000"
+
+# ARTIST XML INDEXING
+# These paths are appended to default[:attivio][:install_path]
+default[:attivio][:directory][:xml_ingest] = "/input"
+default[:attivio][:directory][:priority_ingest] = "/input/priority"
+default[:attivio][:directory][:fac_archive] = "/fac-archive3"
+
+default[:attivio][:xml][:artist_delete_batch_size] = "10"
+default[:attivio][:xml][:track_update_batch_size] = "10"
+default[:attivio][:xml][:artist_update_batch_size] = "10"
+default[:attivio][:xml][:track_bundle_update_batch_size] = "10"
--- cookbooks/attivio/recipes/standalone.rb
+++ cookbooks/attivio/recipes/standalone.rb
@@ -1,23 +1,85 @@
+app_secrets = Chef::EncryptedDataBagItem.load("secrets", "attivio")
 
-template "#{node[:attivio][:config_path]}/iheartradio-#{node[:attivio][:env]}.xml" do
+template "#{node[:attivio][:config_path]}/iheartradio-#{node.chef_environment}.xml" do
   source "iheartradio-env.xml.erb"
   owner node[:attivio][:user]
   group node[:attivio][:group]
   variables({
-              :attivio_env => node[:attivio][:env],
-              :search_config => node[:attivio][:search_config]
+              :attivio_env => node.chef_environment,
+              :search_config => node[:attivio][:search_config],
+              :attivio_conf => node[:attivio][:config_path]
             })
 end
 
-template "#{node[:attivio][:config_path]}/iheartradio.#{node[:attivio][:env]}.properties" do
+template "#{node[:attivio][:config_path]}/iheartradio.xml" do
+  source "iheartradio.xml.erb"
+  owner node[:attivio][:user]
+  group node[:attivio][:group]
+  variables({
+              :attivio_env => node.chef_environment
+            })
+end
+
+template "#{node[:attivio][:config_path]}/iheartradio.#{node.chef_environment}.properties" do
   source "iheartradio.env.properties.erb"
   owner node[:attivio][:user]
   group node[:attivio][:group]
   variables({
               :attivio_env => node[:attivio][:env],
+              :install_path => node[:attivio][:install_path],
               :connector_port => node[:attivio][:connector_port],
               :indexer_port => node[:attivio][:indexer_port],
               :searcher_port => node[:attivio][:searcher_port],
-              :cluster => Array.new
+              :dbconns => app_secrets["#{node.chef_environment}"],
+              :directory => node[:attivio][:directory],
+              :xml => node[:attivio][:xml],
+              :cluster => nil
             })
 end
+
+template "#{node[:attivio][:config_path]}/topology-nodes-#{node.chef_environment}.xml" do
+  source "topology-nodes-env.xml.erb"
+  owner node[:attivio][:user]
+  group node[:attivio][:group]
+  variables({
+              :connector_port => node[:attivio][:connector_port],
+              :indexer_port => node[:attivio][:indexer_port],
+              :searcher_port => node[:attivio][:searcher_port],
+              :cluster => nil
+            })
+end
+
+directory "#{node[:attivio][:bin_path]}/#{node.chef_environment}" do
+  owner node[:attivio][:user]
+  group node[:attivio][:group]
+  mode "0755"
+end
+
+template "#{node[:attivio][:bin_path]}/#{node.chef_environment}/env.sh" do
+  source "env.sh.erb"
+  owner node[:attivio][:user]
+  group node[:attivio][:group]
+  variables({
+              :attivio_home => node[:attivio][:aie_install_path],
+              :attivio_project => node[:attivio][:install_path],
+              :zookeeper => nil
+            })
+end
+
+template "#{node[:attivio][:bin_path]}/#{node.chef_environment}/start-aie-local.sh" do
+  source "start-aie-local.sh.erb"
+  owner node[:attivio][:user]
+  group node[:attivio][:group]
+  mode "0755"
+  variables({
+              :attivio_mem => node[:attivio][:memory],
+              :iheart_env_xml => "#{node[:attivio][:config_path]}/iheartradio-#{node.chef_environment}.xml"
+            })
+end
+
+file "#{node[:attivio][:aie_install_path]}/conf/attivio.license" do
+  owner node[:attivio][:user]
+  group node[:attivio][:group]
+  content app_secrets["license"]
+end
+
