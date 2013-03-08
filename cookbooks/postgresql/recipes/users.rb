518ec95b77e592493c5fa2c5644c62437b60fd7c
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-08 17:20:33 +0000

    skip making psql users for now

--- cookbooks/postgresql/recipes/users.rb
+++ cookbooks/postgresql/recipes/users.rb
@@ -1,16 +1,16 @@
 include_recipe "users::dba"
 
-dbas = search(:users, "groups:dba and groups:sysadmin")
-dbas.each do |u|
-  psql_user u["id"] do
-    host node["fqdn"]
-    port node['postgresql']['config']['port']
-    admin_username 'postgres'
-    admin_password node['postgresql']['password']['postgres']
-    password nil
-    host "localhost"
-  end
-end
+# dbas = search(:users, "groups:dba and groups:sysadmin")
+# dbas.each do |u|
+#   psql_user u["id"] do
+#     host node["fqdn"]
+#     port node['postgresql']['config']['port']
+#     admin_username 'postgres'
+#     admin_password node['postgresql']['password']['postgres']
+#     password nil
+#     host "localhost"
+#   end
+# end
 
 sudo "postgresql-dba" do
   group "dba"
--- cookbooks/users/recipes/default.rb
+++ cookbooks/users/recipes/default.rb
@@ -5,3 +5,4 @@
 include_recipe "users::sysadmins"
 include_recipe "users::dev"
 include_recipe "users::data-team"
+include_recipe "users::dba"
