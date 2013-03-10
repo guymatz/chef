7c7da67e8674c49505e6108050b8ed6eea9dca04
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-10 20:08:36 +0000

    fix syntax

--- cookbooks/basejump/recipes/tools.rb
+++ cookbooks/basejump/recipes/tools.rb
@@ -2,13 +2,14 @@
 app_secrets = Chef::EncryptedDataBagItem.load("secrets", "drac")
 
 %w{ consoleto poweron poweroff rebooter }.each do |s|
-template "/usr/sbin/#{s}" do
-  source "%{s}.sh.erb"
-  owner "root"
-  group "root"
-  mode "0700"
-  variables({
-              :ipmi_user => app_secrets['username'],
-              :ipmi_pass => app_secrets['password']
-            })
+  template "/usr/sbin/#{s}" do
+    source "#{s}.sh.erb"
+    owner "root"
+    group "root"
+    mode "0700"
+    variables({
+                :ipmi_user => app_secrets['username'],
+                :ipmi_pass => app_secrets['password']
+              })
+  end
 end
