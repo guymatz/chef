4dad07b09a10573b4ac13a25df772d527287cf8b
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-14 17:15:26 +0000

    install diffutils everywhere

--- cookbooks/operations/attributes/default.rb
+++ cookbooks/operations/attributes/default.rb
@@ -9,6 +9,6 @@ case node['platform']
 when "ubuntu","debian"
   default['operations']['packages'] = %w[ telnet dstat snmp net-tools htop procps nmap tree sysstat ]
 when "redhat","centos","fedora","scientific","amazon"
-  default['operations']['packages'] = %w[ telnet dstat net-snmp net-tools htop procps nmap tree sysstat ]
+  default['operations']['packages'] = %w[ telnet dstat net-snmp net-tools htop procps nmap tree sysstat diffutils ]
 end
 
