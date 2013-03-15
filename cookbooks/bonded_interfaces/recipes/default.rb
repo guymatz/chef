d68ee6b8c98570ea38890ae43c9627f951238956
Joseph Hammerman <josephhammerman@clearchannel.com>
2013-03-15 05:11:41 +0000

    fixed various syntax errors, etc. in bonded_interfaces

--- cookbooks/bonded_interfaces/recipes/default.rb
+++ cookbooks/bonded_interfaces/recipes/default.rb
@@ -7,7 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node[:bonded_interfaces][:slaves].each do |s|
+slaves = node['override']['bonded_interfaces']['configuration']['slaves']
+
+slaves.each do |s|
 	template "/etc/sysconfig/network-scripts/ifcfg-#{s}" do
 		source "ifcfg-slave.erb"
 		owner "root"
@@ -15,36 +17,36 @@ node[:bonded_interfaces][:slaves].each do |s|
 		mode "0644"
 		variables({
 			:device => s,
-			:master => node[:bonded_interfaces][:master]
+			:master => node['override']['bonded_interfaces']['master']
 		})
-		notifies :run, resources(:execute => "ifdown #{s}")
-		notifies :run, resources(:execute => "ifup #{s}")
+		#notifies :run, resources(:execute => "ifdown #{s}")
+		#notifies :run, resources(:execute => "ifup #{s}")
 	end
 end
 
-template "/etc/sysconfig/network-scripts/ifcfg-#{node[:bonded_interfaces][:master]}" do
+template "/etc/sysconfig/network-scripts/ifcfg-#{node['override']['bonded_interfaces']['master']}" do
 	source "ifcfg-bonded"
 	mode 0644
 	owner "root"
 	group "root"
 	variables({
-                        :master => node[:bonded_interfaces][:master]
+                        :master => node['override']['bonded_interfaces']['master']
         })
-	notifies :run, resources(:execute => "ifdown #{node[:bonded_interfaces][:master]}")
-	notifies :run, resources(:execute => "ifup #{node[:bonded_interfaces][:master]}")
+	#notifies :run, resources(:execute => "ifdown #{node[:override][:bonded_interfaces][:master]}")
+	#notifies :run, resources(:execute => "ifup #{node[:override][:bonded_interfaces][:master]}")
 end
 
-template "/etc/sysconfig/network-scripts/ifcfg-#{node[:bonded_interfaces][:master]}.#{node[:bonded_interfaces][:vlan]}" do
+template "/etc/sysconfig/network-scripts/ifcfg-#{node['override']['bonded_interfaces']['master']}.#{node['override']['bonded_interfaces']['configuration']['vlan']}" do
 	source "ifcfg-bonded_and_trunked.erb"
         owner "root"
         group "root"
         mode "0644"
         variables({
-		:device => node[:bonded_interfaces][:master],
-     		:ip => node[:bonded_interfaces][:configuration][:ip],
-		:vlan => node[:bonded_interfaces][:configuration][:vlan],
-		:netmask => node[:bonded_interfaces][:configuration][:netmask]
+		:device => node['override']['bonded_interfaces']['master'],
+     		:ip => node['override']['bonded_interfaces']['configuration']['ip'],
+		:vlan => node['override']['bonded_interfaces']['configuration']['vlan'],
+		:netmask => node['override']['bonded_interfaces']['configuration']['netmask']
 	})
-	notifies :run resources(:execute => "ifdown #{node[:bonded_interfaces][:master]}.#{node[:bonded_interfaces][:vlan]}") 
-	notifies :run resources(:execute => "ifup #{node[:bonded_interfaces][:master]}.#{node[:bonded_interfaces][:vlan]}")
+	#notifies :run, resources(:execute => "ifdown #{node[:bonded_interfaces][:master]}.#{node[:bonded_interfaces][:configuration][:vlan]}") 
+	#notifies :run, resources(:execute => "ifup #{node[:bonded_interfaces][:master]}.#{node[:bonded_interfaces][:configuration][:vlan]}")
 end
