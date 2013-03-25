9d4bf1ba9afdc1ad5b87ed7927ecf15e1230dd2e
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-25 15:22:09 +0000

    Import mailman version 0.9.5

---
+++ cookbooks/mailman/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "computerlyrik"
+maintainer_email "chef-cookbooks@computerlyrik.de"
+license          "Apache 2.0"
+description      "Installs/Configures mailman"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.9.5"
+
+depends "openssl"
