ac29156afda29a54951b347bed8c241474327f41
Mark Rechler <mrechler@iheart.com>
2013-03-25 16:31:05 -0400

    enable all desired plugins

---
+++ cookbooks/rabbitmq/attributes/amp.rb
@@ -0,0 +1,6 @@
+default[:rabbitmq][:plugins] = [ "amqp_client",
+                                 "mochiweb",
+                                 "rabbitmq_management",
+                                 "rabbitmq_management_agent",
+                                 "webmachine"
+                               ]
--- cookbooks/rabbitmq/recipes/amp.rb
+++ cookbooks/rabbitmq/recipes/amp.rb
@@ -1,12 +1,21 @@
 # Install production rabbit setup
 
+# Setup the management plugin
 include_recipe "rabbitmq::mgmt_console"
 
+# Ensure we have all other desired plugins
+node[:rabbitmq][:plugins].each do |plugin|
+  rabbitmq_plugin plugin do
+    action :enable
+  end
+end
+
+# Setup the amp vhost
 rabbitmq_vhost "/amp" do
   action :add
 end
 
-#Setup all our users/passwords
+# Setup all our users/passwords
 users = Chef::EncryptedDataBagItem.load("rabbitmq", "passwords").to_hash
 users.each do |k,v|
   next if k == "id"
@@ -21,11 +30,13 @@ users.each do |k,v|
   end
 end
 
+# Make thumbplay an admin
 rabbitmq_user "thumbplay" do
   tags "administrator"
   action :set_tags
 end
 
+# Delete the guest user
 rabbitmq_user "guest" do
   action :delete
 end
