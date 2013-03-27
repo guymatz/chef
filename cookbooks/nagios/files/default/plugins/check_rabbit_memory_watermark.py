c2f1f0589e2c8fcda97656b7f66ac98947ee2497
Mark Rechler <mrechler@iheart.com>
2013-03-27 13:59:50 -0400

    add rabbit check python scripts

---
+++ cookbooks/nagios/files/default/plugins/check_rabbit_memory_watermark.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python26
+
+import mechanize
+import sys
+import base64
+import json
+
+target_hostname=sys.argv[1]
+
+username = "thumbplay"
+password = "clearchannel"
+target_page= "http://" + target_hostname + ":55672/api/nodes"
+
+#print target_page
+
+browser = mechanize.Browser()
+browser.addheaders.append(('Authorization', 'Basic %s' % base64.encodestring('%s:%s' % (username, password))))
+rest_response=browser.open(target_page)
+
+objects=json.loads(rest_response.read())
+
+for o in objects:
+ for key in o.keys():
+  if key == 'mem_used':
+   memused = o[key]
+  if key == 'mem_limit':
+   memlimit = o[key]
+
+memutilization=((float(memused) / float(memlimit)) * 100)
+
+if memutilization >= 70.0:
+ print "Memory utilization at %70!"
+ sys.exit(2)
+if memutilization >= 65.0:
+ print "Memory utilization at %65!"
+ sys.exit(1)
+else:
+ print "OK"
+ sys.exit(0)
+
+#if memlimit
+
+
+
