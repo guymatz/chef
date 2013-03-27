7778906a042496333a8cc34ed28755a5bcebef54
John Paul Deragon <jpderagon@gmail.com>
2013-03-27 14:49:40 -0500

    encoder cookbook

---
+++ cookbooks/encoders/attributes/default.rb
@@ -0,0 +1,3 @@
+#Place where application will live
+default[:pkgs] = [ 'libtool-ltdl-devel', 'mpg123', 'alsa-lib', ' compat-libstdc++-33', 'sox', 'lame-libs', 'ncurses-lib', 'libmad-devel', 'libmad' ]
+#default[:gems] = [  ]
---
+++ cookbooks/encoders/metadata.rb
@@ -0,0 +1,10 @@
+name             'encoder'
+maintainer       'ccd ihr ops'
+maintainer_email 'ccd-sa@clearchannel.com'
+license          'All rights reserved'
+description      'Installs/Configures encoder'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'yum'
+depends          'ruby'
+depends          'java'
