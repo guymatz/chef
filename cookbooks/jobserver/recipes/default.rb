680323d7f297c9748adc2d5b7eff1abe3cfe69d9
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-29 14:56:48 +0000

    monitor crond process on jobservers

--- cookbooks/jobserver/recipes/default.rb
+++ cookbooks/jobserver/recipes/default.rb
@@ -11,3 +11,11 @@ if node.chef_environment == 'prod'
   include_recipe "jobserver::ha"
 end
 
+nagios_nrpecheck "Check-Cron" do
+  command "#{node['nagios']['plugin_dir']}/check_procs"
+  warning_condition "1:1"
+  critical_condition "1:1"
+  parameters "-C crond"
+  action :add
+  notifies :restart, resources(:service => "nagios-nrpe-server")
+end
