0b9bab633ddbf4856e561bb7d380993d44edad22
Jake Plimack <jake.plimack@gmail.com>
2013-03-30 17:21:46 +0000

    fix deps

--- cookbooks/monitoring/metadata.rb
+++ cookbooks/monitoring/metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+%w{ vmware-tools }.each do |d|
+  depends d
+end
