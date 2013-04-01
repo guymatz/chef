3d4ad88ee52927cd2cfa7d5112d923f61aa0f8bd
Jake Plimack <jake.plimack@gmail.com>
2013-04-01 20:49:08 +0000

    memcache::multi will setup the node to run multiple instances on different ports * searches databag :memcached and attempts to match the node's host-class with   the ID of the data bag. ** For example a host named iad-web101.ihr has a class "iad-web", and will    match a databag id "iad-web-cache". * for a single instance, use the default recipe and override the attributes

---
+++ roles/memcached.rb
@@ -0,0 +1,12 @@
+name "memcached"
+description "memcached"
+all_env = [
+           "recipe[memcached]"
+          ]
+run_list(all_env)
+env_run_lists(
+              "_default" => all_env,
+              "qa2" => all_env,
+              "prod" => all_env,
+              "ec2" => all_env
+              )
