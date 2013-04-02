7ef2a1b37dd99d3b47b7fa9e4eb3180be5011e4d
Jake Plimack <Jake.plimack@gmail.com>
2013-04-01 22:24:33 -0400

    foodcritic memcached I wrote a LWRP for memcached-instances FC003: Check whether you are running with chef server before using server-specific features: memcached/recipes/multi.rb:41 FC015: Consider converting definition to a LWRP: memcached/definitions/memcached_instance.rb:1 FC043: Prefer new notification syntax: memcached/recipes/default.rb:41 FC043: Prefer new notification syntax: memcached/recipes/default.rb:56 FC043: Prefer new notification syntax: memcached/recipes/default.rb:74 FC043: Prefer new notification syntax: memcached/recipes/multi.rb:83

---
+++ cookbooks/memcached/resources/memcached_instance.rb
@@ -0,0 +1,18 @@
+#
+# Author:	Jake Plimack <jake.plimack@gmail.com
+# Cookbook Name::memcached
+# Resource::memcached_instance
+#
+
+actions :create, :remove
+
+def initialiaze(*args)
+    super
+    @action = [:enable, :start]
+end
+
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :listen, :kind_of => String, default => "0.0.0.0"
+attribute :memory, :kind_of => Integer, :default => nil
+attribute :port, :kind_of => Integer, :default => "11211"
+attrivute :options, :kind_of => String, default => ''
