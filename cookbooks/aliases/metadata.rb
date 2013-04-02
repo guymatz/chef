841bb7750e325155a344dbf40fe3841420ff4e72
Jake Plimack <Jake.plimack@gmail.com>
2013-04-01 21:19:51 -0400

    FC008: Generated cookbook metadata needs updating: aliases/metadata.rb:2 FC008: Generated cookbook metadata needs updating: aliases/metadata.rb:3

--- cookbooks/aliases/metadata.rb
+++ cookbooks/aliases/metadata.rb
@@ -1,6 +1,6 @@
 name             'aliases'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'iHeartRadio'
+maintainer_email 'Jake.plimack@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures aliases'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
