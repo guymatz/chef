9ece1a9d28082a7f3c0b32ac5b0a8604cfd175d2
Jake Plimack <jake.plimack@gmail.com>
2013-04-05 17:25:35 +0000

    adjust default memcached pool size

--- cookbooks/memcached/attributes/default.rb
+++ cookbooks/memcached/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:memcached][:memory]  = 8
+default[:memcached][:memory]  = 4096
 default[:memcached][:port]    = 11211
 default[:memcached][:user]    = "nobody"
 default[:memcached][:listen]  = "0.0.0.0"
