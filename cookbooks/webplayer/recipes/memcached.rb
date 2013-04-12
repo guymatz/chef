7557b52958504e4f7cc000bd76f5f2e63c33bc76
Jake Plimack <jake.plimack@gmail.com>
2013-04-12 18:51:03 +0000

    make sure memcached is started [on boot, too]

--- cookbooks/webplayer/recipes/memcached.rb
+++ cookbooks/webplayer/recipes/memcached.rb
@@ -4,3 +4,8 @@
 include_recipe "memcached"
 
 node.set[:memcached][:memory] = "20480"
+
+service "memcached" do
+  action [:enable, :start]
+  supports :status => true, :start => true, :stop => true, :restart => true
+end
