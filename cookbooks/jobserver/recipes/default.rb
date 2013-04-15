82357bb66ef145f9c60bdb4b97c815ef8e975b6d
Jake Plimack <jake.plimack@gmail.com>
2013-04-15 16:04:18 +0000

    Deploy fac-jobs onto jobserver and setup java if necessary

--- cookbooks/jobserver/recipes/default.rb
+++ cookbooks/jobserver/recipes/default.rb
@@ -9,7 +9,6 @@
 
 if node.chef_environment == 'prod'
   include_recipe "jobserver::ha"
-  include_recipe "fac::talk"
 end
 
 nagios_nrpecheck "Process-Cron" do
@@ -20,3 +19,11 @@ nagios_nrpecheck "Process-Cron" do
   action :add
   notifies :restart, resources(:service => "nagios-nrpe-server")
 end
+
+
+# Things that should run on the jobserver
+#### Keep them organized by the Application(cookbook) that they belong to.
+#### When adding jobs, include the JIRA ticket number in the comments
+#### so we can track them easily.
+
+include_recipe "jobserver::fac"
---
+++ cookbooks/jobserver/recipes/fac.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: jobserver
+# Recipe:: fac
+#
+# Copyright 2013, iHeartRadio
+#
+# All rights reserved - Do Not Redistribute
+#
+# Description: Sets up jobs for FAC-PRN, fac-talk, and fac-music
+#              allong with necessary monitoring for the processes and jobs
+
+%w{ default prn music talk }.each do |r|
+  include_recipe "fac::#{r}"
+end
