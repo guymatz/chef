ee24d24e52e6ef723929c9d7a559c8e8bedb8a2a
Jake Plimack <jake.plimack@gmail.com>
2013-04-16 21:15:14 +0000

    dont install nagios until sourceforge unfucks itself

--- roles/base.rb
+++ roles/base.rb
@@ -29,7 +29,7 @@ env_run_lists(
               "_default" => all_env,
               "qa2" => all_env,
               "dev" => all_env,
-              "prod" => all_env + ["role[monitored]"],
+              "prod" => all_env,
               "ec2-prod" => all_env + ["recipe[users::data-team]"],
               "ec2" => all_env
               )
