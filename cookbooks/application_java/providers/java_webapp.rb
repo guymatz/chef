9552094cfaee305185b4816f075400b46e9bc69f
Jake Plimack <jake.plimack@gmail.com>
2013-04-17 16:40:26 +0000

    Import application_java version 1.0.4

--- cookbooks/application_java/providers/java_webapp.rb
+++ cookbooks/application_java/providers/java_webapp.rb
@@ -20,10 +20,6 @@
 include Chef::Mixin::LanguageIncludeRecipe
 
 action :before_compile do
-
-  # include_recipe 'java'
-
-  new_resource.strategy :java_remote_file
 end
 
 action :before_deploy do
@@ -71,7 +67,7 @@ def create_context_file
 
   template "#{new_resource.path}/shared/#{new_resource.application.name}.xml" do
     source new_resource.context_template || "context.xml.erb"
-    cookbook new_resource.context_template ? new_resource.cookbook_name : "application_java"
+    cookbook new_resource.context_template ? new_resource.cookbook_name.to_s : "application_java"
     owner new_resource.owner
     group new_resource.group
     mode "644"
