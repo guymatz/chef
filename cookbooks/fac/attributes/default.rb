17b3dc2e2284767b5228354b9d543aed86a5d25d
Jake Plimack <jake.plimack@gmail.com>
2013-04-19 15:30:23 +0000

    fix paths

--- cookbooks/fac/attributes/default.rb
+++ cookbooks/fac/attributes/default.rb
@@ -5,8 +5,8 @@ default[:fac][:PRN][:version] = "3.4.4"
 default[:fac][:talk][:version] = "3.4.4"
 default[:fac][:music][:version] = "3.4.4"
 
-default[:fac][:script_path] = "/usr/local/bin/fac"
+default[:fac][:script_path] = "/data/jobs/fac"
 default[:fac][:url] = "http://archiva.ihrdev.com:8080/archiva/repository/internal/com/ccrd/fac"
 default[:fac][:radiobuild][:url] = "http://yum.ihr/files/radio-build-20130416.jar"
 
-default[:fac][:amptools][:repo] = "https://github.com/iheartradio/amp-tools.git"
+default[:fac][:amptools][:repo] = "git@github.com:iheartradio/amp-tools.git"
