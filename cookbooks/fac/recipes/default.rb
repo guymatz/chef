d695e9d076c20a16fde07d26065e3350bd7af66a
Jake Plimack <jake.plimack@gmail.com>
2013-04-19 15:30:00 +0000

    create etc/hosts entries for sompa mongos

--- cookbooks/fac/recipes/default.rb
+++ cookbooks/fac/recipes/default.rb
@@ -10,3 +10,17 @@
 node.set[:java][:oracle][:accept_oracle_download_terms] = true
 node.save
 include_recipe "java"
+
+Chef::Log.info("Bandaiding FAC with some /etc/hosts entries")
+ruby_block "fac-etc-hosts" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/hosts")
+    file.insert_line_if_no_match("/mongodb-fac1b01/", "10.90.47.182\tmongodb-fac1b01")
+    file.insert_line_if_no_match("/mongodb-fac1b02/", "10.90.47.183\tmongodb-fac1b02")
+    file.insert_line_if_no_match("/mongodb-fac1b03/", "10.90.47.184\tmongodb-fac1b03")
+    file.insert_line_if_no_match("/mongodb-fac1b04/", "10.90.47.185\tmongodb-fac1b04")
+    file.write_file
+  end
+  not_if { node.normal.attribute?('whipped') }
+  notifies :restart, "service[network]"
+end
