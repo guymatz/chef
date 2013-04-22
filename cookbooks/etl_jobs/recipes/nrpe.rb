5f3e38a3220c9c36fb9b6ad010b6159559a27af1
Mark Rechler <mrechler@iheart.com>
2013-04-22 20:17:17 +0000

    add exit code checks

--- cookbooks/etl_jobs/recipes/nrpe.rb
+++ cookbooks/etl_jobs/recipes/nrpe.rb
@@ -2,7 +2,32 @@ nagios_nrpecheck "check_app_proc_facebookconsumer" do
   command "#{node['nagios']['plugin_dir']}/check_procs -a '/home/amqp-consumer/facebook-consumer'"
   critical_condition "1:1"
 end
+
 nagios_nrpecheck "check_app_proc_playlogconsumer" do
   command "#{node['nagios']['plugin_dir']}/check_procs -a '/home/amqp-consumer/playlog-consumer'"
   critical_condition "1:1"
+end
+
+nagios_nrpecheck "check_playlog_exit_code" do
+  command "#{node['nagios']['plugin_dir']}/check_exitcode.sh /var/log/playlog/exitCode"
+end
+
+nagios_nrpecheck "check_skiplog_exit_code" do
+  command "#{node['nagios']['plugin_dir']}/check_exitcode.sh /var/log/skiplog/exitCode"
+end
+
+nagios_nrpecheck "check_sysinfo_exit_code" do
+  command "#{node['nagios']['plugin_dir']}/check_exitcode.sh /var/log/sysinfo/exitCode"
+end
+
+nagios_nrpecheck "check_talkplaylog_exit_code" do
+  command "#{node['nagios']['plugin_dir']}/check_exitcode.sh /var/log/talkplaylog/exitCode"
+end
+
+nagios_nrpecheck "check_talkthumbslog_exit_code" do
+  command "#{node['nagios']['plugin_dir']}/check_exitcode.sh /var/log/talkthumbslog/exitCode"
+end
+
+nagios_nrpecheck "check_customthumbslog_exit_code" do
+  command "#{node['nagios']['plugin_dir']}/check_exitcode.sh /var/log/customradiothumbslog/exitCode"
 end
