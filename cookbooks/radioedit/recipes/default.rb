d819cbbe1b41ce4b739cbeee388c1a09d8ac5dd3
Mark Rechler <mrechler@iheart.com>
2013-04-23 14:38:52 +0000

    only create repo file if not there

--- cookbooks/radioedit/recipes/default.rb
+++ cookbooks/radioedit/recipes/default.rb
@@ -9,6 +9,7 @@
 
 remote_file "/etc/yum.repos.d/zeromq.repo" do
   source "http://download.opensuse.org/repositories/home:/fengshuo:/zeromq/CentOS_CentOS-6/home:fengshuo:zeromq.repo"
+  action :create_if_missing
 end
 
 node[:radioedit][:core][:packages].each do |p|
