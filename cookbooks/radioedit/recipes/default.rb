b3361c271703c8dbbf623f7c1224519f1e3ddce5
Mark Rechler <mrechler@iheart.com>
2013-04-23 14:32:23 +0000

    move repo install to default recipe

--- cookbooks/radioedit/recipes/default.rb
+++ cookbooks/radioedit/recipes/default.rb
@@ -7,6 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
+remote_file "/etc/yum.repos.d/zeromq.repo" do
+  source "http://download.opensuse.org/repositories/home:/fengshuo:/zeromq/CentOS_CentOS-6/home:fengshuo:zeromq.repo"
+end
+
 node[:radioedit][:core][:packages].each do |p|
   package p
 end
