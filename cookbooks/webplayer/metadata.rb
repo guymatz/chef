f450943791a44356ba7a357655e6672aaa3d9fd6
Jake Plimack <jake.plimack@gmail.com>
2013-04-25 15:50:26 +0000

    missing users dep

--- cookbooks/webplayer/metadata.rb
+++ cookbooks/webplayer/metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures webplayer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w{ java application_python apache2 bondage }.each do |dep|
+%w{ java application_python apache2 bondage users }.each do |dep|
   depends dep
 end
