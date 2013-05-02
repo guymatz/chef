e71fa3733a3020fdbf3f178de6656553f0c05d32
Mark Rechler <mrechler@iheart.com>
2013-05-02 11:19:52 -0400

    setup custom_talk wsgi app

--- cookbooks/custom_talk/recipes/default.rb
+++ cookbooks/custom_talk/recipes/default.rb
@@ -6,3 +6,26 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "users::deployer"
+
+git "/data/apps/www/custom_talk" do
+  repository "git@github.com:iheartradio/tool-talk.git"
+  reference "deploy_custom_talk"
+  user "ihr-deployer"
+  group "ihr-deployer"
+end
+
+# Enable WSGI Module
+include_recipe "apache2"
+include_recipe "apache2::mod_wsgi"
+
+web_app "custom_talk" do
+  server_name node['hostname']
+  server_aliases [node['fqdn']]
+  docroot "/data/apps/www/custom_talk"
+  app_alias {'/customtalk/public/tp_shared' => '/data/aps/www/customtalk/tp_shared/public',
+             '/customtalk/public' => '/data/apps/www/customtalk/public'}
+  wsgi_daemon 'customtalk_prod user=apache threads=15'
+  wsgi_alias '/customtalk /data/apps/www/customtalk/customtalk.wsgi'
+  wsgi_proc 'customtalk_prod'
+end
