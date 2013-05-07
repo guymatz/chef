f55628f0ad241be52cd4fae1e9f9d052d7794714
John Deragon <jderagon@clearchannel.com>
2013-05-07 21:45:56 +0000

    updated timeout on collector.  added log rotate for fail2ban and splunk

--- cookbooks/splunk/metadata.rb
+++ cookbooks/splunk/metadata.rb
@@ -3,6 +3,8 @@ maintainer_email "andrew.painter@bestbuy.com"
 license          "Apache 2.0"
 description      "Installs/Configures a Splunk Server, Forwarders, and Apps"
 version          "0.1.1"
+
+depends "logrotate"
 %w{redhat centos fedora debian ubuntu}.each do |os|
   supports os
 end
