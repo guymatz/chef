c5902dce9e439368352fa47e70b4fe4fc86911f4
Mark Rechler <mrechler@iheart.com>
2013-05-10 13:59:14 +0000

    fix mongo proc check

--- cookbooks/mongodb/recipes/nagios.rb
+++ cookbooks/mongodb/recipes/nagios.rb
@@ -9,7 +9,7 @@ nagios_nrpecheck "Mongo_Process_Mongos" do
   command "#{node['nagios']['plugin_dir']}/check_procs"
   warning_condition "1:1"
   critical_condition "1:1"
-  parameters "-C mongos -a /etc//mongosd.conf"
+  parameters '-C mongos -a mongosd.conf'
   action :add
   notifies :restart, resources(:service => "nagios-nrpe-server")
 end
