806713f894fa5638814e29cf768b8081711e3aac
John Deragon <jderagon@clearchannel.com>
2013-05-15 17:07:51 +0000

    added diamond to monitorng role

--- roles/monitored.rb
+++ roles/monitored.rb
@@ -3,5 +3,6 @@ description "Sets up Nagios, Rsyslog, Ganglia"
 run_list(
          "recipe[nagios]",
          "recipe[rsyslog::client]",
+         "recipe[diamond]",
          "recipe[monitoring]"
 )
