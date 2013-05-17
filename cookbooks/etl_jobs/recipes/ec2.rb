ad9817b006462244b7d1bfee0746d6d81e28a0a1
Mark Rechler <mrechler@iheart.com>
2013-05-17 21:13:03 +0000

    add all jobs to ec2

--- cookbooks/etl_jobs/recipes/ec2.rb
+++ cookbooks/etl_jobs/recipes/ec2.rb
@@ -20,18 +20,20 @@ directory "/data/log/event"
 directory "/data/log/event/input" do
   mode 0777
 end
-
 directory "/data/log/event/processed"
 remote_file "/data/jobs/event/event_job.jar" do
   source "http://yum.ihr/files/jobs/event/event_job.jar"
 end
-
 remote_file "/data/jobs/event/batch.properties" do
   source "http://yum.ihr/files/jobs/event/batch.properties"
 end
-
 remote_file "/data/jobs/event/log4j.properties" do
   source "http://yum.ihr/files/jobs/event/log4j.properties"
+end
+cron_d "event_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a event-job \"/usr/bin/java -jar /data/jobs/event/event_job.jar launch-context.xml eventJob rundate=`/bin/date +\\%s`\""
+  minute 30
+  hour 5
 end
 
 directory "/data/jobs/playlog"
@@ -54,6 +56,108 @@ end
 cron_d "playlog_job" do
   command "/usr/bin/cronwrap use1b-jobserver101a Playlog-ETL-Job \"/data/jobs/playlog/playlog_wrapper.sh\""
   minute 22
+end
+
+directory "/data/jobs/profile"
+directory "/data/log/profile"
+remote_file "/data/jobs/profile/profile_job.jar" do
+  source "http://yum.ihr/files/jobs/profile/profile_job.jar"
+end
+cron_d "profile_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a profile-job \"/usr/bin/java -jar /data/jobs/profile/profile_job.jar launch-context.xml profileJob rundate=`/bin/date +\\%s`\""
+  minute 30
+  hour 3
+end
+
+directory "/data/jobs/live_thumbs"
+directory "/data/log/liveradiothumbslog"
+directory "/data/log/liveradiothumbslog/processed"
+remote_file "/data/jobs/live_thumbs/live_thumbs_job.jar" do
+  source "http://yum.ihr/files/jobs/live_thumbs/live_thumbs_job.jar"
+end
+cron_d "live_thumbs_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a Liveradio-Thumb-ETL-Job \"/usr/bin/java -jar /data/jobs/live_thumbs/live_thumbs_job.jar launch-context.xml liveradiothumbslogJob rundate=`/bin/date +\\%s`\""
+  minute 51
+end
+
+directory "/data/jobs/custom_thumbs"
+directory "/data/log/customradiothumbslog"
+directory "/data/log/customradiothumbslog/processed"
+remote_file "/data/jobs/custom_thumbs/custom_thumbs_job.jar" do
+  source "http://yum.ihr/files/jobs/custom_thumbs/custom_thumbs_job.jar"
+end
+cron_d "custom_thumbs_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a Customradio-Thumb-ETL-Job \"/usr/bin/java -jar /data/jobs/custom_thumbs/custom_thumbs_job.jar launch-context.xml customradiothumbslogJob rundate=`/bin/date +\\%s`\""
+  minute 38
+end
+
+directory "/data/jobs/talk_thumbs"
+directory "/data/log/talkthumbslog"
+directory "/data/log/talkthumbslog/processed"
+remote_file "/data/jobs/talk_thumbs/talk_thumbs_job.jar" do
+  source "http://yum.ihr/files/jobs/talk_thumbs/talk_thumbs_job.jar"
+end
+cron_d "talk_thumbs_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a Talk-Thumb-Radio-ETL-Job \"/usr/bin/java -jar /data/jobs/talk_thumbs/talk_thumbs_job.jar launch-context.xml talkthumbslogJob rundate=`/bin/date +\\%s`\""
+  minute 41
+end
+
+directory "/data/jobs/skiplog"
+directory "/data/log/skiplog"
+directory "/data/log/skiplog/processed"
+remote_file "/data/jobs/skiplog/skiplog_job.jar" do
+  source "http://yum.ihr/files/jobs/skiplog/skiplog_job.jar"
+end
+remote_file "/data/jobs/skiplog/log4j.properties" do
+  source "http://yum.ihr/files/jobs/skiplog/log4j.properties"
+end
+remote_file "/data/jobs/skiplog/skipbatch.properties" do
+  source "http://yum.ihr/files/jobs/skiplog/skipbatch.properties"
+end
+cron_d "skiplog_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a Skiplog-ETL-Job \"/usr/bin/java -jar /data/jobs/skiplog/skiplog_job.jar launch-context.xml skiplogJob rundate=`/bin/date +\\%s`\""
+  minute 17
+end
+
+directory "/data/jobs/talklog"
+directory "/data/log/talkplaylog"
+directory "/data/log/talkplaylog/processed"
+remote_file "/data/jobs/talklog/talklog_job.jar" do
+  source "http://yum.ihr/files/jobs/talklog/talklog_job.jar"
+end
+remote_file "/data/jobs/talklog/log4j.properties" do
+  source "http://yum.ihr/files/jobs/talklog/log4j.properties"
+end
+remote_file "/data/jobs/talklog/skipbatch.properties" do
+  source "http://yum.ihr/files/jobs/talklog/skipbatch.properties"
+end
+remote_file "/data/jobs/talklog/talkbatch.properties" do
+  source "http://yum.ihr/files/jobs/talklog/talkbatch.properties"
+end
+cron_d "talklog_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a Talklog-ETL-Job \"/usr/bin/java -jar /data/jobs/talklog/talklog_job.jar launch-context.xml talkJob rundate=`/bin/date +\\%s`\""
+  minute 21
+end
+
+directory "/data/jobs/sysinfo"
+directory "/data/log/sysinfo"
+directory "/data/log/sysinfo/input" do
+  mode 0777
+end
+directory "/data/log/sysinfo/processed"
+remote_file "/data/jobs/sysinfo/sysinfo_job.jar" do
+  source "http://yum.ihr/files/jobs/sysinfo/sysinfo_job.jar"
+end
+remote_file "/data/jobs/sysinfo/batch.properties" do
+  source "http://yum.ihr/files/jobs/sysinfo/batch.properties"
+end
+remote_file "/data/jobs/sysinfo/log4j.properties" do
+  source "http://yum.ihr/files/jobs/sysinfo/log4j.properties"
+end
+cron_d "sysinfo_job" do
+  command "/usr/bin/cronwrap use1b-jobserver101a sysinfo-job \"/usr/bin/java -jar /data/jobs/sysinfo/sysinfo_job.jar launch-context.xml sysInfoJob rundate=`/bin/date +\\%s`\""
+  minute 30
+  hour 5
 end
 
 directory "/home/amqp-consumer/playlog-consumer" do
@@ -218,7 +322,7 @@ cookbook_file "/data/jobs/log-archive.sh" do
 end
 cron_d "archive_logs" do
   command "/usr/bin/cronwrap use1b-jobserver101a Archive-Logs \"/data/jobs/log-archive.sh 2>&1 >> /dev/null\""
-  minute "*/15"
+  minute 15
 end
 
 #apis = search(:node, "role:amp AND chef_environment:#{node.chef_environment}")
@@ -255,7 +359,6 @@ directory "/data/log/radiomigration" do
   owner "ihr-deployer"
   group "ihr-deployer"
 end
-
 directory "/data/jobs/radiomigration/data" do
   owner "ihr-deployer"
   group "ihr-deployer"
