696e52c29e3b25955e5160d7a855eec846773b38
Jake Plimack <jake.plimack@gmail.com>
2013-05-17 20:56:36 +0000

    facebook needs mongosd

--- roles/mongosd.rb
+++ roles/mongosd.rb
@@ -1,10 +1,10 @@
 name "mongosd"
 description "mongosd"
 all_env = [
-	   "recipe[mongodb::mongosd]",
-	   "recipe[mongodb::ulimits]",
+           "recipe[mongodb::mongosd]",
+           "recipe[mongodb::ulimits]",
            "recipe[mongodb::nagios]"
-	  ]
+          ]
 run_list(all_env)
 env_run_lists(
               "_default" => all_env,
@@ -13,8 +13,13 @@ env_run_lists(
               "ec2" => all_env
               )
 default_attributes({
-                     "mongodb" => {
-                      }
+                     "mongosd" => {
+                       "mongosconfigdbs" => [
+                                             "iad-mongo-usr101-v240.ihr:57017",
+                                             "iad-mongo-usr103-v240.ihr:57017",
+                                             "iad-mongo-usr105-v240.ihr:57017"
+                                            ]
+                     }
                    })
 override_attributes({
                     })
