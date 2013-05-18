87717caca49c70a0a177d24c4753455a162e4788
Jake Plimack <jake.plimack@gmail.com>
2013-05-18 21:02:52 +0000

    fix dep so we can have a new bastion

--- cookbooks/selinux/attributes/default.rb
+++ cookbooks/selinux/attributes/default.rb
@@ -6,3 +6,5 @@ case node['platform']
 when "centos", "redhat", "fedora"
   default[:selinux][:packages] = %w{ policycoreutils setroubleshoot selinux-policy-targeted selinux-policy libselinux libselinux-python libselinux-utils policycoreutils policycoreutils-python setroubleshoot setroubleshoot-server setroubleshoot-plugins }
 end
+
+default[:selinux][:packages] = %w{}
