534ded00009640b3edca018f4154672be05f9d95
Mark Rechler <mrechler@iheart.com>
2013-05-21 09:43:03 -0400

    add stationlikes job to jobserver

--- cookbooks/etl_jobs/recipes/ec2.rb
+++ cookbooks/etl_jobs/recipes/ec2.rb
@@ -374,3 +374,19 @@ cron_d "radiomigration" do
   hour 21
   user 'ihr-deployer'
 end
+
+directory "/data/jobs/stationlikes"
+directory "/data/log/stationlikes"
+bash "extract-stationlikes" do
+  cwd "/data/jobs/stationlikes"
+  code "tar xpf stationlikes.tar.gz"
+  action :nothing
+end
+remote_file "/data/jobs/stationlikes/stationlikes.tar.gz" do
+  action :create_if_missing
+  notifies :run, 'bash[extract-stationlikes]', :immediately
+end
+cron_d "stationlikes" do
+  command "/data/jobs/stationlikes/ReadFBData.sh"
+  minute 3
+end
