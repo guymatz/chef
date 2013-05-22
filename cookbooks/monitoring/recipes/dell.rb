fdfa9db2c39bff29be59ea82eee00d1a8daa9198
Jake Plimack <jake.plimack@gmail.com>
2013-05-22 16:03:53 +0000

    dell snmp

--- cookbooks/monitoring/recipes/dell.rb
+++ cookbooks/monitoring/recipes/dell.rb
@@ -41,3 +41,12 @@ end
 unless node.run_list.include?("role[dell]")
   node.run_list << "role[dell]"
 end
+
+
+ruby_block "allow dell snmp" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/snmp/snmpd.conf")
+    file.insert_line_if_no_match("/smuxpeer .1.3.6.1.4.1.674.10892.1/", "#Allow Systems Management Data Engine SNMP to connect to snmpd using SMUX\nsmuxpeer .1.3.6.1.4.1.674.10892.1")
+    file.write_file
+  end
+end
