1ecaf1b902198eb073c14238c879939d30205005
John Deragon <jderagon@clearchannel.com>
2013-05-22 03:45:21 +0000

    changed nrpe check for tomcat as it was getting false positives

--- cookbooks/imgscaler/recipes/nrpe.rb
+++ cookbooks/imgscaler/recipes/nrpe.rb
@@ -1,6 +1,6 @@
 # imgscaler nrpe checks
 
 nagios_nrpecheck "check_app_proc_tomcat" do
-  command "#{node['nagios']['plugin_dir']}/check_procs -a 'tomcat'"
+  command "#{node['nagios']['plugin_dir']}/check_procs -a 'catalina.home=/usr/local/tomcat7'"
   critical_condition "1:1"
 end
