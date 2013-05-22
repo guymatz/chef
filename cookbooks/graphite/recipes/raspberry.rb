49519b977decb9fe2b0b19d9aac118935a166867
John Deragon <jderagon@clearchannel.com>
2013-05-22 01:41:35 +0000

    more log cleanup

--- cookbooks/graphite/recipes/raspberry.rb
+++ cookbooks/graphite/recipes/raspberry.rb
@@ -5,28 +5,7 @@
 # Installs IHR custom web landing page for our graphite servers
 #
 # drop a github private deploy key for status-deploy
-deploy_keys = Chef::EncryptedDataBagItem.load("keys", "status-deploy")
-directory "/root/.ssh" do
-    recursive true
-    mode "0700"
-end
-
 package "gnu-free-sans-fonts"
-
-file "/root/.ssh/deploy" do
-    mode "0400"
-    content deploy_keys['private_key']
-    :create_if_missing
-end
-
-file "/root/.ssh/config" do
-    mode "0755"
-    content <<-EOH
-        Host *github.com *.ihr iad-search*
-        IdentityFile "/root/.ssh/deploy"
-        StrictHostKeyChecking no
-    EOH
-end
 
 git node[:graphite][:web_install_path] do
     repository node[:graphite][:repo]
