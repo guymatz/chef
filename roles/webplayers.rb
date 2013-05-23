065bc17222ce9616c87b99649a37185f2806e250
Jake Plimack <jake.plimack@gmail.com>
2013-05-23 16:16:42 +0000

    Split webplayer config per environment * replaced json with ruby role file

---
+++ roles/webplayers.rb
@@ -0,0 +1,17 @@
+name "webplayers"
+description "webplayers"
+all_env = [
+           "recipe[webplayer]"
+          ]
+run_list(all_env)
+env_run_lists(
+              "_default" => all_env,
+              "qa2" => all_env,
+              "prod" => all_env + ["role[auto-bonded]"],
+              "ec2" => all_env,
+              "ec2-prod" => all_env
+              )
+default_attributes "ganglia" => {
+  "gmond_port" => "8650",
+  "cluster_name" => "webplayers"
+}
