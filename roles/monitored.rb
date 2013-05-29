5c33cc4644c48f4402f506fb56cbe55bbc8abb40
Mark Rechler <mrechler@iheart.com>
2013-05-29 10:44:53 +0000

    remove rsyslog from monitored roled

--- roles/monitored.rb
+++ roles/monitored.rb
@@ -2,7 +2,6 @@ name "monitored"
 description "Sets up Nagios, Rsyslog, Ganglia"
 run_list(
          "recipe[nagios]",
-         "recipe[rsyslog::client]",
          "recipe[diamond]",
          "recipe[monitoring]",
          "recipe[snmp]"
