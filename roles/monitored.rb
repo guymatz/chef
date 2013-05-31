a4871e8552b43370b40bd498094f77eea533d033
Jake Plimack <jake.plimack@gmail.com>
2013-05-31 14:53:06 +0000

    Revert "remove rsyslog from monitored roled"
    This reverts commit 5c33cc4644c48f4402f506fb56cbe55bbc8abb40.

--- roles/monitored.rb
+++ roles/monitored.rb
@@ -2,6 +2,7 @@ name "monitored"
 description "Sets up Nagios, Rsyslog, Ganglia"
 run_list(
          "recipe[nagios]",
+         "recipe[rsyslog::client]",
          "recipe[diamond]",
          "recipe[monitoring]",
          "recipe[snmp]"
