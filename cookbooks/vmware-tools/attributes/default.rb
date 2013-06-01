d7458319d3f394c9f74ff312c2f1480d0422952f
Jake Plimack <jake.plimack@gmail.com>
2013-06-01 03:07:59 +0000

    make vmwaretools debian compliant

--- cookbooks/vmware-tools/attributes/default.rb
+++ cookbooks/vmware-tools/attributes/default.rb
@@ -4,5 +4,9 @@
 #
 # Written by Jake Plimack <jake.plimack@gmail.com>
 
-
-default[:vmwaretools][:packages] = %w{ vmware-tools-core }
+case node[:platform_family]
+when "rhel"
+  default[:vmwaretools][:packages] = %w{ vmware-tools-core }
+when "debian"
+  default[:vmwaretools][:packages] = %w{ }
+end
