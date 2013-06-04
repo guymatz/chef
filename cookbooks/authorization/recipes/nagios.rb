92c69911b85e876dcb4874fe31dc00c364c15659
Joseph Hammerman <josephhammerman@clearchannel.com>
2013-06-04 20:39:33 +0000

    Added conneciton percentage checks for DB's

--- cookbooks/authorization/recipes/nagios.rb
+++ cookbooks/authorization/recipes/nagios.rb
@@ -31,3 +31,12 @@ nagios_nrpecheck "Postgres-TxnWraparound-VIP" do
   action :add
   notifies :restart, resources(:service => "nagios-nrpe-server")
 end
+
+nagios_nrpecheck "Postgres-Connections-VIP" do
+  command "#{node['nagios']['plugin_dir']}/check_postgres.pl"
+  parameters "--action=backends"
+  warning_condition "80%"
+  critical_condition "90%"
+  action :add
+  notifies :restart, resources(:service => "nagios-nrpe-server")
+end
