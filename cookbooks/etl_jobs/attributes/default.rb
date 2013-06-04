4ce9e295477ef7e6eec99f5859ba3a88e6e991a8
Jake Plimack <jake.plimack@gmail.com>
2013-06-04 20:29:24 +0000

    dont tar the tars

--- cookbooks/etl_jobs/attributes/default.rb
+++ cookbooks/etl_jobs/attributes/default.rb
@@ -2,7 +2,8 @@
 
 
 default[:enrichment][:appdir] = "/data/apps/enrichment"
-default[:enrichment][:log][:source] = "/data/log/enrichment-consumer"
+default[:enrichment][:log][:source] = "/data/log/enrichment-consumer/enrichmentMisses.txt.*"
 default[:enrichment][:log][:dest] = "iad-ss-nfs101-v600.ihr:/data/exports/files.ihrdev.com/Enrichment"
 default[:enrichment][:log][:retention] = "14" # days
 default[:enrichment][:log][:user] = "ihr-deployer"
+
