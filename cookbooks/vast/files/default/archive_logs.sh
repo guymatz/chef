3d56057c0ce24f703e143812ca4c9f4f2d6c6340
Mark Rechler <mrechler@iheart.com>
2013-06-10 18:54:10 +0000

    fix for archive

--- cookbooks/vast/files/default/archive_logs.sh
+++ cookbooks/vast/files/default/archive_logs.sh
@@ -3,4 +3,4 @@ FILES=$(find /data/apps/tomcat7/logs -name 'localhost_access_log*' -mtime +1)
 cd /data/apps/tomcat7/logs
 tar czpf localhost.access.log.archive.$(date +%Y%m%d).tar.gz $FILES
 rm -f $FILES
-find /data/apps/tomcat7/logs -name 'localhost_access_log*' -mtime +10 -exec rm {} \;
+find /data/apps/tomcat7/logs -name 'localhost.access.log.archive*' -mtime +10 -exec rm {} \;
