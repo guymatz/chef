edcf6077f56da27f567577b6e7397e514e8bde7d
Mark Rechler <mrechler@iheart.com>
2013-06-10 18:52:04 +0000

    more archive tweaks

--- cookbooks/vast/files/default/archive_logs.sh
+++ cookbooks/vast/files/default/archive_logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 FILES=$(find /data/apps/tomcat7/logs -name 'localhost_access_log*' -mtime +1)
 cd /data/apps/tomcat7/logs
-tar czpf localhost.access.log.archive.$(date +%s).tar.gz $FILES
+tar czpf localhost.access.log.archive.$(date +%Y%m%d).tar.gz $FILES
+rm -f $FILES
 find /data/apps/tomcat7/logs -name 'localhost_access_log*' -mtime +10 -exec rm {} \;
