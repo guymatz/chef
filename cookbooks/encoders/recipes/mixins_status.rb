fbae1f27574b97cd202444b41c1c9d7878115502
John Deragon <jderagon@clearchannel.com>
2013-06-18 21:21:28 +0000

    mixins crontab

---
+++ cookbooks/encoders/recipes/mixins_status.rb
@@ -0,0 +1,19 @@
+include_recipe "logrotate"
+
+begin
+  unless tagged?("mixins-deployed")
+
+      cron "mixin_status" do
+        command "/data/apps/converter/status/mixin.sh > /dev/null 2>&1"
+        minute  "3"
+        hour    "*"
+        day     "*"
+        month   "*"
+        weekday "*"
+    end
+
+   tag("mixins-deployed")
+   end
+rescue
+    untag("talk-deployed")
+end
