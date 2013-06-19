142b6551fcb2147d66f3bb61b6644e6df1aea463
John Deragon <jderagon@clearchannel.com>
2013-06-19 19:05:39 +0000

    more testing

--- cookbooks/encoders/Strainerfile.rb
+++
@@ -1,3 +0,0 @@
-knife:       bundle exec knife cookbook test $COOKBOOK
-foodcritic:  bundle exec foodcritic $SANDBOX/$COOKBOOK -f any
-rspec:       (cd $COOKBOOK && bundle exec rspec --color --format documentation)
