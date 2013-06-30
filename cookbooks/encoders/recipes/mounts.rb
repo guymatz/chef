3d41d4c42081bc04edb69d3e4c28eca7ee08f667
John Deragon <jderagon@clearchannel.com>
2013-06-30 01:21:18 +0000

    removed isilon for now

--- cookbooks/encoders/recipes/mounts.rb
+++ cookbooks/encoders/recipes/mounts.rb
@@ -1,30 +1,30 @@
 begin
   unless tagged?("encoder-mounts")
 
-    directory node[:encoders][:ftp_mount_dir] do
-        owner "root"
-        group "converter"
-        not_if do FileTest.directory?(node[:encoders][:ftp_mount_dir]) end
-    end
-
-    directory node[:encoders][:p_content_dir] do
-        owner "root"
-        group "converter"
-        not_if do FileTest.directory?(node[:encoders][:p_content_dir]) end
-    end
-    mount node[:encoders][:ftp_mount_dir] do
-        device "#{node[:encoders][:isilon_server]}:#{node[:encoders][:ftp_mount_export]}"
-        fstype "nfs"
-        options "rw"
-        action [:mount, :enable]
-    end
-
-    mount node[:encoders][:p_content_dir] do
-        device "#{node[:encoders][:isilon_server]}:#{node[:encoders][:p_content_export]}"
-        fstype "nfs"
-        options "rw"
-        action [:mount, :enable]
-    end
+#    directory node[:encoders][:ftp_mount_dir] do
+#        owner "root"
+#        group "converter"
+#        not_if do FileTest.directory?(node[:encoders][:ftp_mount_dir]) end
+#    end
+#
+#    directory node[:encoders][:p_content_dir] do
+#        owner "root"
+#        group "converter"
+#        not_if do FileTest.directory?(node[:encoders][:p_content_dir]) end
+#    end
+#    mount node[:encoders][:ftp_mount_dir] do
+#        device "#{node[:encoders][:isilon_server]}:#{node[:encoders][:ftp_mount_export]}"
+#        fstype "nfs"
+#        options "rw"
+#        action [:mount, :enable]
+#    end
+#
+#    mount node[:encoders][:p_content_dir] do
+#        device "#{node[:encoders][:isilon_server]}:#{node[:encoders][:p_content_export]}"
+#        fstype "nfs"
+#        options "rw"
+#        action [:mount, :enable]
+#    end
 
     tag("encoder-mounts")
     end
