8b32b66e627690f63410043c0521a98bbc5650a0
Mark Rechler <mrechler@iheart.com>
2013-07-02 19:29:29 +0000

    format syslog logrotate rule better

--- cookbooks/logrotate/recipes/syslog.rb
+++ cookbooks/logrotate/recipes/syslog.rb
@@ -1,14 +1,12 @@
 logrotate_app "syslog" do
-  path  <<-eos
-          /var/log/syslog
-          /var/log/cron 
-          /var/log/maillog
-          /var/log/messages
-          /var/log/secure
-          /var/log/spooler
-          /var/log/daemon.log
-          /var/log/mail.*"
-        eos
+  path '/var/log/syslog '\
+       '/var/log/cron '\
+       '/var/log/maillog '\
+       '/var/log/messages '\
+       '/var/log/secure '\
+       '/var/log/spooler '\
+       '/var/log/daemon.log '\
+       '/var/log/mail.*'
   options ["sharedscripts", "compress"]
   postrotate "/bin/kill -HUP `cat /var/run/syslogd.pid 2> /dev/null` 2> /dev/null || true"
   frequency "daily"
