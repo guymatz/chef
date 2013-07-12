06315e4147e4595870602aaa620ecd1abbb4074b
Mark Rechler <mrechler@iheart.com>
2013-07-12 09:19:07 -0400

    remove more sflow

--- cookbooks/host-sflow/recipes/default.rb
+++ cookbooks/host-sflow/recipes/default.rb
@@ -7,24 +7,24 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node[:hsflow][:packages].each do |pkg|
-  package pkg
-end
-
-collectors = node[:hsflow][:collectors]
-
-template "/etc/hsflowd.conf" do
-  source "hsflowd.conf.erb"
-  owner "root"
-  group "root"
-  mode "0755"
-  variables({
-              :collectors => collectors
-            })
-  notifies :restart, "service[hsflowd]"
-end
-
-service "hsflowd" do
-  supports :start => true, :stop => true, :restart => true
-  action [:enable, :start]
-end
+#node[:hsflow][:packages].each do |pkg|
+#  package pkg
+#end
+#
+#collectors = node[:hsflow][:collectors]
+#
+#template "/etc/hsflowd.conf" do
+#  source "hsflowd.conf.erb"
+#  owner "root"
+#  group "root"
+#  mode "0755"
+#  variables({
+#              :collectors => collectors
+#            })
+#  notifies :restart, "service[hsflowd]"
+#end
+#
+#service "hsflowd" do
+#  supports :start => true, :stop => true, :restart => true
+#  action [:enable, :start]
+#end
