e4c573129f6333338d9ff3c3621a92a48a36ed67
Mark Rechler <mrechler@iheart.com>
2013-07-19 16:00:19 +0000

    fix syslog typo

--- cookbooks/logrotate/recipes/syslog.rb
+++ cookbooks/logrotate/recipes/syslog.rb
@@ -10,5 +10,5 @@ logrotate_app "syslog" do
   options ["sharedscripts", "compress"]
   postrotate "/bin/kill -HUP `cat /var/run/syslogd.pid 2> /dev/null` 2> /dev/null || true"
   frequency "daily"
-  roate 7
+  rotate 7
 end
