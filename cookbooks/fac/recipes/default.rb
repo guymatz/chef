5374c0c495ffc71cd9b47aad8eafc426513de5f4
Joseph Hammerman <josephhammerman@clearchannel.com>
2013-07-22 21:49:24 +0000

    Added /etc/hosts entries for QA2 to FAC cookbook, changed encoder filemonitor.sh restart behavior

--- cookbooks/fac/recipes/default.rb
+++ cookbooks/fac/recipes/default.rb
@@ -18,6 +18,8 @@ hosts["mongodb-fac1b01"] = "10.90.47.182"
 hosts["mongodb-fac1b02"] = "10.90.47.183"
 hosts["mongodb-fac1b03"] = "10.90.47.184"
 hosts["mongodb-fac1b04"] = "10.90.47.185"
+hosts["qa2-mongodb-fac0a01.ccrd.clearchannel.com"] = "10.9.176.31"
+hosts["qa2-mongodb-fac0a02.ccrd.clearchannel.com"] = "10.9.176.32"
 hosts.each do |host,ip|
   hostsfile_entry ip do
     hostname host
