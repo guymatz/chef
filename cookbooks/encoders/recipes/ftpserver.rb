ec21a84f342e654ae07eb7cbf0d0e52b7a318354
John Deragon <jderagon@clearchannel.com>
2013-07-23 13:13:48 +0000

    new users, rmemoved bad mounts

--- cookbooks/encoders/recipes/ftpserver.rb
+++ cookbooks/encoders/recipes/ftpserver.rb
@@ -10,7 +10,7 @@
 
 begin
   unless tagged?("encoder-ftp-deployed")
-    ftp_mount_line = "#{node[:encoders][:nfsserver]}:/nfs#{node[:encoders][:ftp_mount]} #{node[:encoders][:ftp_mount]}       nfs   rw,vers=3,bg,soft,tcp,intr  0   0"
+#    ftp_mount_line = "#{node[:encoders][:nfsserver]}:/nfs#{node[:encoders][:ftp_mount]} #{node[:encoders][:ftp_mount]}       nfs   rw,vers=3,bg,soft,tcp,intr  0   0"
 
 
     service "vsftpd" do
@@ -18,21 +18,21 @@ begin
         supports :status => true, :start => true, :stop => true, :restart => true
     end
 
-    execute "mkdirs" do
-        command "mkdir -p #{node[:encoders][:ftp_mount]}"
-    end
+#    execute "mkdirs" do
+#        command "mkdir -p #{node[:encoders][:ftp_mount]}"
+#    end
+##
+#    execute "mounts" do
+#        command "/bin/mount -a"
+#        action :run
+#    end
 
-    execute "mounts" do
-        command "/bin/mount -a"
-        action :run
-    end
-
-    append_if_no_line "encoder_ftp" do
-        path "/etc/fstab"
-        line ftp_mount_line
-        notifies :run, "execute[mkdirs]", :immediately
-        notifies :run, "execute[mounts]", :immediately
-    end
+#    append_if_no_line "encoder_ftp" do
+#        path "/etc/fstab"
+#        line ftp_mount_line
+#        notifies :run, "execute[mkdirs]", :immediately
+#        notifies :run, "execute[mounts]", :immediately
+#    end
 
     node[:ftpserver].each do |pkg|
         yum_package pkg do
@@ -78,7 +78,7 @@ begin
     bash "createdb" do
         code <<-EOF
             /usr/bin/db_load -T -t hash -f /etc/vsftpd/vuser/music_users.txt /etc/vsftpd/vuser/music_vusers.db
-            rm -f /etc/vsftpd/vuser/music_users.txt
+            #rm -f /etc/vsftpd/vuser/music_users.txt
         EOF
     end
 
