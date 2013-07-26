310151ac3f301783faf2326f8818954f0876ee29
Gregory Patmore <gregorypatmore@gmail.com>
2013-07-26 13:45:59 -0400

    Added cnpuser group with sudo permissions REF:OPS-4601

---
+++ cookbooks/users/recipes/cnpuser.rb
@@ -0,0 +1,19 @@
+# #######################################################
+# @CREATED 7/26/13 
+# @AUTHOR Gregory Patmore
+# @CONTACT gregorypatmore@clearchannel.com
+# @DESC CNP Users Group
+# @REF https://jira.ccrd.clearchannel.com/browse/OPS-4601
+# 
+# #######################################################
+users_manage "cnpuser" do
+  group_id 5039
+  action [ :remove, :create ]
+end
+
+sudo "cnpuser" do
+  group "cnpuser"
+  commands ["ALL"]
+  runas "root"
+  nopasswd true
+end
