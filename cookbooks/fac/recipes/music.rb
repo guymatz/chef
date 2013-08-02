cfc9e86a735b334ccdf3f2289df1fdc78f1af50a
Mark Rechler <mrechler@iheart.com>
2013-08-02 15:25:46 +0000

    fix cronwrap host for fac-music

--- cookbooks/fac/recipes/music.rb
+++ cookbooks/fac/recipes/music.rb
@@ -85,6 +85,6 @@ cron_d "fac-music" do
  minute "2"
  hour "0"
  weekday "2" # tuesday
- command "/usr/bin/cronwrap use1b-jobserver101a fac-music \"#{script_dir}/fac-incremental-runner.sh\""
+ command "/usr/bin/cronwrap iad-jobserver101a fac-music \"#{script_dir}/fac-incremental-runner.sh\""
  user "root"
 end
