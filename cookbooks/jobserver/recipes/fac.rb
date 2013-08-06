5601682f380b0b8cc915aecd4b5ccc1ea66bb9b6
Guy Matz <guymatz@clearchannel.com>
2013-08-06 17:19:36 -0400

    added fac::sherpa

--- cookbooks/jobserver/recipes/fac.rb
+++ cookbooks/jobserver/recipes/fac.rb
@@ -9,6 +9,6 @@
 # Description: Sets up jobs for FAC-PRN, fac-talk, and fac-music
 #              allong with necessary monitoring for the processes and jobs
 
-%w{ default prn music talk radiobuild updatestream }.each do |r|
+%w{ default prn music talk radiobuild updatestream sherpa }.each do |r|
   include_recipe "fac::#{r}"
 end
