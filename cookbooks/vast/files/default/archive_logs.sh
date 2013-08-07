dc6b641310d196d6b81bcaf5972edc79a9b10c1e
Gregory Patmore <gregorypatmore@gmail.com>
2013-08-07 14:09:12 -0400

    rewrite to fix invalid path glob and make one tar file per log file.

--- cookbooks/vast/files/default/archive_logs.sh
+++ cookbooks/vast/files/default/archive_logs.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
-FILES=$(find /data/apps/tomcat7/logs -name 'localhost_access_log*' -mtime +1)
-cd /data/apps/tomcat7/logs
-tar czpf localhost.access.log.archive.$(date +%Y%m%d).tar.gz $FILES
-rm -f $FILES
-find /data/apps/tomcat7/logs -name 'localhost.access.log.archive*' -mtime +10 -exec rm {} \;
+# #########################################
+# @FILE archive_logs.sh
+# @AUTHOR Gregory Patmore
+# @CONTACT gregorypatmore@clearchannel.com
+# @DESC Script to manage localhost_access_log files for vast. archiving recent past files, and deleting older out of date archive files.
+#
+# #########################################
+FILES=$(find /data/apps/tomcat7/logs -name 'localhost_access_log.*-*-*.txt' -mtime +1)
+echo "INFO: [$(date)] $0 : Targetting (${#FILES[@]}) files for compression";
+
+pushd /data/apps/tomcat7/logs
+for f in ${FILES}; do
+	# tar up each file 
+	tar czvpf $f.tar.gz $f;
+
+	# verify the file exists and, if so, remove the log file. else complain
+	[[ $? -eq 0 ]] \
+		&& ( [[ -f $f.tar.gz ]] && rm -fv $f ) \
+		|| echo "Error: [$(date)] $0 : Failed to compress file ($f)" >&2;
+done;
+popd #/data/apps/tomcat7/logs
+
+REMOVE=$(find /data/apps/tomcat7/logs -name 'localhost_access_log.*-*-*.txt*.gz' -mtime +10);
+echo "INFO: [$(date)] $0 : Targetting (${#REMOVE[@]}) compressed files for deletion";
+
+for f in ${REMOVE}; do
+	rm -fv $f
+done;
