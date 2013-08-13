9f8adf64a6306873ae59d3fc4341024f057fe76c
Guy Matz <guymatz@clearchannel.com>
2013-08-13 12:56:12 -0400

    files are getting created, but not with the variables set

--- cookbooks/encoders/recipes/talk_manager.rb
+++ cookbooks/encoders/recipes/talk_manager.rb
@@ -11,64 +11,64 @@
 begin
   unless tagged?("talk-manager")
 
-##@#    node[:encoders][:talk][:manager][:startup_scripts].each do |script,tplate|
-##@#      template script do
-##@#        source tplate
-##@#        owner "root"
-##@#        mode "0755"
-##@#        variables({
-##@#          :num_converters => node[:talk_scanner][:num_processors]
-##@#        })
-##@#      end
-##@#      service script.gsub(/\/etc\/init.d\//, "") do
-##@#        action [:enable]
-##@#      end
-##@#    end
-##@#
-##@#    node[:encoders][:talk][:manager][:monitor_scripts].each do |srpt|
-##@#      log("1-----> looping through #{srpt}.")
-##@#      template "/usr/local/bin/#{srpt}.sh" do
-##@#        log(srpt)
-##@#        source "#{srpt}.sh"
-##@#        owner "root"
-##@#        mode "0755"
-##@#  #      variables({
-##@#  #        :num_scanners => node[:talk_scanners][:num_processors]
-##@#  #      })
-##@#      end
-##@#    end
-##@#  #    cron_d srpt do
-##@#  #      command "/usr/local/bin/#{srpt}.sh > /dev/null 2>&1"
-##@#  #      minute  "*/2"
-##@#  #      hour  "*"
-##@#  #      day   "*"
-##@#  #      month   "*"
-##@#  #      weekday "*"
-##@#  #      user "root"
-##@#  #    end
-##@#  #  end
-##@#
-##@#    cron_d "talk_stagnant_file_check" do
-##@#      command "/data/apps/converter/current/bin/talk_stagnant_file_check.sh > /dev/null 2>&1"
-##@#      minute  "0"
-##@#      hour  "*"
-##@#      day   "*"
-##@#      month   "*"
-##@#      weekday "*"
-##@#      user "root"
-##@#    end
-##@#
-##@#    cron_d "talk_automated_takedown" do
-##@#      command "/data/apps/converter/current/bin/talk_automated_takedown.sh > /dev/null 2>&1"
-##@#      minute  "0"
-##@#      hour  "21"
-##@#      day   "*"
-##@#      month   "*"
-##@#      weekday "0"
-##@#      user "root"
-##@#    end
-##@#
-     tag("talk-manager")
+    node[:encoders][:talk][:manager][:startup_scripts].each do |script,tplate|
+      template script do
+        source tplate
+        owner "root"
+        mode "0755"
+        variables({
+          :num_converters => node[:talk_scanner][:num_processors]
+        })
+      end
+      service script.gsub(/\/etc\/init.d\//, "") do
+        action [:enable]
+      end
+    end
+
+    node[:encoders][:talk][:manager][:monitor_scripts].each do |srpt|
+      #log("1-----> looping through #{srpt}.")
+      template "/usr/local/bin/#{srpt}.sh" do
+        #log(srpt)
+        source "#{srpt}.erb"
+        owner "root"
+        mode "0755"
+#        variables({
+#          :num_scanners => node[:talk_scanners][:num_processors]
+#        })
+      end
+    end
+  #    cron_d srpt do
+  #      command "/usr/local/bin/#{srpt}.sh > /dev/null 2>&1"
+  #      minute  "*/2"
+  #      hour  "*"
+  #      day   "*"
+  #      month   "*"
+  #      weekday "*"
+  #      user "root"
+  #    end
+  #  end
+
+    cron_d "talk_stagnant_file_check" do
+      command "/data/apps/converter/current/bin/talk_stagnant_file_check.sh > /dev/null 2>&1"
+      minute  "0"
+      hour  "*"
+      day   "*"
+      month   "*"
+      weekday "*"
+      user "root"
+    end
+
+    cron_d "talk_automated_takedown" do
+      command "/data/apps/converter/current/bin/talk_automated_takedown.sh > /dev/null 2>&1"
+      minute  "0"
+      hour  "21"
+      day   "*"
+      month   "*"
+      weekday "0"
+      user "root"
+    end
+
+#    tag("talk-manager")
   end
 rescue
   untag("talk-manager")
