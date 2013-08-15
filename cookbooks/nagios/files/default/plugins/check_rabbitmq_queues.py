7e79829b74ce2b4bc477a6bc9bc097a6375a6706
Mark Rechler <mrechler@iheart.com>
2013-08-15 11:47:48 -0400

    change file name, update comments

--- cookbooks/nagios/files/default/plugins/check_rabbitmq_queues.py
+++ cookbooks/nagios/files/default/plugins/check_rabbitmq_queues.py
@@ -11,16 +11,16 @@ parser.add_argument('--password', help='RabbitMQ auth password', required=True)
 parser.add_argument('--msgs', help='# of messages to alert on in any queue', required=True)
 args = parser.parse_args()
 
-# Grab the connection state json
+# Grab the queue state json
 rabbit_request = requests.get(args.url, auth=(args.user, args.password))
 bad_queues = []
 
-# Filter for not running connections
+# Filter for queues that have more messages than desired
 for queue in rabbit_request.json():
     if queue['messages'] > int(args.msgs):
         bad_queues.append(queue)
 
-# Output bad connections if they exist and exit with code 2 (critical)
+# Output bad queues if they exist and exit with code 2 (critical)
 if len(bad_queues):
     print "CRITICAL: The following queues are backed up!"
     for queue in bad_queues:
