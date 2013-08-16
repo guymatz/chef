e3a3c1210655e59a5d21b308d09f0166add57c87
Mark Rechler <mrechler@iheart.com>
2013-08-16 16:50:14 +0000

    setup ruby block to modify grub addition of noop

--- cookbooks/vmware-tools/recipes/noop.rb
+++ cookbooks/vmware-tools/recipes/noop.rb
@@ -1,9 +1,19 @@
 # Set noop as the IO scheduler for vmware guests
-if node.attribute?('virtualization') && node[:virtualization][:system] == 'vmware' && ode[:virtualization][:role] == 'guest'
+if node.attribute?('virtualization') && node[:virtualization][:system] == 'vmware' && node[:virtualization][:role] == 'guest'
   execute 'set-noop' do
-    command <<-EOH
-    echo noop > /sys/block/sda/queue/scheduler
-    sed -i 's/rhgb quiet\n/rhgb quiet elevator=noop\n/g' /boot/grub/menu.lst
-    EOH
+    command 'echo noop > /sys/block/sda/queue/scheduler'
+  end
+  ruby_block 'set grub noop' do
+    block do
+      grub_txt = Array.new
+      File.open('/boot/grub/menu.lst', 'r') {|f| grub_txt = f.readlines }
+      unless grub_txt.to_s.include?('elevator=noop')
+        grub_txt.each do |x|
+          puts x
+          grub_txt[grub_txt.index(x)] = x.gsub(/\n/, ' elevator=noop') if x.include?('kernel')
+        end
+        File.open('/boot/grub/menu.lst', 'w') {|f| grub_txt.each { |x| f.puts(x) }}
+      end
+    end
   end
 end
