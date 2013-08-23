0e3855766f5b6aaaf5f182dec10b6e30d1c98138
Gregory Patmore <gregorypatmore@gmail.com>
2013-08-23 14:17:10 -0400

    saving cronwrap replacement file

---
+++ cookbooks/cron/files/default/cronvisor.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+PATH=$PATH:/bin:/usr/local/bin:/usr/bin:/sbin
+export PATH
+
+
+
+
+if [ $# -lt 3 ]; then
+    echo
+    echo "   Usage:"
+    echo "   $0 host service \"quoted command\""
+    echo
+    exit 254
+fi
+
+
+# Which host are we supposed to run on?
+BATCH_IP=$(host $1 | awk '/has address/ {print $4}')
+
+# Does that IP even exist?
+# Possibly due to network issues, so don't make noise
+if [ -z $BATCH_IP ]; then
+    # echo "Invalid host to run on -- cannot resolve $1"
+    exit 150
+fi
+
+# IP exists. Do we have it?
+CONCH=$(ip addr show | egrep "${BATCH_IP}" | wc -l)
+if [ "$CONCH" -eq 0 ]; then
+    # Nope, we don't have it
+    exit 0
+fi
+
+TMPFILE="/var/tmp/cronwrap.$$"
+HOST=$1
+SERVICENAME=$2
+START=$(date +%s)
+/bin/bash -c "$3" >$TMPFILE.out 2>$TMPFILE.err
+RETVAL=$?        # one for our exit value
+NRETVAL=$RETVAL  # for nagios_passive
+END=$(date +%s)
+DURATION=$(echo $END - $START | bc)
+
+if [ $NRETVAL -gt 0 ]; then
+    STATUS="An unknown error has occurred (after running for $DURATION seconds)"
+    NRETVAL=1
+else
+    STATUS="OK - $2 ran in $DURATION seconds"
+fi
+
+CMD_ARGS=$(echo -e "$HOST\t$SERVICENAME\t$NRETVAL\t$STATUS")
+/usr/bin/nagios_passive.py $CMD_ARGS
+
+# send the actual script output to syslog.
+echo "$2 ran in $DURATION seconds (from $START to $END)" | logger -p cron.info -t $2
+cat $TMPFILE.out | logger -p cron.info -t $2
+cat $TMPFILE.err | logger -p cron.error -t $2
+
+rm $TMPFILE.out
+rm $TMPFILE.err
+
+exit $RETVAL
