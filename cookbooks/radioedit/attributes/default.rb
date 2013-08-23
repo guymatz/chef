f2d89541ce681ef8a935c691ca2db06e4a4bf82c
Gregory Patmore <gregorypatmore@gmail.com>
2013-08-23 14:47:37 -0400

    updated branch name to 'master'

--- cookbooks/radioedit/attributes/default.rb
+++ cookbooks/radioedit/attributes/default.rb
@@ -48,7 +48,7 @@ default[:radioedit][:nginx][:port] = 8000
 # epona settings
 default[:radioedit][:epona][:path] = "/data/apps/radioedit";
 default[:radioedit][:epona][:repo] = "git@github.ihrint.com:radioedit/core.git"
-default[:radioedit][:epona][:branch] = "deploy"
+default[:radioedit][:epona][:branch] = "master"
 default[:radioedit][:epona][:port] = 8000
 default[:radioedit][:epona][:host] = "unix"
 default[:radioedit][:epona][:packages] = %w{ python27 python27-libs python27-devel python27-test python27-tools nginx zlib-devel libjpeg zlib gcc python-devel git libevent-devel libevent zeromq-devel zeromq python-setuptools python-ldap postgresql-devel openldap-devel varnish readline-devel patch libjpeg-devel }
