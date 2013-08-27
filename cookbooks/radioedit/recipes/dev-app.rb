a9652c9bd995bb7d1470c8d69c550905b1169ff3
Gregory Patmore <gregorypatmore@gmail.com>
2013-08-27 13:00:57 -0400

    normalized memached src install

--- cookbooks/radioedit/recipes/dev-app.rb
+++ cookbooks/radioedit/recipes/dev-app.rb
@@ -40,22 +40,6 @@ yum_package "memcached" do
   action [ :install, :upgrade ]
 end
 
-# install libmemcached from source 
-# @TODO: change to rpm install 
-remote_file "/root/build/libmemcached-0.51.tar.gz" do
-  source "https://launchpad.net/libmemcached/1.0/0.51/+download/libmemcached-0.51.tar.gz"
-  notifies :run, "bash[install_libmemcached]", :immediately
-end
-
-bash "install_libmemcached" do
-  user "root"
-  cwd "/root/build"
-  code <<-EOH
-    tar -zxf libmemcached-0.51.tar.gz
-    (cd libmemcached-0.51/ && ./configure && make && make install)
-  EOH
-  action :nothing
-end
 
 python_virtualenv "#{node[:radioedit][:epona][:path]}/envs/radioedit" do
   interpreter "python27"
