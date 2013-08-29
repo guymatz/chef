8d5c1f2a25a2b7fef11b933abdc8e2be1fc8f879
John Deragon <jderagon@clearchannel.com>
2013-08-29 14:47:56 +0000

    graphite monitoring for 302's etc for newsletter

--- cookbooks/logster/attributes/default.rb
+++ cookbooks/logster/attributes/default.rb
@@ -1,7 +1,8 @@
 default[:logster][:static_files] = {
         "/usr/lib/python2.6/site-packages/logster-0.0.1-py2.6.egg/logster/parsers/ApacheLogster.py" => "ApacheLogster.py",
         "/usr/lib/python2.6/site-packages/logster-0.0.1-py2.6.egg/logster/parsers/Log4jIHR.py" => "Log4jIHR.py",
-        "/usr/lib/python2.6/site-packages/logster-0.0.1-py2.6.egg/logster/parsers/TomcatLogster.py" => "TomcatLogster.py"
+        "/usr/lib/python2.6/site-packages/logster-0.0.1-py2.6.egg/logster/parsers/TomcatLogster.py" => "TomcatLogster.py",
+        "/usr/lib/python2.6/site-packages/logster-0.0.1-py2.6.egg/logster/parsers/NewsLogster.py" => "NewsLogster.py"
 }
 
 default[:logster]["statefile_cron"] = { "minute" => "1", "hour" => "0", "path" => nil}
--- roles/news.rb
+++ roles/news.rb
@@ -2,5 +2,6 @@ name "news"
 description "News"
 run_list(
          "recipe[users::deployer]",
+         "recipe[graphite::news]"
          "recipe[news]"
 )
