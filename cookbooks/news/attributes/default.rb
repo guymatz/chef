c9c4aba1c8b2d28f2a79da174da988c00792de8c
John Deragon <jderagon@clearchannel.com>
2013-08-30 00:32:08 +0000

    change to allow connection max limit

--- cookbooks/news/attributes/default.rb
+++ cookbooks/news/attributes/default.rb
@@ -4,5 +4,6 @@ default[:news][:deployer]	='ihr-deployer'
 default[:news][:news_path]	='/data/apps/newsletter'
 default[:news][:repo]		='git@github.com:iheartradio/dfpemail.git'
 default[:news][:rev]		='8fb375edca63c04e2c503caa43d851add6354682'
-default[:news][:mongo]		=[ 'iad-mongo-news101-v240.ihr:37017', 'iad-mongo-news102-v240.ihr:37017' ]
+#default[:news][:mongo]		=[ 'iad-mongo-news101-v240.ihr:37017', 'iad-mongo-news102-v240.ihr:37017' ]
+default[:news][:mongo]		=[ 'iad-mongo-fac101-v240.ihr:37017', 'iad-mongo-fac102-v240.ihr:37017', 'iad-mongo-fac103-v240.ihr:37017' ]
 default[:news][:secret_key]	='RedaaakyanRiWyedViaaarrUch'
