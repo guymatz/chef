0eb3cdfa0aea8e73e956376f912064acc2493413
Guy Matz <guymatz@clearchannel.com>
2013-09-23 11:45:19 -0400

    relaying through cc.com relay for all cc.com bound mail.  All other mail being sent directly

--- cookbooks/sendmail/recipes/sendmail_relay.rb
+++ cookbooks/sendmail/recipes/sendmail_relay.rb
@@ -5,3 +5,11 @@ cookbook_file "/etc/mail/sendmail.mc" do
   group "root"
   notifies :restart, "service[sendmail]"
 end
+
+cookbook_file "/etc/mail/mailertable" do
+  source "mailertable"
+  mode 0640
+  owner "root"
+  group "root"
+  notifies :restart, "service[sendmail]"
+end
