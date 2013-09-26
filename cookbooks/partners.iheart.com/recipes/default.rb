49c6685bf655c06fd1d5914123a02b8a5f988b4b
Mark Rechler <mrechler@iheart.com>
2013-09-26 19:44:23 +0000

    tweak cookbook for django

--- cookbooks/partners.iheart.com/recipes/default.rb
+++ cookbooks/partners.iheart.com/recipes/default.rb
@@ -15,50 +15,30 @@
   package p
 end
 
-directory "/data/apps/partners/shared/venv" do
-  recursive true
-  owner node[:partners][:deployer]
-  group node[:partners][:group]
-  action :create
-end
+unless tagged?("partners-deployed")
+  application "partners" do
+    path "/data/apps/partners/"
+    owner node[:partners][:deployer]
+    group node[:partners][:group]
+    repository node[:partners][:repo]
+    revision node[:partners][:rev]
+    migrate false
+    shallow_clone false
 
-python_virtualenv "/data/apps/partners/shared/venv" do
-  interpreter "/usr/bin/python27"
-  owner node[:partners][:deployer]
-  group node[:partners][:group]
-  action :create   
-end
-
-    unless tagged?("partners-deployed")
-
-    application "partners" do
-      path "/data/apps/partners/"
-      owner node[:partners][:deployer]
-      group node[:partners][:group]
-      repository node[:partners][:repo]
-      revision node[:partners][:rev]
-      before_restart do
-        bash "setup venv" do
-          code <<-EOH
-          chown -R #{node[:partners][:deployer]}. #{node[:partners][:deploy_path]}
-          . /data/apps/partners/shared/venv/bin/activate && \
-          pip install -r "/data/apps/partners/shared/cached-copy/requirements.txt"
-          pip install gevent
-          EOH
-        end
-      end
-
-      gunicorn do
-        app_module "partners:app"
-        host "0.0.0.0"
-        port 8080
-        workers 9
-        worker_class "gevent"
-        virtualenv "/data/apps/partners/shared/venv"
-        autostart true
-        accesslog "/var/log/partners/partners-gunicorn-access.log"
-      end
+    django do
+      interpreter "python27"
+      requirements "requirements.txt"
     end
+
+    gunicorn do
+      app_module :django
+      worker_class "gevent"
+      port 8080
+      autostart true
+      workers 9
+      virtualenv "#{node[:partners][:deploy_path]}/shared/venv"
+    end
+  end
 
     directory "/var/log/partners" do
         owner 'root'
@@ -81,9 +61,3 @@ end
 
     tag("partners-deployed")
   end
-
-bash "setup venv" do
-      code <<-EOH
-      chown -R #{node[:partners][:user]}. #{node[:partners][:deploy_path]}
-      EOH
-end
