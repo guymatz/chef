7e43c9d7f182dc2a754a6cf841904983c0977763
Guy Matz <guymatz@clearchannel.com>
2013-09-27 14:22:03 -0400

    creating new user for sysadmin work

--- cookbooks/users/recipes/sysadmins.rb
+++ cookbooks/users/recipes/sysadmins.rb
@@ -21,6 +21,7 @@
 # Searches data bag "users" for groups attribute "sysadmin".
 # Places returned users in Unix group "sysadmin" with GID 2300.
 include_recipe "users::root"
+include_recipe "users::ihr-admin"
 users_manage "sysadmin" do
   group_id 2300
   action [ :remove, :create ]
