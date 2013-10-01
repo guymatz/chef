566b5726478717a89cbf08ee4c53e5c6517c6d5a
Guy Matz <guymatz@clearchannel.com>
2013-10-01 16:40:10 -0400

    changed admin user to ihr-deployer

--- cookbooks/ftp_admin/attributes/default.rb
+++ cookbooks/ftp_admin/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:ftp_admin][:user]		='ftp_admin'
-default[:ftp_admin][:group]		='ftp_admin'
+default[:ftp_admin][:user]		='ihr-deployer'
+default[:ftp_admin][:group]		='ihr-deployer'
 default[:ftp_admin][:deployer]	='ihr-deployer'
 default[:ftp_admin][:ftp_admin_path]	='/data/apps/ftp_admin'
 default[:ftp_admin][:repo]		='git@github.ihrint.com:OPS/ftp_admin.git'
