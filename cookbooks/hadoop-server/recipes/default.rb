0ab2df50c19d9d3262f41d2592a4d7a5c064d127
John Deragon <jderagon@clearchannel.com>
2013-10-15 16:39:23 +0000

    haddop init script

--- cookbooks/hadoop-server/recipes/default.rb
+++ cookbooks/hadoop-server/recipes/default.rb
@@ -1,5 +1,5 @@
 
-  template "/etc/init.d/S10hadoop-patch" do
+  template "/etc/rc.d/init.d/hadoop-patch" do
     source "S10hadoop-patch.erb"
     mode 0775
     variables(
@@ -8,5 +8,5 @@
 
 
   link "/etc/rc.d/rc5.d/S10hadoop-patch" do
-    to "/etc/init.d/S10hadoop-patch"
+    to "../init.d/hadoop-patch"
   end
