00056845dae566be83472ec3ae0caa9304046572
Aaron Clements <aaronclements@clearchannel.com>
2013-10-16 14:17:59 -0400

    Adding alert per OPS-5502

--- cookbooks/memcached/recipes/monitoring.rb
+++ cookbooks/memcached/recipes/monitoring.rb
@@ -3,3 +3,8 @@ node[:memcached][:monitoring][:packages].each do |p|
   package p
 end
 
+nagios_nrpecheck "Memcached_Machine_Memory" do
+  command "#{node['nagios']['plugin_dir']}/check_mem.sh"
+  parameters '-w 85 -c 90'
+  action :add
+end
