92a589c33e26de7e3441afecdda6dc11a1980d75
Aaron Clements <aaronclements@clearchannel.com>
2013-10-16 14:23:28 -0400

    adjustment

--- cookbooks/memcached/recipes/monitoring.rb
+++ cookbooks/memcached/recipes/monitoring.rb
@@ -3,8 +3,3 @@ node[:memcached][:monitoring][:packages].each do |p|
   package p
 end
 
-nagios_nrpecheck "Memcached_Machine_Memory" do
-  command "#{node['nagios']['plugin_dir']}/check_mem.sh"
-  parameters '-w 85 -c 90'
-  action :add
-end
---
+++ cookbooks/memcached/recipes/nagios.rb
@@ -0,0 +1,5 @@
+nagios_nrpecheck "Memcached_Machine_Memory" do
+  command "#{node['nagios']['plugin_dir']}/check_mem.sh"
+  parameters '-w 85 -c 90'
+  action :add
+end
