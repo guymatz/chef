9d4b16a7b9452244f7affaf813caaf402f5efd2e
Irina Kaprizkina <irinakaprizkina@clearchannel.com>
2013-10-22 11:23:48 -0400

    Testing the scripts

--- cookbooks/mongodb/recipes/admin_scripts.rb
+++ cookbooks/mongodb/recipes/admin_scripts.rb
@@ -24,7 +24,7 @@ bash "chown scripts dir" do
 end
 
 cron_d "compact_MongoDB" do
-  minute "07"
+  minute "43"
   hour   "11"
   user "root"
   command "/root/scripts/mongodba/bin/compactData.sh -h 127.0.0.1 -p 37017 -f d "
