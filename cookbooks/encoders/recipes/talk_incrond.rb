f28e2d5babe537dc1f8a124b7bc95ec2da4b674c
John Deragon <jderagon@clearchannel.com>
2013-10-23 18:10:49 +0000

    added users to hadoop, talk incron fixes

--- cookbooks/encoders/recipes/talk_incrond.rb
+++ cookbooks/encoders/recipes/talk_incrond.rb
@@ -1,14 +1,34 @@
 begin
 
-   application "content-talk" do
-      path "/data/apps/content-talk/"
-      owner node[:encoder][:user]
-      group node[:encoder][:group]
-      repository node[:encftp][:incrond][:github_url]
-      restart_command "/usr/bin/incrontab -u converter /data/apps/content-talk/current/talk-incron/incrontab/filemonitors.incron.tab" 
-      revision "master"
-   end
+#   application "content-talk" do
+#      path "/data/apps/content-talk/"
+#      owner node[:encoder][:user]
+#      group node[:encoder][:group]
+#      repository node[:encftp][:incrond][:github_url]
+#      restart_command "/usr/bin/incrontab -u converter /data/apps/content-talk/current/talk-incron/incrontab/filemonitors.incron.tab" 
+#      deploy_key "encoder_deploy"
+#      ssh_wrapper "/home/converter/encoder-wrap-ssh.sh"
+#      revision "master"
+#   end
 
+#    application "content-talk" do
+#        path "/data/apps/content-talk"
+#        deploy_key "encoder_deploy"
+#        repository node[:encftp][:incrond][:github_url]
+#        revision "master"
+#    end
+
+#    deploy "content-talk" do
+#        repo node[:encftp][:incrond][:github_url]
+#        symlinks.clear
+#        symlink_before_migrate.clear
+#        create_dirs_before_symlink.clear
+#        user "converter"
+#        deploy_to "/data/apps/content-talk"
+#        action :deploy
+#        ssh_wrapper "/home/converter/encoder-wrap-ssh.sh"
+#    end
+#
     service "incrond" do
         action [:enable, :start]
     end
