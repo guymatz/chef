872668a3a46aa0a72b01c3b9cbc93dea56001177
Gregory Patmore <gregorypatmore@gmail.com>
2013-10-23 14:44:29 -0400

    updated stuff

--- cookbooks/radioedit/recipes/dev-app.rb
+++ cookbooks/radioedit/recipes/dev-app.rb
@@ -1,17 +1,17 @@
 #
 # Cookbook Name:: radioedit
-# Recipe:: dev-app
+# Recipe:: prod-app
 #
 # Copyright 2013, iHeartRadio
 #
 # All rights reserved - Do Not Redistribute
-# Installs requirements for a dev version of an app server
+# Installs requirements for a production version of the radioedit app server
 #
 
 include_recipe "yum::epel"
 
 # make all required directories
-node[:radioedit][:epona][:req_dirs].each do |d|
+node[:radioedit][:bob][:req_dirs].each do |d|
   directory d do
     owner "ihr-deployer"
     group "ihr-deployer"
@@ -19,75 +19,130 @@ node[:radioedit][:epona][:req_dirs].each do |d|
   end
 end
 
-node[:radioedit][:epona][:packages].each do |p|
+node[:radioedit][:bob][:packages].each do |p|
   yum_package p do
     action [ :install, :upgrade ]
   end
 end
 
-template "#{node[:radioedit][:epona][:path]}/shared/settings.json" do
-  source "epona-settings.json.erb"
+template "#{node[:radioedit][:bob][:path]}/shared/settings.json" do
+  source "production-settings.json.erb"
   owner "ihr-deployer"
   group "ihr-deployer"
 end
 
-execute "set-app-env" do
-  command "/data/apps/radioedit/setenv.sh"
-  action :nothing
-end
-
-template "/data/apps/radioedit/setenv.sh" do
-  source "epona-env.sh.erb"
+link "#{node[:radioedit][:bob][:path]}/settings.json" do
+  to "#{node[:radioedit][:bob][:path]}/shared/settings.json"
+  action :create
   owner "ihr-deployer"
   group "ihr-deployer"
-  mode 0755
-  notifies :run, "execute[set-app-env]", :immediately
+  not_if "test -L #{node[:radioedit][:bob][:path]}/shared/settings.json"
 end
 
 
-python_virtualenv "#{node[:radioedit][:epona][:venv_path]}" do
+
+python_virtualenv "#{node[:radioedit][:bob][:venv_path]}" do
   interpreter "python27"
   owner "ihr-deployer"
   group "ihr-deployer"
   action :create
 end
 
-node[:radioedit][:epona][:packages].each  { |p| package p }
-node[:radioedit][:epona][:pips].each { |p| 
-  python_pip p do 
-    virtualenv "#{node[:radioedit][:epona][:venv_path]}"
-    action :install
-  end
-}
-
-# link "/usr/local/bin/supervisorctl" do
-#   to "/data/apps/radioedit/envs/core/bin/supervisorctl"
-#   action :create
-#    not_if "test -L /usr/local/bin/supervisorctl"
-# end
 
 application "radioedit-core" do
-  repository "#{node[:radioedit][:epona][:repo]}"
-  revision "#{node[:radioedit][:epona][:branch]}"
-  path "#{node[:radioedit][:epona][:path]}"
+  repository "#{node[:radioedit][:bob][:repo]}"
+  revision "#{node[:radioedit][:bob][:branch]}"
+  path "#{node[:radioedit][:bob][:path]}"
   owner "ihr-deployer"
   group "ihr-deployer"
   enable_submodules true
 
   gunicorn do
     app_module 'wsgi'
-    port node[:radioedit][:epona][:port]
-    host node[:radioedit][:epona][:host]
-    workers node[:radioedit][:epona][:num_workers]
-    pidfile node[:radioedit][:epona][:pid_file]
-    virtualenv "#{node[:radioedit][:epona][:venv_path]}"
-    stdout_logfile "#{node[:radioedit][:epona][:out_log]}"
-    stderr_logfile "#{node[:radioedit][:epona][:err_log]}"
-    packages node[:radioedit][:epona][:pips]
+    port node[:radioedit][:bob][:port]
+    host node[:radioedit][:bob][:host]
+    workers node[:radioedit][:bob][:num_workers]
+    pidfile node[:radioedit][:bob][:pid_file]
+    virtualenv "#{node[:radioedit][:bob][:venv_path]}"
+    stdout_logfile "#{node[:radioedit][:bob][:out_log]}"
+    stderr_logfile "#{node[:radioedit][:bob][:err_log]}"
+    packages node[:radioedit][:bob][:pips]
     loglevel "DEBUG"
     interpreter "python27"
   end
 end
+
+# gp adding these templates to a util directory until a way using existing chef resource objects is found.
+template "#{node[:radioedit][:bob][:utildir]}/supervisor" do
+  source "dev-supervisor.initd.erb"
+  owner "root"
+  group "root"
+  mode 0755
+  action [:create]
+end
+
+template "#{node[:radioedit][:bob][:utildir]}/radioedit.conf" do
+  source "dev-radioedit.conf.erb"
+  owner "root"
+  group "root"
+  mode 0666
+  action [:create]
+end
+
+template "#{node[:radioedit][:bob][:utildir]}/upd_confs.sh" do
+  source "dev-reset-configs.sh.erb"
+  owner "root"
+  group "root"
+  mode 0755
+  action [:delete,:create]
+end
+
+# reset ownership to nginx to allow static file serving
+directory "#{node[:radioedit][:bob][:staticdir]}" do
+    owner "nginx"
+    group "nginx"
+    action :create
+  end
+
+template "#{node[:radioedit][:bob][:staticdir]}/android.json" do
+  source "staticfile-android.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444
+  action [:delete, :create]
+end
+
+template "#{node[:radioedit][:bob][:staticdir]}/fux.json" do
+  source "staticfile-fux.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444 
+  action [:delete, :create]
+end
+
+template "#{node[:radioedit][:bob][:staticdir]}/iphone.json" do
+  source "staticfile-iphone.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444
+  action [:delete, :create]
+end
+
+template "#{node[:radioedit][:bob][:staticdir]}/kenwood.json" do
+  source "staticfile-kenwood.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444
+  action [:delete, :create]
+end
+
+
+#GP hackety hack hack - @TODO Templatize what this script does
+execute "reset-confs" do
+  command "#{node[:radioedit][:bob][:utildir]}/upd_confs.sh"
+  action :run
+end
+
 
 
 
