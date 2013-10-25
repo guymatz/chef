97b2d57b6ba1d92c9081496aaf658533b075e042
John Deragon <jderagon@clearchannel.com>
2013-10-25 14:49:19 +0000

    fixed incron restart

--- cookbooks/encoders/recipes/talk_incrond.rb
+++ cookbooks/encoders/recipes/talk_incrond.rb
@@ -11,24 +11,26 @@ begin
 #      revision "master"
 #   end
 
-#    application "content-talk" do
-#        path "/data/apps/content-talk"
-#        deploy_key "encoder_deploy"
-#        repository node[:encftp][:incrond][:github_url]
-#        revision "master"
-#    end
+    application "content-talk" do
+        path "/data/apps/content-talk"
+        deploy_key "encoder_deploy"
+        repository node[:encftp][:incrond][:github_url]
+        revision "master"
+    end
 
-#    deploy "content-talk" do
-#        repo node[:encftp][:incrond][:github_url]
-#        symlinks.clear
-#        symlink_before_migrate.clear
-#        create_dirs_before_symlink.clear
-#        user "converter"
-#        deploy_to "/data/apps/content-talk"
-#        action :deploy
-#        ssh_wrapper "/home/converter/encoder-wrap-ssh.sh"
-#    end
-#
+    deploy "content-talk" do
+        repo node[:encftp][:incrond][:github_url]
+        symlinks.clear
+        symlink_before_migrate.clear
+        create_dirs_before_symlink.clear
+        user "converter"
+        deploy_to "/data/apps/content-talk"
+#        restart_command "/usr/bin/incrontab -u converter /data/apps/content-talk/current/talk-incron/incrontab/filemonitors.incron.tab" 
+        restart_command "/usr/local/bin/load_incron.sh"
+        action  :deploy
+        ssh_wrapper "/home/converter/encoder-wrap-ssh.sh"
+    end
+
     service "incrond" do
         action [:enable, :start]
     end
