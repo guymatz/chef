958ccc279629f5f20fefd62d3e191fc73eb80d22
Gregory Patmore <gregorypatmore@gmail.com>
2013-10-25 16:44:39 -0400

    updated java with recipe to install oracle jdk 1.7.0_45

--- cookbooks/etl_jobs/recipes/default.rb
+++ cookbooks/etl_jobs/recipes/default.rb
@@ -53,17 +53,7 @@ cron_d "playlog_job" do
   minute "22,52"
 end
 
-directory "/data/jobs/profile"
-directory "/data/log/profile"
-remote_file "/data/jobs/profile/profile_job.jar" do
-  source "http://yum.ihr/files/jobs/profile/profile_job.jar"
-end
 
-#cron_d "profile_job" do
-#  command "/usr/bin/cronwrap iad-jobserver101.ihr profile-job \"/usr/bin/java -jar /data/jobs/profile/profile_job.jar launch-context.xml profileJob rundate=`/bin/date +\\%s`\""
-#  minute 30
-#  hour 3
-#end
 
 #
 #ALTERED PER OPS-4760
@@ -467,14 +457,16 @@ cron_d "event_job" do
   hour 5
 end
 
+
 # Added per OPS-5313
+# updated per OPS-5649
 directory "/data/jobs/profile"
 directory "/data/log/profile"
 remote_file "/data/jobs/profile/profile_job.jar" do
   source "http://yum.ihr/files/jobs/profile/profile_job.jar"
 end
 cron_d "profile_job" do
-  command "/usr/bin/nsca_relay -S profile-job -- /usr/bin/java -jar /data/jobs/profile/profile_job.jar launch-context.xml profileJob rundate=`/bin/date +\\%s`"
+  command "/usr/bin/nsca_relay -S profile-job -- usr/lib/jvm/java-1.7.0/bin/java -jar /data/jobs/profile/profile_job.jar launch-context.xml profileJob rundate=$(/bin/date +\%s) favoritesStartDate=$(/bin/date \%Y\%m\%d)"
   minute 0
   hour 0
 end
