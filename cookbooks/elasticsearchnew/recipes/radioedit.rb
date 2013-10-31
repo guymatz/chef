ce996f0f54bf2bf7f0160761579d9cbd24162b7d
Gregory Patmore <gregorypatmore@gmail.com>
2013-10-31 15:04:00 -0400

    fixes

--- cookbooks/elasticsearchnew/recipes/radioedit.rb
+++ cookbooks/elasticsearchnew/recipes/radioedit.rb
@@ -7,40 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 unless tagged?('elasticsearchnew-deployed')
-  %w{ users::elasticsearch elasticsearchnew::users users::deployer elasticsearchnew::elasticsearch elasticsearchnew::ulimits }.each do |cb|
-    include_recipe cb
-  end
-  
-  # %w{ configs data input logs }.each do |der|
-  #   directory "#{node[:elasticsearchnew][:ihrsearch_path]}/#{der}" do
-  #     owner node[:elasticsearchnew][:user]
-  #     group node[:elasticsearchnew][:group]
-  #     recursive true
-  #   end
-  # end
-  
-  # pkg = "configs.tgz"
-  
-  # execute "chown-ihr-search-configs" do
-  #   command "chown -R #{node[:elasticsearchnew][:user]}.#{node[:elasticsearchnew][:group]} #{node[:elasticsearchnew][:ihrsearch_path]}"
-  #   action :nothing
-  # end
-  
-  # execute "Untar-ihr-search-configs" do
-  #   command "tar zxf #{pkg} -C #{node[:elasticsearchnew][:ihrsearch_path]}/configs"
-  #   cwd Chef::Config[:file_cache_path]
-  #   action :nothing
-  #   notifies :run, resources(:execute => "chown-ihr-search-configs"), :immediately
-  #   user node[:elasticsearchnew][:user]
-  #   group node[:elasticsearchnew][:group]
-  # end
-  
-  # remote_file "#{Chef::Config[:file_cache_path]}/#{pkg}" do
-  #   source "#{node[:elasticsearchnew][:url]}/es-configs/#{pkg}"
-  #   notifies :run, resources(:execute => "Untar-ihr-search-configs"), :immediately
-  #   owner node[:elasticsearchnew][:user]
-  #   group node[:elasticsearchnew][:group]
-  # end
+  %w{ users::elasticsearch 
+      elasticsearchnew::users 
+      users::deployer 
+      elasticsearchnew::elasticsearch 
+      elasticsearchnew::ulimits 
+    }.each { |cb| include_recipe cb;}
   
   template "/etc/init.d/elasticsearch" do
     source "elasticsearch.init.erb"i
@@ -72,11 +44,7 @@ unless tagged?('elasticsearchnew-deployed')
                })
     notifies :restart, "service[elasticsearch]"
   end
-  
-  # template "#{node[:elasticsearchnew][:ihrsearch_path]}/configs/logging.yml" do
-  #   source "logging.yml.erb"
-  #   owner node[:elasticsearchnew][:user]
-  #   group node[:elasticsearchnew][:group]
-  # end
+
   tag('elasticsearchnew-deployed')
 end
+
