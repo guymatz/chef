d79f04ccbe405adead47191df479ccd0d35b3eaa
Gregory Patmore <gregorypatmore@gmail.com>
2013-10-31 10:32:05 -0400

    updating recipe to make more sense with the use of the radioedit envs.

--- cookbooks/radioedit/attributes/default.rb
+++ cookbooks/radioedit/attributes/default.rb
@@ -48,6 +48,316 @@ default[:radioedit][:nginx][:port] = 8000
 # ################################################################
 # Development Environment settings.
 # ################################################################
+default[:radioedit][:dev][:path] = "/data/apps/radioedit";
+default[:radioedit][:dev][:pid_file] = "/var/run/radioedit/radioedit-epona.pid"
+default[:radioedit][:dev][:log_dir] = "/var/log/radioedit"
+default[:radioedit][:dev][:out_log] = "#{default[:radioedit][:dev][:log_dir]}/epona.log"
+default[:radioedit][:dev][:err_log] = "#{default[:radioedit][:dev][:log_dir]}/epona.err"
+default[:radioedit][:dev][:venv_path] = "#{default[:radioedit][:dev][:path]}/envs/core";
+default[:radioedit][:dev][:module] = "wsgi:application"
+default[:radioedit][:dev][:repo] = "git@github.ihrint.com:radioedit/core.git"
+default[:radioedit][:dev][:branch] = "release"
+default[:radioedit][:dev][:intbranch] = "testing"
+default[:radioedit][:dev][:packages] = %w{ libevent memcached python27 python27-libs python27-devel python27-test python27-tools nginx zlib-devel openjpeg-devel zlib gcc python-devel git libevent-devel zeromq-devel zeromq python-setuptools python-ldap postgresql-devel openldap-devel varnish readline-devel patch libjpeg-devel }
+default[:radioedit][:dev][:pips] = %w{ supervisor pymongo python-memcached greenlet statsd pyzmq flask flask-cache flask-pymongo pillow blinker celery colorama docutils gevent-websocket jsonschema mock pql pylibmc pyparsing python-dateutil python-ldap requests simplejson raven pytest }
+default[:radioedit][:dev][:env] = "ihr_testing"
+default[:radioedit][:dev][:num_workers] = 5
+default[:radioedit][:dev][:port] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:dev][:listen] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:dev][:host] = "unix"
+default[:radioedit][:dev][:utildir] = "#{default[:radioedit][:dev][:path]}/util"
+default[:radioedit][:dev][:mongo_cstring] = "mongodb://127.0.0.1:27017/radioedit-f"
+default[:radioedit][:dev][:req_dirs] = %w{ 
+  /data 
+  /data/apps 
+  /data/apps/radioedit 
+  /data/apps/radioedit/util 
+  /data/apps/radioedit/static
+  /data/apps/radioedit/releases 
+  /data/apps/radioedit/envs 
+  /data/apps/radioedit/envs/core 
+  /var/run/radioedit 
+  /var/log/radioedit 
+  /data/apps/radioedit/shared 
+}
+default[:radioedit][:dev][:packages] = %w{ 
+  libevent 
+  memcached 
+  python27 
+  python27-libs 
+  python27-devel 
+  python27-test 
+  python27-tools 
+  nginx 
+  zlib-devel 
+  openjpeg-devel 
+  zlib 
+  gcc 
+  python-devel 
+  git 
+  libevent-devel 
+  zeromq-devel 
+  zeromq 
+  python-setuptools 
+  python-ldap 
+  postgresql-devel 
+  openldap-devel 
+  varnish 
+  readline-devel 
+  patch 
+  libjpeg-devel 
+}
+
+default[:radioedit][:dev][:pips] = %w{ 
+  supervisor 
+  pymongo 
+  python-memcached 
+  greenlet 
+  statsd 
+  pyzmq 
+  flask 
+  flask-cache 
+  flask-pymongo 
+  pillow 
+  blinker 
+  celery 
+  colorama 
+  docutils 
+  gevent-websocket 
+  jsonschema 
+  mock 
+  pql 
+  pylibmc 
+  pyparsing 
+  python-dateutil 
+  python-ldap 
+  requests 
+  simplejson 
+  raven 
+  pytest 
+}
+# ################################################################
+# END Development Environment settings.
+# ################################################################
+
+
+# ################################################################
+# Staging Environment settings.
+# ################################################################
+default[:radioedit][:staging][:repo] = "git@github.ihrint.com:radioedit/core.git"
+default[:radioedit][:staging][:branch] = "staging"
+default[:radioedit][:staging][:env] = "ihr_testing"
+default[:radioedit][:staging][:path] = "/data/apps/radioedit";
+default[:radioedit][:staging][:pid_file] = "/var/run/radioedit/radioedit-epona.pid"
+default[:radioedit][:staging][:log_dir] = "/var/log/radioedit"
+default[:radioedit][:staging][:out_log] = "#{default[:radioedit][:staging][:log_dir]}/application.log"
+default[:radioedit][:staging][:err_log] = "#{default[:radioedit][:staging][:log_dir]}/applicaiton.err"
+default[:radioedit][:staging][:venv_path] = "#{default[:radioedit][:staging][:path]}/envs/core";
+default[:radioedit][:staging][:module] = "wsgi:application"
+default[:radioedit][:staging][:num_workers] = 5
+default[:radioedit][:staging][:port] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:staging][:listen] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:staging][:host] = "unix"
+default[:radioedit][:staging][:utildir] = "#{default[:radioedit][:staging][:path]}/util"
+default[:radioedit][:staging][:staticdir] = "#{default[:radioedit][:staging][:path]}/static"
+default[:radioedit][:staging][:mongo_cstring] = "mongodb://127.0.0.1:27017/radioedit-epona"
+default[:radioedit][:staging][:req_dirs] = %w{ 
+  /data 
+  /data/apps 
+  /data/apps/radioedit 
+  /data/apps/radioedit/util 
+  /data/apps/radioedit/static
+  /data/apps/radioedit/releases 
+  /data/apps/radioedit/envs 
+  /data/apps/radioedit/envs/core 
+  /var/run/radioedit 
+  /var/log/radioedit 
+  /data/apps/radioedit/shared 
+}
+default[:radioedit][:staging][:packages] = %w{ 
+  libevent 
+  memcached 
+  python27 
+  python27-libs 
+  python27-devel 
+  python27-test 
+  python27-tools 
+  nginx 
+  zlib-devel 
+  openjpeg-devel 
+  zlib 
+  gcc 
+  python-devel 
+  git 
+  libevent-devel 
+  zeromq-devel 
+  zeromq 
+  python-setuptools 
+  python-ldap 
+  postgresql-devel 
+  openldap-devel 
+  varnish 
+  readline-devel 
+  patch 
+  libjpeg-devel 
+}
+default[:radioedit][:staging][:pips] = %w{ 
+  supervisor 
+  pymongo 
+  python-memcached 
+  greenlet 
+  statsd 
+  pyzmq 
+  flask 
+  flask-cache 
+  flask-pymongo 
+  pillow 
+  blinker 
+  celery 
+  colorama 
+  docutils 
+  gevent-websocket 
+  jsonschema 
+  mock 
+  pql 
+  pylibmc 
+  pyparsing 
+  python-dateutil 
+  python-ldap 
+  requests 
+  simplejson 
+  raven 
+  pytest 
+}
+# ################################################################
+# End of staging env
+# ################################################################
+
+# ################################################################
+# PRODUCTION SETTINGS!!!!!
+# ################################################################
+default[:radioedit][:production][:repo] = "git@github.ihrint.com:radioedit/core.git"
+default[:radioedit][:production][:branch] = "release"
+default[:radioedit][:production][:env] = "ihr_testing"
+default[:radioedit][:production][:path] = "/data/apps/radioedit";
+default[:radioedit][:production][:pid_file] = "/var/run/radioedit/radioedit-epona.pid"
+default[:radioedit][:production][:log_dir] = "/var/log/radioedit"
+default[:radioedit][:production][:out_log] = "#{default[:radioedit][:production][:log_dir]}/application.log"
+default[:radioedit][:production][:err_log] = "#{default[:radioedit][:production][:log_dir]}/applicaiton.err"
+default[:radioedit][:production][:venv_path] = "#{default[:radioedit][:production][:path]}/envs/core";
+default[:radioedit][:production][:module] = "wsgi:application"
+default[:radioedit][:production][:num_workers] = 5
+default[:radioedit][:production][:port] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:production][:listen] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:production][:host] = "unix"
+default[:radioedit][:production][:utildir] = "#{default[:radioedit][:production][:path]}/util"
+default[:radioedit][:production][:staticdir] = "#{default[:radioedit][:production][:path]}/static"
+default[:radioedit][:production][:mongo_cstring] = "mongodb://iad-mongo-shared101.ihr:37017/radioedit-f"
+
+default[:radioedit][:production][:req_dirs] = %w{ 
+  /data 
+  /data/apps 
+  /data/apps/radioedit 
+  /data/apps/radioedit/util 
+  /data/apps/radioedit/static
+  /data/apps/radioedit/releases 
+  /data/apps/radioedit/envs 
+  /data/apps/radioedit/envs/core 
+  /var/run/radioedit 
+  /var/log/radioedit 
+  /data/apps/radioedit/shared 
+}
+
+default[:radioedit][:production][:packages] = %w{ 
+  libevent 
+  memcached 
+  python27 
+  python27-libs 
+  python27-devel 
+  python27-test 
+  python27-tools 
+  nginx 
+  zlib-devel 
+  openjpeg-devel 
+  zlib 
+  gcc 
+  python-devel 
+  git 
+  libevent-devel 
+  zeromq-devel 
+  zeromq 
+  python-setuptools 
+  python-ldap 
+  postgresql-devel 
+  openldap-devel 
+  varnish 
+  readline-devel 
+  patch 
+  libjpeg-devel 
+}
+
+default[:radioedit][:production][:pips] = %w{ 
+  supervisor 
+  pymongo 
+  python-memcached 
+  greenlet 
+  statsd 
+  pyzmq 
+  flask 
+  flask-cache 
+  flask-pymongo 
+  pillow 
+  blinker 
+  celery 
+  colorama 
+  docutils 
+  gevent-websocket 
+  jsonschema 
+  mock 
+  pql 
+  pylibmc 
+  pyparsing 
+  python-dateutil 
+  python-ldap 
+  requests 
+  simplejson 
+  raven 
+  pytest 
+}
+# ################################################################
+# END PRODUCTION SETTINGS
+# ################################################################
+
+
+# ###################################################################
+# ###################################################################
+# ###################################################################
+# ###################################################################
+# ###################################################################
+
+
+
+# ################################################################
+# Development Environment settings.
+# ################################################################
+
+default[:radioedit][:bob][:repo] = "git@github.ihrint.com:radioedit/core.git"
+default[:radioedit][:bob][:branch] = "staging"
+default[:radioedit][:bob][:env] = "ihr_testing"
+default[:radioedit][:bob][:path] = "/data/apps/radioedit";
+default[:radioedit][:bob][:pid_file] = "/var/run/radioedit/radioedit-epona.pid"
+default[:radioedit][:bob][:log_dir] = "/var/log/radioedit"
+default[:radioedit][:bob][:out_log] = "#{default[:radioedit][:bob][:log_dir]}/application.log"
+default[:radioedit][:bob][:err_log] = "#{default[:radioedit][:bob][:log_dir]}/applicaiton.err"
+default[:radioedit][:bob][:venv_path] = "#{default[:radioedit][:bob][:path]}/envs/core";
+default[:radioedit][:bob][:module] = "wsgi:application"
+default[:radioedit][:bob][:num_workers] = 5
+default[:radioedit][:bob][:port] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:bob][:listen] = "/var/tmp/radioedit-cms.sock"
+default[:radioedit][:bob][:host] = "unix"
+default[:radioedit][:bob][:utildir] = "#{default[:radioedit][:bob][:path]}/util"
+default[:radioedit][:bob][:staticdir] = "#{default[:radioedit][:bob][:path]}/static"
+default[:radioedit][:bob][:mongo_cstring] = "mongodb://127.0.0.1:27017/radioedit-epona"
 default[:radioedit][:bob][:req_dirs] = %w{ 
   /data 
   /data/apps 
@@ -116,23 +426,7 @@ default[:radioedit][:bob][:pips] = %w{
   raven 
   pytest 
 }
-default[:radioedit][:bob][:repo] = "git@github.ihrint.com:radioedit/core.git"
-default[:radioedit][:bob][:branch] = "staging"
-default[:radioedit][:bob][:env] = "ihr_testing"
-default[:radioedit][:bob][:path] = "/data/apps/radioedit";
-default[:radioedit][:bob][:pid_file] = "/var/run/radioedit/radioedit-epona.pid"
-default[:radioedit][:bob][:log_dir] = "/var/log/radioedit"
-default[:radioedit][:bob][:out_log] = "#{default[:radioedit][:bob][:log_dir]}/application.log"
-default[:radioedit][:bob][:err_log] = "#{default[:radioedit][:bob][:log_dir]}/applicaiton.err"
-default[:radioedit][:bob][:venv_path] = "#{default[:radioedit][:bob][:path]}/envs/core";
-default[:radioedit][:bob][:module] = "wsgi:application"
-default[:radioedit][:bob][:num_workers] = 5
-default[:radioedit][:bob][:port] = "/var/tmp/radioedit-cms.sock"
-default[:radioedit][:bob][:listen] = "/var/tmp/radioedit-cms.sock"
-default[:radioedit][:bob][:host] = "unix"
-default[:radioedit][:bob][:utildir] = "#{default[:radioedit][:bob][:path]}/util"
-default[:radioedit][:bob][:staticdir] = "#{default[:radioedit][:bob][:path]}/static"
-default[:radioedit][:bob][:mongo_cstring] = "mongodb://127.0.0.1:27017/radioedit-epona"
+
 # ################################################################
 # End of 'bob' env
 # ################################################################
@@ -229,100 +523,6 @@ default[:radioedit][:epona][:pips] = %w{
 # End Epona QA environment attributes
 
 
-# ################################################################
-# PRODUCTION SETTINGS!!!!!
-# ################################################################
-default[:radioedit][:production][:repo] = "git@github.ihrint.com:radioedit/core.git"
-default[:radioedit][:production][:branch] = "release"
-default[:radioedit][:production][:env] = "ihr_testing"
-default[:radioedit][:production][:path] = "/data/apps/radioedit";
-default[:radioedit][:production][:pid_file] = "/var/run/radioedit/radioedit-epona.pid"
-default[:radioedit][:production][:log_dir] = "/var/log/radioedit"
-default[:radioedit][:production][:out_log] = "#{default[:radioedit][:production][:log_dir]}/application.log"
-default[:radioedit][:production][:err_log] = "#{default[:radioedit][:production][:log_dir]}/applicaiton.err"
-default[:radioedit][:production][:venv_path] = "#{default[:radioedit][:production][:path]}/envs/core";
-default[:radioedit][:production][:module] = "wsgi:application"
-default[:radioedit][:production][:num_workers] = 5
-default[:radioedit][:production][:port] = "/var/tmp/radioedit-cms.sock"
-default[:radioedit][:production][:listen] = "/var/tmp/radioedit-cms.sock"
-default[:radioedit][:production][:host] = "unix"
-default[:radioedit][:production][:utildir] = "#{default[:radioedit][:production][:path]}/util"
-default[:radioedit][:production][:staticdir] = "#{default[:radioedit][:production][:path]}/static"
-default[:radioedit][:production][:mongo_cstring] = "mongodb://iad-mongo-shared101.ihr:37017/radioedit-f"
-
-default[:radioedit][:production][:req_dirs] = %w{ 
-  /data 
-  /data/apps 
-  /data/apps/radioedit 
-  /data/apps/radioedit/util 
-  /data/apps/radioedit/static
-  /data/apps/radioedit/releases 
-  /data/apps/radioedit/envs 
-  /data/apps/radioedit/envs/core 
-  /var/run/radioedit 
-  /var/log/radioedit 
-  /data/apps/radioedit/shared 
-}
-
-default[:radioedit][:production][:packages] = %w{ 
-  libevent 
-  memcached 
-  python27 
-  python27-libs 
-  python27-devel 
-  python27-test 
-  python27-tools 
-  nginx 
-  zlib-devel 
-  openjpeg-devel 
-  zlib 
-  gcc 
-  python-devel 
-  git 
-  libevent-devel 
-  zeromq-devel 
-  zeromq 
-  python-setuptools 
-  python-ldap 
-  postgresql-devel 
-  openldap-devel 
-  varnish 
-  readline-devel 
-  patch 
-  libjpeg-devel 
-}
-
-default[:radioedit][:production][:pips] = %w{ 
-  supervisor 
-  pymongo 
-  python-memcached 
-  greenlet 
-  statsd 
-  pyzmq 
-  flask 
-  flask-cache 
-  flask-pymongo 
-  pillow 
-  blinker 
-  celery 
-  colorama 
-  docutils 
-  gevent-websocket 
-  jsonschema 
-  mock 
-  pql 
-  pylibmc 
-  pyparsing 
-  python-dateutil 
-  python-ldap 
-  requests 
-  simplejson 
-  raven 
-  pytest 
-}
-# ################################################################
-# END PRODUCTION SETTINGS
-# ################################################################
 
 
 
---
+++ cookbooks/radioedit/recipes/staging-app.rb
@@ -0,0 +1,152 @@
+#
+# Cookbook Name:: radioedit
+# Recipe:: staging-app
+#
+# Copyright 2013, iHeartRadio
+#
+# All rights reserved - Do Not Redistribute
+# Installs requirements for a production version of the radioedit app server
+#
+
+include_recipe "yum::epel"
+
+# make all required directories
+node[:radioedit][:bob][:req_dirs].each do |d|
+  directory d do
+    owner "ihr-deployer"
+    group "ihr-deployer"
+    action :create
+  end
+end
+
+node[:radioedit][:staging][:packages].each do |p|
+  yum_package p do
+    action [ :install, :upgrade ]
+  end
+end
+
+template "#{node[:radioedit][:staging][:path]}/shared/settings.json" do
+  source "dev-settings.json.erb"
+  owner "ihr-deployer"
+  group "ihr-deployer"
+end
+
+link "#{node[:radioedit][:staging][:path]}/settings.json" do
+  to "#{node[:radioedit][:staging][:path]}/shared/settings.json"
+  action :create
+  owner "ihr-deployer"
+  group "ihr-deployer"
+  not_if "test -L #{node[:radioedit][:staging][:path]}/shared/settings.json"
+end
+
+
+
+python_virtualenv "#{node[:radioedit][:staging][:venv_path]}" do
+  interpreter "python27"
+  owner "ihr-deployer"
+  group "ihr-deployer"
+  action :create
+end
+
+
+application "radioedit-core" do
+  repository "#{node[:radioedit][:staging][:repo]}"
+  revision "#{node[:radioedit][:staging][:branch]}"
+  path "#{node[:radioedit][:staging][:path]}"
+  owner "ihr-deployer"
+  group "ihr-deployer"
+  enable_submodules true
+
+  gunicorn do
+    app_module 'wsgi:application'
+    port node[:radioedit][:staging][:port]
+    host node[:radioedit][:staging][:host]
+    workers node[:radioedit][:staging][:num_workers]
+    pidfile node[:radioedit][:staging][:pid_file]
+    virtualenv "#{node[:radioedit][:staging][:venv_path]}"
+    stdout_logfile "#{node[:radioedit][:staging][:out_log]}"
+    stderr_logfile "#{node[:radioedit][:staging][:err_log]}"
+    packages node[:radioedit][:staging][:pips]
+    loglevel "DEBUG"
+    interpreter "python27"
+  end
+end
+
+# gp adding these templates to a util directory until a way using existing chef resource objects is found.
+template "#{node[:radioedit][:staging][:utildir]}/supervisor" do
+  source "dev-supervisor-initd.erb"
+  owner "root"
+  group "root"
+  mode 0755
+  action [ :create ]
+end
+
+template "#{node[:radioedit][:staging][:utildir]}/radioedit.conf" do
+  source "dev-radioedit.conf.erb"
+  owner "root"
+  group "root"
+  mode 0666
+  action [ :create ]
+end
+
+template "#{node[:radioedit][:staging][:utildir]}/upd_confs.sh" do
+  source "dev-reset-configs.sh.erb"
+  owner "root"
+  group "root"
+  mode 0755
+  action [ :delete,:create ]
+end
+
+# reset ownership to nginx to allow static file serving
+directory "#{node[:radioedit][:staging][:staticdir]}" do
+    owner "nginx"
+    group "nginx"
+    action :create
+  end
+
+template "#{node[:radioedit][:staging][:staticdir]}/android.json" do
+  source "staticfile-android.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444
+  action [ :delete, :create ]
+end
+
+template "#{node[:radioedit][:staging][:staticdir]}/fux.json" do
+  source "staticfile-fux.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444 
+  action [ :delete, :create ]
+end
+
+template "#{node[:radioedit][:staging][:staticdir]}/iphone.json" do
+  source "staticfile-iphone.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444
+  action [ :delete, :create ]
+end
+
+template "#{node[:radioedit][:staging][:staticdir]}/kenwood.json" do
+  source "staticfile-kenwood.json.erb"
+  owner "nginx"
+  group "nginx"
+  mode 0444
+  action [ :delete, :create ]
+end
+
+
+#GP hackety hack hack - @TODO Templatize what this script does
+execute "reset-confs" do
+  command "#{node[:radioedit][:staging][:utildir]}/upd_confs.sh"
+  action :run
+end
+
+
+
+
+
+
+
+
