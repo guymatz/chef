213be82404c7e94a67aa2b69987c0868a884c35a
Guy Matz <guymatz@clearchannel.com>
2013-11-04 16:50:19 -0500

    switch to using a template instead of helper function.  Still trying to use hash_pwd from helper library, but can't get it to go

--- cookbooks/proftpd/libraries/helper.rb
+++ cookbooks/proftpd/libraries/helper.rb
@@ -1,6 +1,4 @@
-module Proftpd
-
-  module Helpers
+module Helper
     def add_to_ftpasswd(user, user_info, pwd_file)
       user_entry = create_passwd_entry(user, user_info)
       return if user_exists?(user_entry, pwd_file)
@@ -8,24 +6,32 @@ module Proftpd
     end
 
     def create_passwd_entry(user, user_info)
-      pwd = user_info[:password]
+      pwd = user_info['password']
       # Convert pwd to a crypted MD5 hash
       pwd = hash_pwd(pwd)
-      uid = user_info[:uid]
+      uid = user_info['uid']
       gid = uid
-      home_dir = user_info[:uid]
-      pwd_entry = "#{user}:#{pwd}:#{uid}:#{gid}::#{home_dir}:#{shell}"
+      home_dir = user_info[:home_dir]
+      shell = user_info[:shell]
+      pwd_entry = "#{user}:#{pwd}:#{uid}:#{gid}::#{home_dir}:#{shell}\n"
       return pwd_entry
     end
 
     def user_exists?(user_entry, pwd_file)
-      results = open(pwd_file) { |f| f.grep(/#{user_entry}/) }
+      results = []
+      begin
+        results = open(pwd_file) { |f| f.grep(/#{Regexp.escape(user_entry)}/) }
+      rescue
+        Chef::Log.error("Could not open #{pwd_file}")
+      end
       return true if results.size > 0
       return false
     end
 
     def write_user_to_file(user_entry, pwd_file)
-      File.open(pwd_file, "w") { |f|
+      Chef::Log.info("Adding #{user_entry}")
+      File.open(pwd_file, "a+") { |f|
+        Chef::Log.info("Adding #{user_entry}")
         f.write(user_entry)
       }
     end
@@ -33,19 +39,17 @@ module Proftpd
     def hash_pwd(pwd) 
     	# Leaving out the randomness for now to make comparisons of 
     	# entries easier
-			##@# seeds = ('a'..'z').to_a
-			##@# seeds.concat( ('A'..'Z').to_a )
-			##@# seeds.concat( (0..9).to_a )
-			##@# seeds.concat ['/', '.']
-			##@# seeds.compact!
-			##@# salt_string = '$1$'
-			##@# 8.times { salt_string << seeds[ rand(seeds.size) ].to_s }
+  		##@# seeds = ('a'..'z').to_a
+  		##@# seeds.concat( ('A'..'Z').to_a )
+  		##@# seeds.concat( (0..9).to_a )
+  		##@# seeds.concat ['/', '.']
+  		##@# seeds.compact!
+  		##@# salt_string = '$1$'
+  		##@# 8.times { salt_string << seeds[ rand(seeds.size) ].to_s }
       salt_string = '$1$iheartrd'
 
       # Crypt a password suitable for use in shadow files
-      pwd.crypt( salt_string )
+      pwd.crypt( salt_string )  
     end
-
-  end
-
+  
 end
--- cookbooks/proftpd/recipes/auth_file.rb
+++ cookbooks/proftpd/recipes/auth_file.rb
@@ -7,118 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# class Chef::Recipe
+#   include Helper
+# end
 
-begin
-
-  #GP Edit - 7/30/13 Log rotation for all *.log files in /var/log/manager REF:OPS-4768
-  logrotate_app "enc_manager_logs" do
-    cookbook "logrotate"
-    path "/var/log/manager/*.log"
-    frequency "daily"
-    rotate 4
-    options ["missingok", "compress", "copytruncate", "notifempty"]
-    create "644 root root"
-  end  
-  
-  unless tagged?("encoder-ftp-deployed")
-
-    application "content-talk" do
-      path "/data/apps/content-talk/"
-      owner node[:encoder][:users]
-      group node[:encoder][:group]
-      repository node[:encftp][:incrond][:github_url]
-      revision "master"
-   end
-
-    directory node[:encoders][:p_ftp_mount] do
-        owner "converter"
-        group "converter"
-        not_if do FileTest.directory?(node[:encoders][:p_ftp_mount]) end
-    end
-
-    mount node[:encoders][:p_ftp_mount] do
-        device "#{node[:encoders][:isilon_server]}:#{node[:encoders][:p_ftp_export]}"
-        fstype "nfs"
-        options "rw,vers=3,bg,soft,tcp,intr"
-        action [:mount, :enable]
-    end
-
-
-    service "vsftpd" do
-        action [ :nothing ]
-        supports :status => true, :start => true, :stop => true, :restart => true
-    end
-
-    node[:ftpserver].each do |pkg|
-        yum_package pkg do
-              arch "x86_64"
-        end
-    end
-
-
-##
-## Not the most elegant parsing of the databag, but this shows my inexperience in ruby
-## need to clean it up.
-## JPD
+cookbook_file node[:proftpd][:user_file] do
+  action :create_if_missing
+end
 
 # Get our encrypted data
-    users = Chef::EncryptedDataBagItem.load("music_upload", "upload_users").to_hash
+users = Chef::EncryptedDataBagItem.load("music_upload", "upload_users-dev").to_hash
 
-    users.each do |k,v|
-        next if k == "id"
-        v.each do |dis, pw|
-            bash "keys" do
-                code <<-EOF
-                    echo "#{k}" >> /etc/vsftpd/vuser/music_users.txt
-                    echo "#{pw}" >> /etc/vsftpd/vuser/music_users.txt
-                    if [ ! -d #{node[:encoders][:ftp_mount]}/#{k} ]
-                    then
-                        mkdir #{node[:encoders][:ftp_mount]}/#{k}
-                        chmod 2775 #{node[:encoders][:ftp_mount]}/#{k}
-                        chown #{node[:encoders][:ftpuser]} #{node[:encoders][:ftp_mount]}/#{k}
-                        chgrp #{node[:encoders][:group]} #{node[:encoders][:ftp_mount]}/#{k}
-                    fi
-                EOF
-            end
-        end
-    end
-
-    bash "createdb" do
-        code <<-EOF
-            /usr/bin/db_load -T -t hash -f /etc/vsftpd/vuser/music_users.txt /etc/vsftpd/vuser/music_vusers.db
-            #rm -f /etc/vsftpd/vuser/music_users.txt
-        EOF
-    end
-
-
-    append_if_no_line "sftp" do
-        path "/etc/ssh/sshd_config"
-        line "Subsystem sftp /usr/libexec/openssh/sftp-server"
-    end
-
-    service "vsftpd" do
-       action [:enable, :start ]
-    end
-
-    service "incrond" do
-            action [:enable, :start]
-    end
-
-    cron_d "mixin_status" do
-        command "(cd /data/apps/converter/current/bin; ./mixin_traffic_poller.rb) > /dev/null 2>&1"
-        minute  "*/15"
-        hour    "*"
-        day     "*"
-        month   "*"
-        weekday "*"
-        user "root"
-    end
-
-    tag("encoder-ftp-deployed")
-    end
-    nagios_nrpecheck "check_newest_file_age" do
-      command "/usr/lib/nagios/plugins/check_newest_file_age.sh -d /data/inbound-ftp/nextgen/"
-    end
-rescue
-    untag("encoder-ftp-deployed")
+template "#{node[:proftpd][:user_file]}" do
+  source "ftpd.passwd.erb"
+  mode 0644
+  owner node[:proftpd][:user]
+  group node[:proftpd][:group]
+  variables(
+    :users => users
+  )
+  # helpers(Helper)
 end
+
+# users.each do |username,user_info|
+#   # There's some garbage in the json
+#   next unless user_info.is_a?(Hash)
+#   user_info[:home_dir] = "#{node['proftpd']['default_root']}/#{username}"
+#   user_info[:shell] = node['proftpd']['default_shell']
+#   log("#{user_info.inspect}")
+#   add_to_ftpasswd(username, user_info, node[:proftpd][:user_file])
+# end
