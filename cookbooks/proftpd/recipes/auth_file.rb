f46295d93262a95f430ead37b289c18da8584426
Guy Matz <guymatz@clearchannel.com>
2013-11-05 11:42:26 -0500

    working

--- cookbooks/proftpd/recipes/auth_file.rb
+++ cookbooks/proftpd/recipes/auth_file.rb
@@ -26,14 +26,4 @@ template "#{node[:proftpd][:user_file]}" do
   variables(
     :users => users
   )
-  # helpers(Helper)
 end
-
-# users.each do |username,user_info|
-#   # There's some garbage in the json
-#   next unless user_info.is_a?(Hash)
-#   user_info[:home_dir] = "#{node['proftpd']['default_root']}/#{username}"
-#   user_info[:shell] = node['proftpd']['default_shell']
-#   log("#{user_info.inspect}")
-#   add_to_ftpasswd(username, user_info, node[:proftpd][:user_file])
-# end
