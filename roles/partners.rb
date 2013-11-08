0527ba8b840cb2eaf491fa47de1de229b8b65faa
Mark Rechler <mrechler@iheart.com>
2013-11-08 19:18:14 +0000

    create role for partners.iheart.com

---
+++ roles/partners.rb
@@ -0,0 +1,18 @@
+name "partners"
+description "sets up a partners.iheart.com server"
+all_env = [
+           "recipe[partners.iheart.com]"
+          ]
+run_list(all_env)
+
+env_run_lists(
+              "_default" => all_env,
+              "qa2" => all_env,
+              "dev" => all_env,
+              "prod" => all_env,
+              "ec2-prod" => all_env,
+              "ec2" => all_env
+              )
+
+default_attributes ({
+                    })
