4a0e791708a5e703407a350c9ac7f7b5d3f4a596
Aaron Clements <aaronclements@clearchannel.com>
2013-11-14 11:53:42 -0500

    per OPS-5845 Tool EMR2 release

--- cookbooks/amp/attributes/default.rb
+++ cookbooks/amp/attributes/default.rb
@@ -1,6 +1,6 @@
 
-default[:amp][:version] = "tool-emr"
-default[:amp][:amp_rest_version] = "2.20.2"
+default[:amp][:version] = "tool-emr2"
+default[:amp][:amp_rest_version] = "2.20.3"
 default[:amp][:url] = "http://files.ihrdev.com/amp"
 
 default[:amp][:packages] = %w{ mongo-10gen-server mongo-10gen }
