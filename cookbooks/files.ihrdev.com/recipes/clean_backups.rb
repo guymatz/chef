a9d71c3a06dd2d31fd2175a25ca1b08b9ab9935b
Joseph Hammerman <josephhammerman@clearchannel.com>
2013-11-18 20:27:55 +0000

    Changed clean_backups to be more aggressive, run daily)

--- cookbooks/files.ihrdev.com/recipes/clean_backups.rb
+++ cookbooks/files.ihrdev.com/recipes/clean_backups.rb
@@ -1,8 +1,8 @@
 cron_d "clean_backups" do
-  command "/usr/bin/find /data/exports/files.ihrdev.com/auth_bak /data/exports/files.ihrdev.com/ing_bak /data/exports/files.ihrdev.com/mongo_fac_bak /data/exports/files.ihrdev.com/mongo_usr_bak /data/exports/files.ihrdev.com/mongo_util_bak -type f -mtime +7 -exec rm -f {} \;"
+  command "/usr/bin/find /data/exports/files.ihrdev.com/auth_bak /data/exports/files.ihrdev.com/ing_bak /data/exports/files.ihrdev.com/mongo_fac_bak /data/exports/files.ihrdev.com/mongo_usr_bak /data/exports/files.ihrdev.com/mongo_util_bak -type f -mtime +3 -exec rm -f {} \\;"
   minute  "25"
   hour    "6"
   month   "*"
-  weekday "3"
+  weekday "*"
   user    "root"
 end
