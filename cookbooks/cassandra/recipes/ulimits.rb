1864f3843323d044e5c9a9b47cd603b831a5c2a0
Guy Matz <guymatz@clearchannel.com>
2013-11-26 11:15:44 -0500

    unlimited ulimits

--- cookbooks/cassandra/recipes/ulimits.rb
+++ cookbooks/cassandra/recipes/ulimits.rb
@@ -1,19 +1,7 @@
-template "/etc/security/limits.d/cassandra.conf" do
-  source "limits.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables ({ :domain => node[:cassandra][:user],
-               :ulimits => node[:cassandra][:ulimits]
-            })
+file "/etc/security/limits.d/cassandra.conf" do
+  action :delete
 end
 
-template "/etc/security/limits.d/cassandra_root.conf" do
-  source "limits.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables ({ :domain => "root",
-               :ulimits => node[:cassandra][:root_ulimits]
-            })
+file "/etc/security/limits.d/cassandra_root.conf" do
+  action :delete
 end
--- cookbooks/facebook/recipes/ulimits.rb
+++ cookbooks/facebook/recipes/ulimits.rb
@@ -5,13 +5,6 @@
 
 
 
-template "/etc/security/limits.d/facebook.conf" do
-  source "limits.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables ({
-               :domain => node[:tomcat7][:user],
-               :ulimits => node[:facebook][:ulimits]
-             })
+file "/etc/security/limits.d/facebook.conf" do
+  action :delete
 end
