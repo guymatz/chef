2923fd0ad942bbdefcb1cd81484ae52f5c14c8ab
Mark Rechler <mrechler@iheart.com>
2013-12-02 15:39:15 +0000

    reduce number of days backups are kept

--- cookbooks/files.ihrdev.com/recipes/clean_backups.rb
+++ cookbooks/files.ihrdev.com/recipes/clean_backups.rb
@@ -1,5 +1,5 @@
 cron_d "clean_backups" do
-  command "/usr/bin/find /data/exports/files.ihrdev.com/auth_bak /data/exports/files.ihrdev.com/ing_bak /data/exports/files.ihrdev.com/mongo_fac_bak /data/exports/files.ihrdev.com/mongo_usr_bak /data/exports/files.ihrdev.com/mongo_util_bak -type f -mtime +3 -exec rm -f {} \\;"
+  command "/usr/bin/find /data/exports/files.ihrdev.com/auth_bak /data/exports/files.ihrdev.com/ing_bak /data/exports/files.ihrdev.com/mongo_fac_bak /data/exports/files.ihrdev.com/mongo_usr_bak /data/exports/files.ihrdev.com/mongo_util_bak -type f -mtime +1 -exec rm -f {} \\;"
   minute  "25"
   hour    "6"
   month   "*"
