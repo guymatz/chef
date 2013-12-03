1b8831e058916a04520c55ef88330adcf1b020b2
Joseph Hammerman <josephhammerman@clearchannel.com>
2013-12-03 17:48:11 -0500

    changed xbox.war deploy logic to include env

--- cookbooks/amp/recipes/xbox.rb
+++ cookbooks/amp/recipes/xbox.rb
@@ -13,7 +13,7 @@ service "tomcat" do
 end
 
 remote_file "#{node[:tomcat7][:webapp_dir]}/xbox.war" do
-  source "#{node[:amp][:url]}/xbox.war"
+  source "#{node[:amp][:url]}/#{node[:chef_environment]}/xbox.war"
   owner node[:tomcat7][:user]
   group node[:tomcat7][:group]
   mode "0755"
