a764448828493cf9381aaf6373f6aa3f16e9b052
Mark Rechler <mrechler@iheart.com>
2013-12-05 15:15:23 -0500

    bump up heap size

--- cookbooks/amp/attributes/subscription.rb
+++ cookbooks/amp/attributes/subscription.rb
@@ -41,7 +41,7 @@ else
   default[:subscription][:dirs] = [ default[:subscription][:parent_path], default[:subscription][:path] ]
   default[:subscription][:user] = "amp"
   default[:subscription][:group] = "amp"
-  default[:subscription][:heap_size] = "512m"
+  default[:subscription][:heap_size] = "1024m"
   default[:subscription][:jar_download] = "http://files.ihrdev.com/subscription-service/stage/subscriptions-service.jar"
   default[:subscription][:yml_download] = "http://files.ihrdev.com/subscription-service/stage/config.yml.erb"
   ### _DEFAULT ATTRIBUTES END ###
