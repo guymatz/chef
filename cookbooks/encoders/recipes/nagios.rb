deea57933f8ad36716a347766eaace26e5dab37f
Aaron Clements <aaronclements@clearchannel.com>
2013-12-05 11:09:07 -0500

    adding vantrix process check

--- cookbooks/encoders/recipes/nagios.rb
+++ cookbooks/encoders/recipes/nagios.rb
@@ -1,3 +1,9 @@
+nagios_nrpecheck "Check_Vantrix" do
+  command "#{node['nagios']['plugin_dir']}/check_procs"
+  parameters '-C /opt/spotxde/sbin/trx/VATranscoder'
+  action :add
+end
+
 nagios_nrpecheck "Check_incrond" do
   command "#{node['nagios']['plugin_dir']}/check_procs"
   parameters '-C incrond'
