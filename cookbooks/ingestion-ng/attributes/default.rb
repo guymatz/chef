963fef458f8eca16579c3c6426d5a8f7cb172436
Pavel Katsev <pkatsev@gmail.com>
2013-12-09 12:44:18 -0500

    fix freetds wiring

--- cookbooks/ingestion-ng/attributes/default.rb
+++ cookbooks/ingestion-ng/attributes/default.rb
@@ -51,16 +51,16 @@ default[:encoders][:s_encoder_export] = "/data/export/encoder"
 default[:encoders][:s_encoder_mount] = "/data/encoder"
 
 # FreeTDS
-default[:freetds][:servers][default[:ingestion_ng][:db][:name]] = {
+default[:freetds][:servers][default[:ingestion_ng][:db][:host]] = {
   :description => 'Ingestion3 MSsql in AWS',
-  :host => '10.5.1.52',
+  :host => 'use1b-ingestion-sqlserver101.ihr',
   :port => 1433,
   :tds_version => '8.0',
   :client_charset => 'UTF-8'
 }
 
 # ODBC
-default[:odbc][:name] = default[:ingestion_ng][:db][:name]
+default[:odbc][:name] = default[:ingestion_ng][:db][:host]
 default[:odbc][:driver] = '/usr/lib64/libtdsodbc.so'
 default[:odbc][:database] = 'ingestion'
 default[:odbc][:description] = default[:freetds][:servers][default[:odbc][:name]][:description]
