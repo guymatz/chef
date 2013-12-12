7ae47bfb98dca2b3b54915a880ef3df156035638
Guy Matz <guymatz@clearchannel.com>
2013-12-12 10:33:58 -0500

    removing ulimits in favor of ulimit cookbook, which grants high/unlimited privs

--- cookbooks/facebook/metadata.rb
+++ cookbooks/facebook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'none@none.com'
 license          'All rights reserved'
 description      'Installs/Configures facebook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{ users nagios tomcat7 java mongodb logrotate cron }.each do |dep|
   depends dep
