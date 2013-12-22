6c9efe33aec9c7d6d3473c20ace35fc04f17377d
Joseph Hammerman <josephhammerman@clearchannel.com>
2013-12-22 21:17:51 +0000

    Didn't git delete the previous files...

--- cookbooks/files.ihrdev.com/recipes/clean_backups_mongo.rb
+++
@@ -1,8 +0,0 @@
-cron_d "clean_backups_mongo" do
-  command "/usr/bin/find /data/exports/files.ihrdev.com/mongo_fac_bak /data/exports/files.ihrdev.com/mongo_usr_bak /data/exports/files.ihrdev.com/mongo_util_bak -type f -printf '\\%T@ \\%P\\n' | sort -n | cut -d' ' -f2- | head -n -1 | xargs rm"
-  minute  "25"
-  hour    "6"
-  month   "*"
-  weekday "*"
-  user    "root"
-end
--- cookbooks/files.ihrdev.com/recipes/clean_backups_pgsql.rb
+++
@@ -1,8 +0,0 @@
-cron_d "clean_backups_pgsql" do
-  command "/usr/bin/find /data/exports/files.ihrdev.com/auth_bak /data/exports/files.ihrdev.com/ing_bak -type f -printf '\\%T@ \\%P\\n' | sort -n | cut -d' ' -f2- | head -n -3 | xargs rm"
-  minute  "25"
-  hour    "6"
-  month   "*"
-  weekday "*"
-  user    "root"
-end
