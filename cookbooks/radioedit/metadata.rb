5dc1fc41d7c3d1ef9fa4d5c469524ba9568c80cd
Mark Rechler <mrechler@iheart.com>
2014-01-09 14:25:28 -0500

    make attributes env aware and bump cookbook number

--- cookbooks/radioedit/metadata.rb
+++ cookbooks/radioedit/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'none@none.com'
 license          'All rights reserved'
 description      'Installs/Configures radioedit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{ python application_python sudo }.each do |d|
   depends d
