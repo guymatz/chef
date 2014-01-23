e0984aaad84fdc86b2ad4dac1018ff4d76af011e
Joseph Hammerman <josephhammerman@clearchannel.com>
2014-01-23 11:36:42 -0500

    change made to deploy logic for ES, to backup previus version on disk

--- cookbooks/elasticsearchnew/recipes/default.rb
+++ cookbooks/elasticsearchnew/recipes/default.rb
@@ -27,11 +27,7 @@ unless tagged?('elasticsearchnew-deployed')
   end
   
   execute "Untar-ihr-search-configs" do
-    unless Dir.exist? "#{node[:elasticsearchnew][:ihrsearch_path]}/configs.bak"
-      command "tar zxf #{pkg} -C #{node[:elasticsearchnew][:ihrsearch_path]}/configs"
-    else
-      command "rm -rf #{node[:elasticsearchnew][:ihrsearch_path]}/configs.bak; mv #{node[:elasticsearchnew][:ihrsearch_path]}/configs #{node[:elasticsearchnew][:ihrsearch_path]}/configs.bak; mkdir #{node[:elasticsearchnew][:ihrsearch_path]}/configs; tar zxf #{pkg} -C #{node[:elasticsearchnew][:ihrsearch_path]}/configs"
-    end  
+    command "rm -rf #{node[:elasticsearchnew][:ihrsearch_path]}/configs.bak; mv #{node[:elasticsearchnew][:ihrsearch_path]}/configs #{node[:elasticsearchnew][:ihrsearch_path]}/configs.bak; mkdir #{node[:elasticsearchnew][:ihrsearch_path]}/configs; tar zxf #{pkg} -C #{node[:elasticsearchnew][:ihrsearch_path]}/configs"
     cwd Chef::Config[:file_cache_path]
     action :nothing
     notifies :run, resources(:execute => "chown-ihr-search-configs"), :immediately
