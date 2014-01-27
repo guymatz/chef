d2527af7cd58da457abbc8e5f5e00ab154d6eb17
Gregory Patmore <gregorypatmore@gmail.com>
2014-01-27 11:24:50 -0500

    updated to lower case 'p' in prontf clause of final find command

--- cookbooks/disaster_recovery/recipes/unpack_attivio.rb
+++ cookbooks/disaster_recovery/recipes/unpack_attivio.rb
@@ -7,7 +7,7 @@ end
 
 
 cron_d "unpack_attivio" do
-  command "/bin/rm -f /data/exports/dr_backups/attivio/decompressed/*; /bin/tar xzf $(/usr/bin/find /data/exports/dr_backups/attivio  -name '*tgz' -type f -printf '\\%T@ \\%p\\n' | sort -n | tail -1 | cut -f2- -d\" \") -C /data/exports/dr_backups/attivio/decompressed; /bin/find /data/exports/dr_backups/attivio -maxdepth 1 -type f -name '*.tgz' -printf '\\%T@ \\%P\\n' | sort -n | cut -d' ' -f2- | head -n -1 | xargs rm"
+  command "/bin/rm -f /data/exports/dr_backups/attivio/decompressed/*; /bin/tar xzf $(/usr/bin/find /data/exports/dr_backups/attivio  -name '*tgz' -type f -printf '\\%T@ \\%p\\n' | sort -n | tail -1 | cut -f2- -d\" \") -C /data/exports/dr_backups/attivio/decompressed; /bin/find /data/exports/dr_backups/attivio -maxdepth 1 -type f -name '*.tgz' -printf '\\%T@ \\%p\\n' | sort -n | cut -d' ' -f2- | head -n -1 | xargs rm"
   minute  "25"
   hour    "16"
   month   "*"
