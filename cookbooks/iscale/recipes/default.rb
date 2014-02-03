85acba25bf80f5c09e6e24b0ffbea483809bb713
Gregory Patmore <gregorypatmore@gmail.com>
2014-02-03 18:59:07 -0500

    added varnish templates, settings templates, switch cased the attributes based on env, removed chef tag clocking codebase updates for all but prod environments.

--- cookbooks/iscale/recipes/default.rb
+++ cookbooks/iscale/recipes/default.rb
@@ -7,10 +7,63 @@
 # All rights reserved - Do Not Redistribute
 #
 
-application "iscale" do
-  path "/data/apps/iscale"
-  owner "root"
-  group "root"
-  repository 'git@github.com:iheartradio/iscale.git'
-  revision 'master'
+template '/etc/init.d/iscale' do
+  source 'iscale.sh.erb'
+  owner "#{node[:iscale][:user]}"
+  group "#{node[:iscale][:group]}"
+  mode "755"
+  variables ({
+    :user         => "#{node[:iscale][:user]}",
+    :app_home     => "#{node[:iscale][:app_home]}",
+    :lock_file    => "#{node[:iscale][:lock_file]}"
+  })
+end
+
+template '/etc/sysconfig/varnish' do
+  source 'sysconfig_varnish.erb'
+  owner "#{node[:iscale][:user]}"
+  group "#{node[:iscale][:group]}"
+  mode "644"
+  # variables({})
+end 
+
+template '/etc/varnish/default.vcl' do
+  source 'default.vcl.erb'
+  owner "#{node[:iscale][:user]}"
+  group "#{node[:iscale][:group]}"
+  mode "644"
+  # variables({})
+end 
+
+template "#{node[:iscale][:install_path]}/settings.json" do
+  source 'settings.json.erb'
+  owner "#{node[:iscale][:user]}"
+  group "#{node[:iscale][:group]}"
+  mode "644"
+  variables ({
+    :settings   => node[:iscale][:app_settings]
+  })
+end 
+
+service 'iscale' do
+  supports :start => true, :stop => true, :restart => true
+  action [ :enable ]
+end
+
+unless tagged?("#{node[:iscale][:deploy_tag]}") || node.chef_environment !~ /^prod/
+
+  application "iscale" do
+    path "#{node[:iscale][:install_path]}"
+    owner "#{node[:iscale][:user]}"
+    group "#{node[:iscale][:group]}"
+    repository "#{node[:iscale][:git_repository]}"
+    revision "#{node[:iscale][:git_branch]}"
+
+    notifies :restart, "service[iscale]", :immediately
+  end
+
+  
+
+  tag("#{node[:iscale][:deploy_tag]}")
+
 end
