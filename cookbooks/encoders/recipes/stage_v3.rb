7542fcc2c6b196ad32597a8b04989bc8169b894b
Guy Matz <guymatz@clearchannel.com>
2014-02-03 17:36:14 -0500

    making progress towards gettingv3 ingestion configured in stage

--- cookbooks/encoders/recipes/stage_v3.rb
+++ cookbooks/encoders/recipes/stage_v3.rb
@@ -41,6 +41,14 @@ begin
      end
   end
 
+  node[:gems].each do |gem,ver|
+     gem_package gem do
+       action :install
+       options "--no-ri --no-rdoc"
+       version ver
+     end
+  end
+
     tag("stage-v3")
     end
 rescue
