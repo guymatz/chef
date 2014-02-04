b5f49989dc0f5c6284b1b319926915e4786d328b
Gregory Patmore <gregorypatmore@gmail.com>
2014-02-04 11:47:47 -0500

    updated npm install recipe to just install from our local repo.  this will need to get a more formal refactor, just pushing through this temp change as we are only using it on one stack that is not in production yet.

--- cookbooks/nodejs/recipes/npm.rb
+++ cookbooks/nodejs/recipes/npm.rb
@@ -18,21 +18,23 @@
 # limitations under the License.
 #
 
-include_recipe "nodejs"
+yum_package 'npm';
 
-package "curl"
+# include_recipe "nodejs"
 
-npm_src_url = "http://registry.npmjs.org/npm/-/npm-#{node['nodejs']['npm']}.tgz"
+# package "curl"
 
-bash "install npm - package manager for node" do
-  cwd "/usr/local/src"
-  user "root"
-  code <<-EOH
-    mkdir -p npm-v#{node['nodejs']['npm']} && \
-    cd npm-v#{node['nodejs']['npm']}
-    curl -L #{npm_src_url} | tar xzf - --strip-components=1 && \
-    make uninstall dev
-  EOH
-  not_if "#{node['nodejs']['dir']}/bin/npm -v 2>&1 | grep '#{node['nodejs']['npm']}'"
-end
+# npm_src_url = "http://registry.npmjs.org/npm/-/npm-#{node['nodejs']['npm']}.tgz"
+
+# bash "install npm - package manager for node" do
+#   cwd "/usr/local/src"
+#   user "root"
+#   code <<-EOH
+#     mkdir -p npm-v#{node['nodejs']['npm']} && \
+#     cd npm-v#{node['nodejs']['npm']}
+#     curl -L #{npm_src_url} | tar xzf - --strip-components=1 && \
+#     make uninstall dev
+#   EOH
+#   not_if "#{node['nodejs']['dir']}/bin/npm -v 2>&1 | grep '#{node['nodejs']['npm']}'"
+# end
 
