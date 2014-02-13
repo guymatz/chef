513056035e3f4ab4c858361cdfd60bdbf133c796
grubino <GregRubino@clearchannel.com>
2014-02-13 17:33:34 -0500

    removing data warehouse attributes.

--- cookbooks/etl_jobs/attributes/default.rb
+++ cookbooks/etl_jobs/attributes/default.rb
@@ -12,11 +12,6 @@ default[:db_sync_tools][:repo] = "git@github.com:iheartradio/amp-tools"
 default[:db_sync_tools][:reference] = "d74b373c8b76f0a6994a1bb8427b045d891e7add"
 case chef_environment
 when /^prod/
-  default[:etl][:data_warehouse][:jdbc_url] = "jdbc:jtds:sqlserver://iheartdw.ihrint.com:1433;databaseName=IHRDWH"
-  default[:etl][:data_warehouse][:host] = "iad-dwh.prod.ihr"
-  default[:etl][:data_warehouse][:port] = "1433"
-  default[:etl][:data_warehouse][:batch_user_name] = "appBatch"
-  default[:etl][:data_warehouse][:batch_user_password] = "i8piZZa4u"
   default[:etl][:event_job][:archive_directory] = "/isilon/event/processed"
   default[:etl][:event_job][:source_directory] = "/isilon/event"
   default[:etl][:event_job][:log_directory] = "/isilon/event/"
@@ -36,11 +31,6 @@ when /^prod/
   default[:rabbit][:user_name] = "amp-tomcat"
   default[:rabbit][:password] = "tppw2011!"
 when /^stage/
-  default[:etl][:data_warehouse][:jdbc_url] = "jdbc:jtds:sqlserver://10.5.50.101;databaseName=IHRDWH"
-  default[:etl][:data_warehouse][:host] = "iheartdw.ihrint.com"
-  default[:etl][:data_warehouse][:port] = "1433"
-  default[:etl][:data_warehouse][:batch_user_name] = "appBatch"
-  default[:etl][:data_warehouse][:batch_user_password] = "i8piZZa4u"
   default[:etl][:event_job][:archive_directory] = "/data/log/event/processed"
   default[:etl][:event_job][:source_directory] = "/data/log/event/input"
   default[:etl][:event_job][:log_directory] = "/data/log/event"
