4ca6c89e33c911c6d576aaaaf47eec289da33123
Mark Rechler <mrechler@iheart.com>
2014-02-17 16:19:58 -0500

    add some attributes from stg in

--- cookbooks/amp/attributes/default.rb
+++ cookbooks/amp/attributes/default.rb
@@ -27,3 +27,18 @@ end
 # GP EDIT 8/16/13 Included a list of people to notify when endpoints show alert levels of 500s 
 # Used in ./templates/default/amp-extended-log-chk.sh.erb
 default[:amp][:logging][:notify_list] = %w{ jeremybraff@clearchannel.com kengilmer@clearchannel.com LaurentVauthrin@clearchannel.com mauricewalden@clearchannel.com }
+
+
+# Trying to port over relevant attributes from stg
+default[:amp][:usr_mongo][:hosts] = "localhost:27017"
+default[:amp][:usr_mongo][:connections_per_host] = 300
+default[:amp][:usr_mongo][:connect_timeout] = 3000
+default[:amp][:usr_mongo][:socket_timeout] = 3000
+default[:amp][:usr_mongo][:maxWaitTime] = 3000
+default[:amp][:usr_mongo][:threads_allowed_to_block_for_connections_multiplier] = 5
+default[:amp][:fac_mongo][:hosts] = "iad-mongo-fac101-v240.ihr:37017,iad-mongo-fac102-v240.ihr:37017,iad-mongo-fac103-v240.ihr:37017,iad-mongo-fac104-v240.ihr:37017"
+default[:amp][:fac_mongo][:connections_per_host] = 300
+default[:amp][:fac_mongo][:connect_timeout] = 5000
+default[:amp][:fac_mongo][:socket_timeout] = 5000
+default[:amp][:fac_mongo][:maxWaitTime] = 5000
+default[:amp][:fac_mongo][:threads_allowed_to_block_for_connections_multiplier] = 5
