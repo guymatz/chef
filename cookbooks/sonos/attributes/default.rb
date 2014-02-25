01684cf863097542b70be2a4e9020ea1e1787d54
Aaron Clements <aaronclements@clearchannel.com>
2014-02-25 12:50:19 -0500

    adding centos

--- cookbooks/sonos/attributes/default.rb
+++ cookbooks/sonos/attributes/default.rb
@@ -3,6 +3,8 @@ default[:sonos][:rev] = "5d34a2656c0fbe63dc3fad0eea05faceaba7ba95"
 default[:sonos][:deploy_path] = "/data/apps/sonos"
 
 case node[:platform_family]
+when "centos"
+  default[:sonos][:packages] = %w{ python27 python27-devel python27-libs python27-debuginfo python27-tools libxslt-devel libxml2-devel }
 when "rhel"
   default[:sonos][:packages] = %w{ python27 python27-devel python27-libs python27-debuginfo python27-tools libxslt-devel libxml2-devel }
 when "debian"
