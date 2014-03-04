0dd887fa32cd3702c4df0a7cc20dbf13ce46d67e
Gregory Patmore <gregorypatmore@gmail.com>
2014-03-04 16:34:17 -0500

    updated version and plugins recipe to distribute river plugin

--- cookbooks/elasticsearchnew/recipes/plugins.rb
+++ cookbooks/elasticsearchnew/recipes/plugins.rb
@@ -30,60 +30,65 @@ unless tagged?("es-plugins-installed")
     group node[:elasticsearchnew][:group]
   end
 
-  if /stage/ =~ node.chef_environment
-    execute "install-river-rabbitmq-plugin" do
-      command "#{ES_HOME}/bin/plugin --url #{node[:elasticsearchnew][:url]}/#{node.chef_environment}/es-plugins/elasticsearch-river-rabbitmq.zip --install river-rabbitmq"
-      cwd Chef::Config[:file_cache_path]
-      notifies :restart, "service[elasticsearch]"
-      user node[:elasticsearchnew][:user]
-      group node[:elasticsearchnew][:group]
-    end
+  
+  execute "install-river-rabbitmq-plugin" do
+    command "#{ES_HOME}/bin/plugin --url #{node[:elasticsearchnew][:url]}/#{node.chef_environment}/es-plugins/elasticsearch-river-rabbitmq.zip --install river-rabbitmq"
+    cwd Chef::Config[:file_cache_path]
+    notifies :restart, "service[elasticsearch]"
+    user node[:elasticsearchnew][:user]
+    group node[:elasticsearchnew][:group]
+  end
 
   primary_node=search(:node, "role:elasticsearchnew AND chef_environment:#{node.chef_environment}")[0]
 
   Chef::Log.info("Primary node info: #{primary_node}")
   Chef::Log.info("Host name info: #{node[:hostname]}")
 
-  if primary_node[:hostname] == node[:hostname]
-    execute "configure-river-rabbitmq-plugin" do
-      command <<-EOH 
-        /usr/bin/curl -XPUT '#{node[:ipaddress]}:9200/_river/my_river/_meta' -d '{ \
-        "type" : "rabbitmq", \
-        "rabbitmq" : { \
-            "addresses" : [ \
-                { \
-                    "host" : "iad-stg-rabbitmq101.ihr.", \
-                    "port" : 5672 \
-                }, \
-                { \
-                    "host" : "iad-stg-rabbitmq102.ihr.", \
-                    "port" : 5672 \
-                } \
-            ], \
-            "user" : "fac", \
-            "pass" : "tppw2011!", \
-            "vhost" : "/ingestion", \
-            "queue" : "elasticsearch", \
-            "exchange" : "elasticsearch", \
-            "routing_key" : "elasticsearch", \
-            "exchange_declare" : true, \
-            "exchange_type" : "direct", \
-            "exchange_durable" : true, \
-            "queue_declare" : true, \
-            "queue_bind" : true, \
-            "queue_durable" : true, \
-            "queue_auto_delete" : false, \
-            "heartbeat" : "30s" \
-        }, \
-        "index" : { \
-            "bulk_size" : 100, \
-            "bulk_timeout" : "50ms", \
-            "ordered" : false \
-        } \
-       }'
-      EOH
-    end
-  end
-end
+  if /stage/ =~ node.chef_environment
+
+    if primary_node[:hostname] == node[:hostname]
+      execute "configure-river-rabbitmq-plugin" do
+        command <<-EOH 
+          /usr/bin/curl -XPUT '#{node[:ipaddress]}:9200/_river/my_river/_meta' -d '{ \
+          "type" : "rabbitmq", \
+          "rabbitmq" : { \
+              "addresses" : [ \
+                  { \
+                      "host" : "iad-stg-rabbitmq101.ihr.", \
+                      "port" : 5672 \
+                  }, \
+                  { \
+                      "host" : "iad-stg-rabbitmq102.ihr.", \
+                      "port" : 5672 \
+                  } \
+              ], \
+              "user" : "fac", \
+              "pass" : "tppw2011!", \
+              "vhost" : "/ingestion", \
+              "queue" : "elasticsearch", \
+              "exchange" : "elasticsearch", \
+              "routing_key" : "elasticsearch", \
+              "exchange_declare" : true, \
+              "exchange_type" : "direct", \
+              "exchange_durable" : true, \
+              "queue_declare" : true, \
+              "queue_bind" : true, \
+              "queue_durable" : true, \
+              "queue_auto_delete" : false, \
+              "heartbeat" : "30s" \
+          }, \
+          "index" : { \
+              "bulk_size" : 100, \
+              "bulk_timeout" : "50ms", \
+              "ordered" : false \
+          } \
+         }'
+        EOH
+      end # execute
+
+    end # if primary
+
+end #unless tagged
   tag("es-plugins-installed")
+  
 end
