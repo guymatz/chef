3732c4f615f70673e682624a44a52f308ab738b8
Gregory Patmore <gregorypatmore@gmail.com>
2014-03-05 18:22:19 -0500

    bumped the cb version to 1.0.1 and updated the rev attribute to the guinness release tag

--- cookbooks/webplayer/metadata.rb
+++ cookbooks/webplayer/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'none@none.com'
 license          'All rights reserved'
 description      'Installs/Configures webplayer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 %w{ java application_python apache2 bondage users logrotate cron membase }.each do |dep|
   depends dep
