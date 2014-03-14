fccd6b6f2b21907a302e595683c367451cb374f7
Gregory Patmore <gregorypatmore@gmail.com>
2014-03-14 03:58:21 -0400

    refactoring towards lwrp attributes

--- cookbooks/radioedit/providers/unicorn.rb
+++ cookbooks/radioedit/providers/unicorn.rb
@@ -23,166 +23,95 @@
 #
 action :init do 
     unless node.tags.include? "radioedit.#{new_resource.name}"
-        Chef::Log.info("Bootstrapping app #{new_resource.name}")
+      Chef::Log.info("Bootstrapping app #{new_resource.name}")
 
-        #
-        # this is where we set some temp variables as it is easier than
-        # if we keep having to reference node / defaults.   
-        # we shouldn't be overriding anything here, the place to override app defaults
-        # is under the specific stage settings in attributes/default.rb
-        #
-        app_name = "#{new_resource.name}"
-        app_user = "#{node[:radioedit][:app_user]}"
-        app_port = "/var/tmp/radioedit-#{app_name}.sock"
-        log_level = "DEBUG"
-#        log_level =  "#{[:radioedit][#{app_name}][:log_level]}"
-        pid_file = "/var/run/radioedit-#{app_name}.pid"
-        app_listen = "/var/tmp/radioedit-#{app_name}.sock"
-        out_log =  "#{node[:radioedit][:log_dir]}/#{app_name}_error.log"
-        err_log =  "#{node[:radioedit][:log_dir]}/#{app_name}_error.log"
-        app_root = "/data/apps/radioedit/#{app_name}"
-        env_path = "#{app_root}/env"
-        src_path = "#{app_root}/releases"
+      #
+      # this is where we set some temp variables as it is easier than
+      # if we keep having to reference node / defaults.   
+      # we shouldn't be overriding anything here, the place to override app defaults
+      # is under the specific stage settings in attributes/default.rb
+      #
+      log_level = @new_resource.log_level
+      pid_file = @new_resource.pid_file
+      out_log =  @new_resource.out_log
+      err_log =  @new_resource.err_log
+      @new_resource.root_dir = @new_resource.root_dir
+      env_path = @new_resource.env_dir || "#{@new_resource.root_dir}/env"
+      src_path = @new_resource.src_dir || "#{@new_resource.root_dir}/releases"
+      app_repo =  @new_resource.repository
+      app_revision = @new_resource.revision
+      app_workers = @new_resource.workers
+      app_env = @new_resource.environment_name
+      env_vars = @new_resource.env_vars
 
-        #
-        # Create directories for application
-        #
-        [app_root, env_path, src_path].each do |d|
-            directory d do
-              owner app_user
-              group app_user
-            end
-        end # directories
-
-        #
-        # setup virtual env #TODO JPD I DON"T KNOW IF THIS ACTUALLY WORKS
-        #
-#        python_virtualenv env_path do
-#            interpreter "python27"
-#            owner app_user
-#            group app_user
-#            options "--distribute"
-#            action :create
-#        end # virtualenv
-
-       #
-       # we need to do some more setting of variables based on which type of recipe we are calling
-       # We can't just call these directly below because the variable exapnsion isn't working 
-       # (double expansion such as:  node[:radioedit][#{app_name}][:repo] wasn't working)
-       #
-       case app_name
-        when /app_api/
-            app_repo =  node[:radioedit][:app_api][:repo]
-            app_revision = node[:radioedit][:app_api][:deploy_revision] 
-            app_workers = node[:radioedit][:app_api][:num_workers]
-
-            app_env = node[:radioedit][:app_api][:environment]
-            app_mongo = node[:radioedit][:app_api][:environment][:RD_MONGO_URI]
-
-            Chef::Log.warn("DUMPING ENV VARS: #{node[:radioedit][:app_api]}")
-            Chef::Log.warn("DUMPING ENV VARS (alias): #{app_env}")
-
-        when /app_auth/
-            app_repo =  node[:radioedit][:app_auth][:repo]
-            app_revision = node[:radioedit][:app_auth][:deploy_revision] 
-            app_workers = node[:radioedit][:app_auth][:num_workers]
-
-            app_env = node[:radioedit][:app_auth][:environment]
-
-       end # case
-
-       # template "#{app_root}/current/settings.json" do
-       #   source "stage-settings.json.erb"
-       #   owner app_user
-       #   group app_user
-       #      variables({
-       #          # TODO put variables for this file. jpd
-       #      })
-       #  end
-
-       # link "#{app_root}/shared/settings.json" do
-       #   to "#{app_root}/current/settings.json"
-       #   action :create
-       #   owner app_user
-       #   group app_user
-       #   not_if "test -L #{app_root}/shared/settings.json"
-       # end
-
-      # Chef::Log.warn("DEPLOYING APP_NAME: #{app_name}");
-      # Chef::Log.warn("DEPLOYING APP_USER: #{app_user}");
-      # Chef::Log.warn("DEPLOYING APP_PORT: #{app_port}");
-      # Chef::Log.warn("DEPLOYING LOG_LEVEL: #{log_level}");
-      # Chef::Log.warn("DEPLOYING PID_FILE: #{pid_file}");
-      # Chef::Log.warn("DEPLOYING APP_LISTEN: #{app_listen}");
-      # Chef::Log.warn("DEPLOYING OUT_LOG: #{out_log}");
-      # Chef::Log.warn("DEPLOYING ERR_LOG: #{err_log}");
-      # Chef::Log.warn("DEPLOYING APP_ROOT: #{app_root}");
-      # Chef::Log.warn("DEPLOYING ENV_PATH: #{env_path}");
-      # Chef::Log.warn("DEPLOYING SRC_PATH: #{src_path}");
-      # Chef::Log.warn("DEPLOYING APP_REPO: #{app_repo}");
-      # Chef::Log.warn("DEPLOYING APP_REV: #{app_revision}");
-      # Chef::Log.warn("DEPLOYING APP_WORKERS: #{app_workers}");
-      # Chef::Log.warn("DEPLOYING ENV_VARS: #{app_env}");
-      # Chef::Log.warn("DEPLOYING NUM_ENV_VARS: #{app_env.length}");
+      #
+      # Create directories for application
+      #
+      [ @new_resource.root_dir, @new_resource.env_dir, @new_resource.src_dir ].each do |d|
+          directory d do
+            owner @new_resource.user
+            group @new_resource.user
+          end
+      end # directories
 
         
-        #
-        # set up the actual application and gunicorn
-        #
-        application app_name do
-            repository app_repo
-            revision app_revision
-            path app_root
-            owner app_user
-            group app_user
-            enable_submodules true
+      #
+      # set up the actual application and gunicorn
+      #
+      application @new_resource.name do
+          repository @new_resource.repository
+          revision @new_resource.revision
+          path @new_resource.root_dir
+          owner @new_resource.user
+          group @new_resource.user
+          enable_submodules @new_resource.enable_submodules
 
-            gunicorn do
-              app_module "wsgi"
-              settings_template "re-gunicorn.py.erb"
-              port app_port
-              host node[:radioedit][:host]
-              workers app_workers
-              pidfile pid_file
-              stdout_logfile out_log
-              stderr_logfile err_log
-              loglevel log_level
-              interpreter "python27"
-              requirements "requirements.txt"
-              autostart true
-              virtualenv env_path
-              environment app_env
-            end # gunicorn
-            
-        end # application
+          gunicorn do
+            app_module @new_resource.module
+            settings_template "re-gunicorn.py.erb"
+            port @new_resource.port
+            host @new_resource.host
+            workers @new_resource.workers
+            pidfile @new_resource.pid_file
+            stdout_logfile @new_resource.stdout_logfile
+            stderr_logfile @new_resource.stderr_logfile
+            loglevel @new_resource.loglevel 
+            interpreter "python27"
+            requirements "requirements.txt"
+            autostart @new_resource.autostart
+            virtualenv @new_resource.venv_dir
+            environment @new_resource.environment
+          end # gunicorn
+          
+      end # application
 
         #
         # install pip requirements
         #
 #        execute "install_requirements" do
-#            cwd app_root
-#            user app_user
-#            command "#{env_path}/bin/pip install -r #{app_root}/current/requirements.txt"
+#            cwd @new_resource.root_dir
+#            user @new_resource.user
+#            command "#{env_path}/bin/pip install -r #{@new_resource.root_dir}/current/requirements.txt"
 #        end
 
         #
         # Nginx Config
         #
-        template "/etc/nginx/conf.d/#{app_name}.conf" do
-          source "nginx-#{app_name}.conf.erb"
+        template "/etc/nginx/conf.d/#{new_resource.name}.conf" do
+          source "nginx-#{new_resource.name}.conf.erb"
           owner "root"
           group "root"
           mode 0600
           variables({
-              :app_name =>            app_name,
+              :app_name =>            @new_resource.name,
               :legacy_static_root =>  node[:radioedit][:legacy_static_root],
-              :webserver_port =>      node[:radioedit]["#{app_name}"][:nginx_listen],
+              :webserver_port =>      node[:radioedit]["#{new_resource.name}"][:nginx_listen],
           })
             
         end
 
         # Create service init script for application.
-        template "/etc/init/#{app_name}.conf" do
+        template "/etc/init/#{new_resource.name}.conf" do
             source "radioedit-initd.sh.erb"
             owner "root"
             group "root"
@@ -191,7 +120,7 @@ action :init do
         #
         # setup log rotate for the app we just setup
         # 
-        logrotate_app "#{app_name}.out" do
+        logrotate_app "#{new_resource.name}.out" do
           cookbook "logrotate"
           path out_log
           options ["missingok", "compress", "notifempty", "sharedscripts","dateext"]
@@ -202,7 +131,7 @@ action :init do
           postrotate ' [ -f #{pid_file}] && kill -USR1 `cat #{pid_file}`'
         end
 
-        logrotate_app "#{app_name}.err" do
+        logrotate_app "#{new_resource.name}.err" do
           cookbook "logrotate"
           path err_log
           options ["missingok", "compress", "notifempty", "sharedscripts","dateext"]
--- cookbooks/radioedit/resources/unicorn.rb
+++ cookbooks/radioedit/resources/unicorn.rb
@@ -13,7 +13,30 @@
 ### this provides the setup of a python/gunicorn app
 ###
 
-attribute :unicorn, :kind_of => String, :name_attribute => true
-
 actions :init
 default_action :init
+
+# default name attribute
+attribute :unicorn, :required => true, :kind_of => String, :name_attribute => true
+# user to run the app as
+attribute :user, :kind_of => String, :default => "root"
+# port listener for app communication
+attribute :port, :required => true, :kind_of => [Integer, String]
+# severity level for log reporting
+attribute :loglevel, :kind_of => String, default => "ERROR", equal_to => %w{ DEBUG, INFO, WARN, ERROR }
+attribute :pid_file
+attribute :stdout_log
+attribute :stderr_log
+attribute :root_dir
+attribute :venv_dir
+attribute :src_dir
+attribute :repository
+attribute :revision
+attribute :workers
+attribute :environment
+attribute :environment_name
+attribute :enable_submodules
+attribute :module
+attribute :host
+attribute :autostart
+attribute :deploy_tag
