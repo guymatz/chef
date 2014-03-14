8a155a405286f8ed39f2dca69d1a9262de1a4063
John Deragon <jderagon@clearchannel.com>
2014-03-14 12:34:50 -0400

    es2graphite setup

--- cookbooks/graphite/recipes/collectors.rb
+++ cookbooks/graphite/recipes/collectors.rb
@@ -19,3 +19,18 @@ cron "carpathia" do
   user "root"
   command "/data/collectors/carpathia.py"
 end
+
+cookbook_file "/data/collectors/es2graphite.py" do
+  source "es2graphite.py"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+supervisor_service "es2graphite" do
+  command "/data/collectors/es2graphite -p elastic -g iad-graphite101.ihr iad-search401-v200.ihr:9200"
+  action [:enable, :restart]
+  autostart true
+  user "nobody"
+  numprocs 1
+end
