8e2a2a0a002472e3f422e97345355f5dfc22f98c
Joseph Hammerman <josephhammerman@clearchannel.com>
2014-03-17 14:04:12 -0400

    removing update_es_index recipe from default.rb

--- cookbooks/jobserver/recipes/default.rb
+++ cookbooks/jobserver/recipes/default.rb
@@ -9,7 +9,6 @@
 
 include_recipe "users::jobserver"
 include_recipe "users::jobserver-sudo"
-include_recipe "jobserver::update_es_index"
 
 if node.chef_environment == 'prod'
   include_recipe "jobserver::ha"
