5764dc7a9d0fcefe13b2a6f87123b693a711f1ad
John Li <johnli@clearchannel.com>
2014-03-17 12:39:03 -0400

    corrected to use the correct recipe/cookbook

--- cookbooks/facebook/recipes/log_cleanup.rb
+++ cookbooks/facebook/recipes/log_cleanup.rb
@@ -6,7 +6,3 @@ cron_d "fbgraph_log_cleanup" do
   command 'rm -f /var/log/fbgraph-consumer/*log.*'
 end
 
-cron_d "fbconsumer_log_cleanup" do
-  hour "22"
-  command 'find /data/log/facebook-consumer* -type f -mtime +30 -exec rm -rf {} \;'
-end
