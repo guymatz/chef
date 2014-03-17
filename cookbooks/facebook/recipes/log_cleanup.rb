7279ef76f8f9451f45229bd072768067ae50aab7
John Li <johnli@clearchannel.com>
2014-03-17 12:05:45 -0400

    adding new cron job to cleanup /data/log/facebook-consumer* on iad-jobserver101a.ihr OPS-6361

--- cookbooks/facebook/recipes/log_cleanup.rb
+++ cookbooks/facebook/recipes/log_cleanup.rb
@@ -5,3 +5,8 @@ cron_d "fbgraph_log_cleanup" do
   minute 0
   command 'rm -f /var/log/fbgraph-consumer/*log.*'
 end
+
+cron_d "fbconsumer_log_cleanup" do
+  hour "22"
+  command 'find /data/log/facebook-consumer* -type f -mtime +30 -exec rm -rf {} \;'
+end
