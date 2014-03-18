d8aa3950499e48f127e04574ff5a23e4a125f07d
Gregory Patmore <gregorypatmore@gmail.com>
2014-03-18 16:53:26 -0400

    debugging script

--- cookbooks/radioedit/attributes/default.rb
+++ cookbooks/radioedit/attributes/default.rb
@@ -196,7 +196,7 @@ case chef_environment
         };
 
         default[:radioedit][:stage][:nfs_locdir] = "/data/binstore"
-        default[:radioedit][:stage][:nfs_server] = "iad-stg-nfs101.ihr"
+        default[:radioedit][:stage][:nfs_server] = "iad-stg-nfs101-v700.ihr"
         default[:radioedit][:stage][:nfs_remdir] = "/data/export/radioedit"
 
         #
