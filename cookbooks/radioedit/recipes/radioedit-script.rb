41747ffd68c81a37630626bc1b23150463bd57a6
Gregory Patmore <gregorypatmore@gmail.com>
2014-03-18 13:08:40 -0400

    debugging script

--- cookbooks/radioedit/recipes/radioedit-script.rb
+++ cookbooks/radioedit/recipes/radioedit-script.rb
@@ -34,26 +34,25 @@ radioedit_node "app_script" do
 
     not_if { node.chef_environment =~ /^prod/ && node.tags.include?(node[:radioedit][:app_script][:deploy_tag]) }
 
-    user node[:radioedit][:app_auth][:user_name]
-    host node[:radioedit][:app_auth][:host]
-    port node[:radioedit][:app_auth][:port]
-    app_module node[:radioedit][:app_auth][:module]
-    webserver_listen node[:radioedit][:app_auth][:nginx_listen]
-    webserver_name node[:radioedit][:app_auth][:webserver_name]
-    log_level node[:radioedit][:app_auth][:log_level]
-    pid_file node[:radioedit][:app_auth][:pid_file]
-    stdout_log "#{node[:radioedit][:log_dir]}/app_auth.out"
-    stderr_log "#{node[:radioedit][:log_dir]}/app_auth.err"
-    root_dir node[:radioedit][:app_auth][:root_dir]
-    venv_dir "#{node[:radioedit][:app_auth][:root_dir]}/env"
-    src_dir "#{node[:radioedit][:app_auth][:root_dir]}/releases"
-    repository node[:radioedit][:app_auth][:repo]
-    revision node[:radioedit][:app_auth][:deploy_revision]
+    user node[:radioedit][:app_script][:user_name]
+    host node[:radioedit][:app_script][:host]
+    port node[:radioedit][:app_script][:port]
+    webserver_listen node[:radioedit][:app_script][:nginx_listen]
+    webserver_name node[:radioedit][:app_script][:webserver_name]
+    log_level node[:radioedit][:app_script][:log_level]
+    pid_file node[:radioedit][:app_script][:pid_file]
+    stdout_log "#{node[:radioedit][:log_dir]}/app_script.out"
+    stderr_log "#{node[:radioedit][:log_dir]}/app_script.err"
+    root_dir node[:radioedit][:app_script][:root_dir]
+    venv_dir "#{node[:radioedit][:app_script][:root_dir]}/env"
+    src_dir "#{node[:radioedit][:app_script][:root_dir]}/releases"
+    repository node[:radioedit][:app_script][:repo]
+    revision node[:radioedit][:app_script][:deploy_revision]
     enable_submodules true
-    workers node[:radioedit][:app_auth][:num_workers]
-    environment node[:radioedit][:app_auth][:environment]
+    workers node[:radioedit][:app_script][:num_workers]
+    environment node[:radioedit][:app_script][:environment]
     autostart true
-    deploy_tag node[:radioedit][:app_auth][:deploy_tag]
+    deploy_tag node[:radioedit][:app_script][:deploy_tag]
 
 end
 
