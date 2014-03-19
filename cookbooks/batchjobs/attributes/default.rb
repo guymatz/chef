3c0ccdcb35b7c19f56b89477b9c2972a29ce8d15
Joseph Hammerman <josephhammerman@clearchannel.com>
2014-03-19 16:39:26 -0400

    Changes for OPS-6396

--- cookbooks/batchjobs/attributes/default.rb
+++ cookbooks/batchjobs/attributes/default.rb
@@ -6,10 +6,20 @@ default[:batchjobs][:deploy_path] = "/data/apps/batchjobs"
 default[:batchjobs][:secretpath] = "/etc/chef/encrypted_data_bag_secret"
 default[:batchjobs][:user] = "batchjobs"
 default[:batchjobs][:group] = "batchjobs"
-default[:batchjobs][:mssql_db] = "10.5.61.12"
 case chef_environment
+when /^prod/
+default[:batchjobs][:rabbit_host] = "iad-rabbitmq-vip-v200.ihr"
+default[:batchjobs][:rabbit_port] = 5672
+default[:batchjobs][:rovi_upload_identity] = "Prod"
+default[:batchjobs][:mssql_db] = "iheartdw.ihrint.com"
+when /^stage/
+default[:batchjobs][:rabbit_host] = "iad-stg-rabbitmq-vip-v700.ihr"
+default[:batchjobs][:rabbit_port] = 5672
+default[:batchjobs][:rovi_upload_identity] = "Dev"
+default[:batchjobs][:mssql_db] = "10.5.61.12"
 when /^development/
 default[:batchjobs][:rabbit_host] = "iad-int-rabbitmq101.ihr"
 default[:batchjobs][:rabbit_port] = 5672
 default[:batchjobs][:rovi_upload_identity] = "Dev"
+default[:batchjobs][:mssql_db] = "10.5.61.12"
 end
