34b70ab7181942f0001bef227f7a792941ae0e7e
Aaron Clements <aaronclements@clearchannel.com>
2014-03-25 12:51:29 -0400

    bumping version

--- cookbooks/fac/metadata.rb
+++ cookbooks/fac/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'none@none.com'
 license          'All rights reserved'
 description      'Installs/Configures fac'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 %w{ java cron hostsfile attivio nagios elasticsearch amp }.each do |dep|
   depends dep
