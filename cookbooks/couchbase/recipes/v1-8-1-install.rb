8c8e5be0ca63a450db7cab821e3d7ee89e2eef0d
Gregory Patmore <gregorypatmore@gmail.com>
2014-03-26 15:47:11 -0400

    adding install 1.8.1 recipe

---
+++ cookbooks/couchbase/recipes/v1-8-1-install.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: couchbase
+# Recipe:: v2-1-install.rb
+#
+# Copyright 2013, iHeartRadio
+#
+# All rights reserved - Do Not Redistribute
+# Installs Couchbase 2.1
+#
+# #######################################
+
+# need to uninstall membase-server or it conflicts with couchbase-server 
+# (normally this is easy via package management, however, chef adds a certain barrier of easy use)
+package "membase-server" do
+  action :remove
+end
+
+directory "/data/apps/couchbase/data" do
+  recursive true
+  owner "couchbase"
+  group "couchbase"
+end
+
+# that rpm is the default rpm in our repo
+package "couchbase-server*1.8.1*.x86_64" do 
+  action :install
+end
