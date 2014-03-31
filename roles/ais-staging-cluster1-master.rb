4fe7d16c7febd98f92d3478ec0788a30263a2396
Mark Rechler <mrechler@iheart.com>
2014-03-31 13:05:01 -0400

    new AIS staging roles

---
+++ roles/ais-staging-cluster1-master.rb
@@ -0,0 +1,20 @@
+name 'ais-staging-cluster1-master'
+description 'used to build a staging master server for AIS'
+all_env = [
+           'recipe[ais::deploy]'
+          ]
+run_list(all_env)
+
+env_run_lists(
+              '_default' => all_env,
+              'staging-hls' => all_env
+              )
+
+default_attributes ({
+                      'ais' => {
+                        'install_id' => 'cchlsmaster211013',
+                        'cluster_name' => 'cluster1',
+                        'ais_type' => 'master',
+                        'version' => '6.2.87'
+                      }
+                    })
