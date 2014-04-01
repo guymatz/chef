03398d9c812f63f8b97a97b150bc4869dd4cb091
Joseph Hammerman <josephhammerman@clearchannel.com>
2014-04-01 18:54:02 +0000

    Adding batchjobs user

--- roles/batchjobs_code.rb
+++ roles/batchjobs_code.rb
@@ -2,5 +2,6 @@ name "batchjobs_code"
 description "Batchjobs code deploy"
 run_list(
          "recipe[users::deployer]",
+         "recipe[users::batchjobs]",
          "recipe[batchjobs::code_deploy]"
 )
