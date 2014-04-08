345d1f321116b7f87c9aa328c46bbb86fdf833bd
Gregory Patmore <gregorypatmore@gmail.com>
2014-04-08 12:38:04 -0400

    switched from remote_file to file resource

--- cookbooks/etl_jobs/recipes/remove-facebook-consumer2.rb
+++ cookbooks/etl_jobs/recipes/remove-facebook-consumer2.rb
@@ -8,15 +8,15 @@ end
 directory "/var/run/facebook-consumer-2" do
   action :delete
 end
-remote_file "/home/amqp-consumer/facebook-consumer-2/facebook_consumer.jar" do
+file "/home/amqp-consumer/facebook-consumer-2/facebook_consumer.jar" do
   action :delete 
 end
-remote_file "/home/amqp-consumer/facebook-consumer-2/env.properties" do
+file "/home/amqp-consumer/facebook-consumer-2/env.properties" do
   action :delete
 end
-remote_file "/home/amqp-consumer/facebook-consumer-2/log4j.xml" do
+file "/home/amqp-consumer/facebook-consumer-2/log4j.xml" do
   action :delete
 end
-remote_file "/etc/init.d/facebook-consumer-2" do
+file "/etc/init.d/facebook-consumer-2" do
   action :delete
 end
