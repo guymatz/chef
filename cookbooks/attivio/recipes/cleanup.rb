6dc6eedbfb7826008c71663604039baf194e3162
John Deragon <jderagon@clearchannel.com>
2014-04-09 15:13:31 +0000

    changed 25 to 15 day retention due to disk space issue

--- cookbooks/attivio/recipes/cleanup.rb
+++ cookbooks/attivio/recipes/cleanup.rb
@@ -1,5 +1,5 @@
 cron_d "remove_old_logs" do
   hour 1
   minute 0
-  command 'find /data/apps/attivio31/iheartradio3/logs*/. -mtime +25 -exec rm {} \; > /dev/null 2>&1'
+  command 'find /data/apps/attivio31/iheartradio3/logs*/. -mtime +15 -exec rm {} \; > /dev/null 2>&1'
 end
