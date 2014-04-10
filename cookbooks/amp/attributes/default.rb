006b6fced6f814283d1d201849b0da7a6d0a0be3
mwalden <marcelwalden@clearchannel.com>
2014-04-10 16:04:52 -0400

    Update default.rb

--- cookbooks/amp/attributes/default.rb
+++ cookbooks/amp/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:amp][:version] = "wham-rc5"
-default[:amp][:amp_rest_version] = "2.23.5"
+default[:amp][:version] = "xzibit-uat-7"
+default[:amp][:amp_rest_version] = "2.24.1"
 default[:amp][:url] = "http://files.ihrdev.com/amp"
 
 default[:amp][:packages] = %w{ mongo-10gen-server mongo-10gen }
