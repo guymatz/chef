4765e030c80dca07eb61f174975ef46e265d1f0d
John Deragon <jderagon@clearchannel.com>
2014-04-16 18:07:22 +0000

    bump of prod encoder cookbook ver

--- cookbooks/encoders/metadata.rb
+++ cookbooks/encoders/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ccd-sa@clearchannel.com'
 license          'All rights reserved'
 description      'Installs/Configures encoder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 
 %w{ apache2 yum java line application_ruby tomcat7 logrotate nagios sudo }.each do |dep|
